7f704670d224fe0945f35dbfff86e824ee2cd4e1
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-03 07:52:29 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240803/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240803/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,18 +1,33 @@
-str_A = input()
-str_B = input()
+def build_prefix_table(pattern):
+    prefix_table = [0] * len(pattern)
+    j = 0
 
+    for i in range(1, len(pattern) - 1):
 
-def find_goal(A, B):
-    for i in range(len(A) - len(B) + 1):
-        if A[i] == B[0]:
-            match = True
+        while j > 0 and pattern[j] != pattern[i]:
+            j = prefix_table[j-1]
 
-            for j in range(len(B)):
-                if A[i + j] != B[j]:
-                    match = False
-            
-            if match:
-                return i
+        if pattern[j] == pattern[i]:
+            j += 1
+            pattern[i] = j
+    
+    return prefix_table
+
+def kmp_search(text, pattern):
+    prefix_table = build_prefix_table(pattern)
+    j = 0
+    
+    for i in range(len(text)):
+        while j > 0 and text[i] != pattern[j]:
+            j = prefix_table[j - 1]
+        
+        if text[i] == pattern[j]:
+            j += 1
+
+        if j == len(pattern):
+            return i - j + 1
     return -1
 
-print(find_goal(str_A, str_B))
+str_A = input()
+str_B = input()
+print(kmp_search(str_A, str_B))
