eb1bf0f87dece2a2c1ecf9a76d35adcf5a0bb9e5
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-03 01:55:04 +0900

    Title: 2개 이상의 알파벳, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240803/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -5,7 +5,7 @@ def has_two_unique(string):
     set_string = set(list_string)
     return len(set_string) >= 2
 
-if has_two_unique(string):
+if has_two_unique(str_A):
     print("Yes")
 else:
     print("No")
