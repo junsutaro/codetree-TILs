2fc01844434540d3f05ec0b52b0173792b925662
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-03 02:34:05 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,18 @@
+str_A = input()
+str_B = input()
+
+
+def find_goal(A, B):
+    for i in range(len(A) - len(B) + 1):
+        if A[i] == B[0]:
+            match = True
+
+            for j in range(len(B)):
+                if A[i + j] != B[j]:
+                    match = False
+            
+            if match:
+                return i
+
+
+print(find_goal(str_A, str_B))
