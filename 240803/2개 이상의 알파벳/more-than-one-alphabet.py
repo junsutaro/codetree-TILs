483839218402873acf4c0676cd2f6a506a196523
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-03 01:54:28 +0900

    Title: 2개 이상의 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240803/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240803/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,14 +1,11 @@
 str_A = input()
 
-
-
 def has_two_unique(string):
     list_string = list(string)
     set_string = set(list_string)
+    return len(set_string) >= 2
 
-    return len(list_string) != len(set_string)
-
-if has_two_unique(str_A):
+if has_two_unique(string):
     print("Yes")
 else:
     print("No")
