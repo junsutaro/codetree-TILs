4c01b8f0b686e3210472026ad9d3385522020c4e
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-03 18:50:29 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240803/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -3,31 +3,33 @@ def build_prefix_table(pattern):
     j = 0
 
     for i in range(1, len(pattern)):
-
-        while j > 0 and pattern[j] != pattern[i]:
+        while j > 0 and pattern[i] != pattern[j]:
             j = prefix_table[j-1]
-
+    
         if pattern[i] == pattern[j]:
             j += 1
             prefix_table[i] = j
     
     return prefix_table
 
+
 def kmp_search(text, pattern):
     prefix_table = build_prefix_table(pattern)
+
     j = 0
 
     for i in range(len(text)):
         while j > 0 and text[i] != pattern[j]:
             j = prefix_table[j - 1]
-        
+
         if text[i] == pattern[j]:
             j += 1
-
+        
         if j == len(pattern):
-            return i - j + 1
-    return -1
+            return i - j  + 1
 
-str_A = input()
-str_B = input()
-print(kmp_search(str_A, str_B))
+
+text = input()
+pattern = input()
+
+print(kmp_search(text, pattern))
