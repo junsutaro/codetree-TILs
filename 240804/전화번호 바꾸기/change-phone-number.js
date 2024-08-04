587ad3764000e32928af01ed1331ae5864e2bccc
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-04 10:29:55 +0900

    Title: 전화번호 바꾸기, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

--- 240804/전화번호 바꾸기/change-phone-number.js
+++ 240804/전화번호 바꾸기/change-phone-number.js
@@ -4,4 +4,4 @@ const input = fs.readFileSync(0).toString().trim();
 const parts = input.split('-');
 
 
-console.log(`${parts[0]}-${parts[1]}-${parts[2]}`);
+console.log(`${parts[0]}-${parts[2]}-${parts[1]}`);
