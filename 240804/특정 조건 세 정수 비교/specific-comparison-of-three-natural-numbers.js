9196310f85b7a36b1c30b584b392f62f11270da6
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-04 23:00:28 +0900

    Title: 특정 조건 세 정수 비교, Time: 168ms, Memory: 14MB, Status: Passed - CodeTree

--- 240804/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240804/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
-const [a, b, c] = input.split(' ');
+const [a, b, c] = input.split(' ').map(Number);
 
 const res1 = a <= b && a <= c ? 1 : 0;
-const res2 = a == b && a == c ? 1 : 0;
+const res2 = a === b && a === c ? 1 : 0;
 
 console.log(`${res1} ${res2}`)
