e7f6419d03194b9aa6513fdeae7cf49ca08f9580
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-05 00:32:03 +0900

    Title: 코로나 메뉴얼, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

--- 240805/코로나 메뉴얼/covid-manual.js
+++ 240805/코로나 메뉴얼/covid-manual.js
@@ -1,21 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const p1 = input[0].split(' ')
-const p2 = input[1].split(' ')
-const p3 = input[2].split(' ')
-
 let cnt = 0;
 
-if ( p1[0] == 'Y' && p1[1] >= 37 ) {
-    cnt += 1
+for (i = 0; i < input.length ; i ++ ) {
+    const [symptom, temp] = input[i].split(' ');
+    if ( symptom == 'Y' && Number(temp) >= 37 ) {
+        cnt += 1
+    }
 }
-if ( p2[0] == 'Y' && p2[1] >= 37 ) {
-    cnt += 1
-}
-if ( p3[0] == 'Y' && p3[1] >= 37 ) {
-    cnt += 1
-}
-
 
 console.log( cnt >= 2 ? 'E' : 'N' )
