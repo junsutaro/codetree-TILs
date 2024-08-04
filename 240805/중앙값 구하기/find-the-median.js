421fed47a1c3ba4623b28e9c16a1782f8b104db5
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-05 00:36:28 +0900

    Title: 중앙값 구하기, Time: 171ms, Memory: 14MB, Status: Passed - CodeTree

--- 240805/중앙값 구하기/find-the-median.js
+++ 240805/중앙값 구하기/find-the-median.js
@@ -5,18 +5,12 @@ const [a, b, c] = input.split(' ').map(Number);
 
 let mid;
 
-if ( a > b ) {
-    if ( b > c ) {
-        mid = b
-    } else if ( c > a ) {
-        mid = a
-    }
-} else if ( a < b ) {
-    if ( b < c ) {
-        mid = b;
-    } else if ( c < a ) {
-        mid = a
-    }
+if ((a >= b && a <= c) || (a >= c && a <= b)) {
+    mid = a;
+} else if ((b >= a && b <= c) || (b >= c && b <= a)) {
+    mid = b;
+} else {
+    mid = c;
 }
 
 console.log(mid)
