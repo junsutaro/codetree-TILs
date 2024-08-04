28223c81ca62d42c1c56041e0ebdabfaa5a17ccc
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-04 11:10:50 +0900

    Title: 큰 수에서 빼기, Time: 236ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240804/큰 수에서 빼기/subtract-from-large-number.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+const [a, b] = input.split(' ').map(Number);
+
+if (a >= b) {
+    console.log(a - b)
+} else {
+    console.log(b - a)
+}
