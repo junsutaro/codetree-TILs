fab3822f5ee6a20b22c74d5174180700e8219f8f
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-05 00:20:01 +0900

    Title: 윤년인가, Time: 218ms, Memory: 14MB, Status: Passed - CodeTree

--- 240805/윤년인가/is-leap-year.js
+++ 240805/윤년인가/is-leap-year.js
@@ -4,3 +4,5 @@ const input = fs.readFileSync(0).toString().trim();
 const year = Number(input);
 
 const isLeapYear = (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0) ;
+
+console.log(isLeapYear)
