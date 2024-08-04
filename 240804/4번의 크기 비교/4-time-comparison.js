d52f06fdf6f8c09bca063ef1a7a4d01c126cdba9
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-04 22:49:44 +0900

    Title: 4번의 크기 비교, Time: 234ms, Memory: 47MB, Status: Passed - CodeTree

--- 240804/4번의 크기 비교/4-time-comparison.js
+++ 240804/4번의 크기 비교/4-time-comparison.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
-const [a, arr] = input.split('\n')
-const [b, c, d, e] = arr.split(' ')
+const arr = input.split('\n')
+const a = Number(arr[0])
+const [b, c, d, e] = arr[1].split(' ').map(Number)
 
 console.log(a > b ? 1 : 0)
 console.log(a > c ? 1 : 0)
