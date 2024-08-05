10a58d38bd0ae36893ac20c0a2724a3d7617ef35
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-06 07:33:25 +0900

    Title: 별표 출력하기 12, Time: 234ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240806/별표 출력하기 12/print-star-12.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+// const n = Number(input);
+
+const n = 7;
+let stars;
+
+for (let i = 0; i < n ; i ++ ) {
+    stars = ''
+    for ( let j = 0 ; j < n ; j ++ ) {
+        if ( i === 0 ) { 
+            stars += '* '
+        }
+
+        if (( i >= 1 ) && ( j % 2 === 0 )) {
+            stars += '  '
+        }
+
+        if (( i >= 1 ) && ( j % 2 === 1 ) && ( j < i )) {
+            stars += '  '
+        }
+        if (( i >= 1 ) && ( j % 2 === 1 ) && ( j >= i )) {
+            stars += '* '
+        }
+    }
+    console.log(stars)
+    
+}
