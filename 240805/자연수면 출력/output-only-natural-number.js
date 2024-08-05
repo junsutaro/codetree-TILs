b99cf494992d5325cc6f349f0e1c9f72e5194fc9
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-05 11:20:34 +0900

    Title: 자연수면 출력, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

--- 240805/자연수면 출력/output-only-natural-number.js
+++ 240805/자연수면 출력/output-only-natural-number.js
@@ -8,6 +8,7 @@ if ( Number.isInteger(a) && a > 0 ) {
     for ( let i = 0; i < b ; i ++ ) {
         output += a
     }   
+    console.log(output.trim())
+} else if ( a <= 0 ) {
+    console.log(0)
 }
-
-console.log(output.trim())
