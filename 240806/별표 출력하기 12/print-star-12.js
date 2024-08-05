bc6d0df4791efbff7c6c794a48723e6f172dd2ed
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-06 07:36:45 +0900

    Title: 별표 출력하기 12, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240806/별표 출력하기 12/print-star-12.js
+++ 240806/별표 출력하기 12/print-star-12.js
@@ -3,25 +3,28 @@ const input = fs.readFileSync(0).toString().trim();
 
 const n = Number(input);
 
-for (let i = 0; i < n; i++) {
-    let stars = '';
-    for (let j = 0; j < n; j++) {
-        if (i === 0) {
-            // 첫 번째 줄: n개의 '*'을 출력
-            stars += '* ';
-        } else {
-            // 두 번째 줄부터: 규칙에 맞게 별표를 출력
-            if (j < i) {
-                // 앞부분 공백
-                stars += '  ';
-            } else if (j % 2 === 1) {
-                // 홀수 인덱스에 '*' 추가
-                stars += '* ';
-            } else {
-                // 짝수 인덱스는 공백
-                stars += '  ';
+// const n = 7;
+let stars;
+
+for (let i = 0; i < n ; i ++ ) {
+    stars = ''
+    for ( let j = 0 ; j < n ; j ++ ) {
+        if ( i === 0 ) {    
+            stars += '* '
+        }
+        else {
+            if (( j < i )) {
+                stars += '  '
             }
+            else if (( j % 2 === 0 )) {
+                stars += '  '
+            }
+            else {
+                stars += '* '
+            }
+
         }
     }
-    console.log(stars);
+    console.log(stars)
+    
 }
