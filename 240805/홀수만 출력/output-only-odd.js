58f3e7d18cce8eb31f9b89dfd9f4a4a844d61181
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-05 11:04:16 +0900

    Title: 홀수만 출력, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240805/홀수만 출력/output-only-odd.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+const [a, b] = input.split(' ').map(Number);
+let output = ' '
+
+for ( let i = a ; i <= b ; i += 2 ) {
+    output += i + ' ';
+}
+
+console.log(output.trim())
