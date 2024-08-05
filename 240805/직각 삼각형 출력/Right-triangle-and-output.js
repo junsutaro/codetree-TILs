d15140c7d5853f20fa0b5839b8961aab8d59dff8
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-05 12:47:02 +0900

    Title: 직각 삼각형 출력, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240805/직각 삼각형 출력/Right-triangle-and-output.js
+++ 240805/직각 삼각형 출력/Right-triangle-and-output.js
@@ -3,10 +3,7 @@ const input = fs.readFileSync(0).toString().trim();
 
 const n = Number(input);
 
-for ( let i = 1 ; i <= n ; i ++ ) {
-    let stars = ''
-    for ( let j = 1 ; j < i*2 ; j++ ) {
-        stars += '*'
-    }
-    console.log(stars)
+for ( let i = 1 ; i < 2*n ; i += 2 ) {
+
+    console.log('*'.repeat(i))
 }
