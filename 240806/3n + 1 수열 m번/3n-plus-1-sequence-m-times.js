c0a801db4e2b4afee69db9bfdb24d2bccb8b91d3
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-06 09:29:55 +0900

    Title: 3n + 1 수열 m번, Time: 212ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240806/3n + 1 수열 m번/3n-plus-1-sequence-m-times.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const m = Number(input[0]);
+for ( let i = 0; i < m ; i ++ ) {
+    let n = input[i+1];
+    let cnt = 0;
+
+    while ( n !== 1 ) {
+        if ( n % 2 === 0 ) {
+            n = n / 2
+            cnt +=1
+        }
+        else {
+            n = n * 3 + 1
+            cnt += 1
+        }
+    }
+    console.log(cnt)
+
+}
