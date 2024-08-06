74aa180e83fceaf4693ebf4b9b1703b134486849
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-07 05:55:03 +0900

    Title: 연속부분수열일까, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240807/연속부분수열일까/contiguous-array-or-not.js
+++ 240807/연속부분수열일까/contiguous-array-or-not.js
@@ -5,22 +5,21 @@ const [n1, n2] = input[0].split(' ').map(Number);
 const A = input[1].split(' ').map(Number);
 const B = input[2].split(' ').map(Number);
 
-function isSubsequence(A, B) {
-    const n1 = A.length;
-    const n2 = B.length;
-    
-    for (let i = 0; i <= n1 - n2; i++) {
+function isSubsequence ( A, B ) {
+    for ( let i = 0 ; i <= n1 - n2 ; i ++ ) {
         let isMatch = true;
-        for (let j = 0; j < n2; j++) {
-            if (A[i + j] !== B[j]) {
+        
+        for ( let j = 0 ; j < n2 ; j ++ ) {
+            if ( A[i + j] !== B[j] ) {
                 isMatch = false;
                 break;
             }
         }
-        if (isMatch) return true;
+
+        return isMatch;
     }
     return false;
 }
 
-const result = isSubsequence(A, B) ? 'Yes' : 'No';
-console.log(result);
+const result = isSubsequence(A, B);
+console.log(result ? 'Yes' : 'No')
