ac4ff04d37027b38d313bde7642d6ff6e7c4cba6
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-06 09:55:11 +0900

    Title: 소수 구하기, Time: 229ms, Memory: 14MB, Status: Passed - CodeTree

--- 240806/소수 구하기/get-prime.js
+++ 240806/소수 구하기/get-prime.js
@@ -5,17 +5,21 @@ const n = Number(input)
 let ans = ''
 
 if ( n >= 2 ) {
-    ans += 2 + ' '
+    ans += '2 '
 }
-for ( let i = 1 ; i <= n ; i ++ ) {
-    if ( n === 1 ) {
-        break
-    }
-    for ( let j = 2 ; j < i ; j ++ ) {
+
+for ( let i = 3 ; i <= n ; i += 2 ) {
+    let isPrime = true;
+
+    for ( let j = 2 ; j * j <= i ; j ++ ) {
         if ( i % j === 0 ) {
+            isPrime = false;
             break;
         }
-        ans += i + ' ';
+    }
+
+    if (isPrime) {
+        ans += i + ' '
     }
 }
 
