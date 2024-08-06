17d3ff246030e2679c5804eec8ba526e9ac66894
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-07 05:09:19 +0900

    Title: 배열 놀이, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240807/배열 놀이/play-with-array.js
@@ -0,0 +1,32 @@
+function quiz ( arr ) {
+    if ( arr[0] === 1 ) {
+        console.log(numbers[0]);
+    }
+    else if ( arr[0] === 2 ) {
+        let index = 0;
+        for ( let i = 0 ; i < n ; i ++ ) {
+            if (numbers[i] === arr[1]) {
+                index = i;
+                break;
+            }
+        }
+        console.log(index + 1);
+    }
+    else {
+        let printedArr = ''
+        for ( let i = arr[1] - 1 ; i < arr[2] ; i ++ ) {
+            printedArr += numbers[i] + ' '
+        }
+        console.log(printedArr);
+    }
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, q] = input[0].split(' ').map(Number);
+const numbers = input[1].split(' ').map(Number);
+
+for ( let i = 2 ; i < q + 2 ; i ++ ) {
+    quiz(input[i].split(' ').map(Number))
+}
