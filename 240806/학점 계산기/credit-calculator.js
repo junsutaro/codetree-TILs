a4983322f63ec301cde245bb2500010d76c01cbd
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-06 10:40:29 +0900

    Title: 학점 계산기, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240806/학점 계산기/credit-calculator.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const scores = input[1].split(' ').map(Number);
+
+const sumVal = scores.reduce((sum, score) => sum + score, 0);
+const avgVal = (sumVal / n).toFixed(1);
+
+const grade = avgVal >= 4.0 ? 'Perfect' :
+              avgVal >= 3.0 ? 'Good' :
+              'Poor'
+
+
+console.log(avgVal);
+console.log(grade);
