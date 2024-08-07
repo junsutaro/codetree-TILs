0bd3ea8292b10f8fa2261207a41825d6eca13cfc
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-07 13:33:52 +0900

    Title: 대각선으로 숫자 채우기, Time: 262ms, Memory: 15MB, Status: Passed - CodeTree

--- 240807/대각선으로 숫자 채우기/diagonal-numbering.js
+++ 240807/대각선으로 숫자 채우기/diagonal-numbering.js
@@ -1,30 +1,21 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 const [n, m] = input.split(' ').map(Number);
-// const [n, m] = [3, 4]
 
 let arr2d = Array.from({ length: n }, () => Array(m).fill(0));
 let temp = 1;
 
-for ( let k = 0 ; k < n + m - 1 ; k ++ ) {
-    if ( k < n ) {
-        for ( let i = 0 ; i <= k ; i ++ ) {
-            if ( k - i < m ) {
-                arr2d[i][k-i] = temp;
-                temp += 1
-            }
-        }
-    } 
-    else {
-        for ( let i = k - m + 1 ; i < n ; i ++ ) {
-            if (i >= 0 && i < n && (k - i) >= 0 && (k - i) < m) {
-                arr2d[i][k-i] = temp;
-                temp += 1
-            }
+for (let k = 0; k < n + m - 1; k++) {
+    let startRow = Math.max(0, k - m + 1);
+    let endRow = Math.min(n - 1, k);
+    for (let i = startRow; i <= endRow; i++) {
+        let j = k - i;
+        if (j < m) {
+            arr2d[i][j] = temp++;
         }
     }
 }
 
-for ( let elem of arr2d ) {
-    console.log(elem.join(' '))
+for (let elem of arr2d) {
+    console.log(elem.join(' '));
 }
