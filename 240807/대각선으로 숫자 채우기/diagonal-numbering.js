7438b64f3bb9268fc680255b9fb6632f65615d0d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-07 13:31:28 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240807/대각선으로 숫자 채우기/diagonal-numbering.js
+++ 240807/대각선으로 숫자 채우기/diagonal-numbering.js
@@ -1,24 +1,30 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 const [n, m] = input.split(' ').map(Number);
+// const [n, m] = [3, 4]
 
 let arr2d = Array.from({ length: n }, () => Array(m).fill(0));
 let temp = 1;
 
-// Iterate over all possible diagonals
-for (let k = 0; k < n + m - 1; k++) {
-    if (k < n) {
-        for (let i = k, j = 0; i >= 0 && j < m; i--, j++) {
-            arr2d[i][j] = temp++;
+for ( let k = 0 ; k < n + m - 1 ; k ++ ) {
+    if ( k < n ) {
+        for ( let i = 0 ; i <= k ; i ++ ) {
+            if ( k - i < m ) {
+                arr2d[i][k-i] = temp;
+                temp += 1
+            }
         }
-    } else {
-        for (let i = n - 1, j = k - n + 1; i >= 0 && j < m; i--, j++) {
-            arr2d[i][j] = temp++;
+    } 
+    else {
+        for ( let i = k - m + 1 ; i < n ; i ++ ) {
+            if ( 0 <= i < n && 0 <= k-i < m ) {
+                arr2d[i][k-i] = temp;
+                temp += 1
+            }
         }
     }
 }
 
-// Output the 2D array
-for (let row of arr2d) {
-    console.log(row.join(' '));
+for ( let elem of arr2d ) {
+    console.log(elem.join(' '))
 }
