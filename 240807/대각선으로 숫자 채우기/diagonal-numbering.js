507716c0ae4e82d12c1f5031e02391342f07ccd4
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-07 12:49:39 +0900

    Title: 대각선으로 숫자 채우기, Time: 228ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240807/대각선으로 숫자 채우기/diagonal-numbering.js
+++ 240807/대각선으로 숫자 채우기/diagonal-numbering.js
@@ -7,19 +7,13 @@ let temp = 1;
 
 // Iterate over all possible diagonals
 for (let k = 0; k < n + m - 1; k++) {
-    if (k < m) {
-        for (let i = 0; i <= k && i < n; i++) {
-            let j = k - i;
-            if (j < m) {
-                arr2d[i][j] = temp++;
-            }
+    if (k < n) {
+        for (let i = k, j = 0; i >= 0 && j < m; i--, j++) {
+            arr2d[i][j] = temp++;
         }
     } else {
-        for (let j = m - 1; j >= k - n + 1; j--) {
-            let i = k - j;
-            if (i < n) {
-                arr2d[i][j] = temp++;
-            }
+        for (let i = n - 1, j = k - n + 1; i >= 0 && j < m; i--, j++) {
+            arr2d[i][j] = temp++;
         }
     }
 }
