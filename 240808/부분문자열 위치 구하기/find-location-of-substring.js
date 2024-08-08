e9ad4ced7c125864ad1757c036d837f03b2f7004
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-08 12:24:30 +0900

    Title: 부분문자열 위치 구하기, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240808/부분문자열 위치 구하기/find-location-of-substring.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const mainString = input[0];
+const subString = input[1];
+
+let index = -1;
+
+for ( let i = 0; i < mainString.length - subString.length ; i ++ ) {
+    let match = true;
+    for ( let j = 0 ; j < subString.length ; j ++ ) {
+        if ( mainString[i + j] !== subString[j] ) {
+            match = false;
+            break;
+        }
+    }
+    if ( match ) {
+        index = i;
+        break;
+    }
+}
+
+console.log(index)
