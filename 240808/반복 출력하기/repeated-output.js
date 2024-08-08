95c180edc3ee508bacd8e924945e1e44ac9afea7
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-08 21:06:37 +0900

    Title: 반복 출력하기, Time: 202ms, Memory: 47MB, Status: Passed - CodeTree

--- 240808/반복 출력하기/repeated-output.js
+++ 240808/반복 출력하기/repeated-output.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
 const n = Number(input)
-let res = ''
 
 
 for ( let i = 0 ; i < n ; i ++ ) {
-    res += `12345^&*()_\n`
+    console.log(`12345^&*()_`)
 }
-
-console.log(res)
