0b212322e404fe71ddbfb226b3355a73ec18b706
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-08 09:50:56 +0900

    Title: 더 긴 문자열, Time: 231ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240808/더 긴 문자열/longer-string.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const [str1, str2] = input.split(' ');
+
+if ( str1.length > str2.length ) {
+    console.log(str1, str1.length);
+} else {
+    console.log(str2, str2.length)
+}
