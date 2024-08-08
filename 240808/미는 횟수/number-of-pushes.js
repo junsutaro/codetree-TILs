b3cc247f9e47d25db7eb1521d93b85f8558fead4
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-08 20:49:44 +0900

    Title: 미는 횟수, Time: 197ms, Memory: 47MB, Status: Passed - CodeTree

--- 240808/미는 횟수/number-of-pushes.js
+++ 240808/미는 횟수/number-of-pushes.js
@@ -1,20 +1,16 @@
-const fs = require("fs");
+const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const A = input[0];
 const B = input[1];
-const len = A.length;
 
-let result = -1;
-
-// 문자열을 n번 밀어보면서 비교
-for (let n = 1; n < len; n++) {
-    // 문자열 A를 n번 우측으로 밀어봄
-    let rotated = A.slice(-n) + A.slice(0, -n);
-    if (rotated === B) {
-        result = n;
+let ans = -1;
+for ( let i = 1 ; i < A.length ; i ++ ) {
+    let changedA = A.slice(-i) + A.slice(0, A.length - i)
+    if ( changedA === B ) {
+        ans = i;
         break;
     }
 }
 
-console.log(result);
+console.log(ans)
