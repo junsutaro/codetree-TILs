dad9cf4a48fdfe6b7e5ae2b91fb8c334e894b639
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-09 10:54:59 +0900

    Title: 함수를 이용한 윤년 판별, Time: 223ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240809/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
+++ 240809/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -7,7 +7,8 @@ const year = Number(input);
 function isLunarYear(year) {
     if ( year % 100 === 0 && year % 400 === 0 ) { 
         return false
-    } else if ( year % 4 === 0 ) { 
+    } 
+    if ( year % 4 === 0 ) { 
         return true
     }
 }
