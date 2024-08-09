5e28217268b745af9ec0fa3f958b3ebe196a74d3
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-09 10:09:26 +0900

    Title: 숫자로 이루어진 사각형, Time: 241ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240809/숫자로 이루어진 사각형/rectangle-with-a-number.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+N = Number(input);
+
+let ans = '';
+
+for ( let i = 0; i < N ; i ++ ) {
+    for ( let j = 0 ; j < 4 ; j ++ ) {
+        ans += `${( i * 4 + j ) % 9 + 1} ` 
+    }
+    ans += '\n'
+}
+
+console.log(ans)
