63bdab13e0709008bf929af1acdc300728cb2279
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-09 10:33:10 +0900

    Title: 함수를 이용한 369 게임, Time: 227ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 240809/함수를 이용한 369 게임/369-games-using-functions.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+
+
+function isContain369(n) {
+    while ( n > 0 ) {
+        const digit = n % 10 ;
+        if ( digit === 3 || digit === 6 || digit === 9 ) {
+            return true
+        }
+        n = parseInt( n / 10 )
+    }
+}
+
+function isMagicNumber(n) {
+    if ( n % 3 || isContain369(n) ) {
+        return true;
+    }
+}
+
+const [a, b] = input.split(' ').map(Number);
+let ans = 0;
+
+for ( let i = a ; i < b ; i ++ ) {
+    if ( isMagicNumber(i) === true ) {
+        ans += 1
+    }
+}
+
+console.log(ans)
