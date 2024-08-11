063db0c3c6bd1ef7572dcdecc3525741bc4544a0
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-11 17:41:06 +0900

    Title: 이상한 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240811/이상한 수열/a-strange-sequence.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+const N = Number(input);
+
+
+function findAns(n) {
+    if ( n < 1 ) {
+        console.log('plz use more than 1');
+        return;
+    }
+    if ( n === 1 ) {
+        return 1;
+    }
+
+    if ( n === 2 ) {
+        return 2;
+    }
+
+    return findAns(n-1) + findAns(parseInt(n / 3))
+}
+
+const ans = findAns(n)
+
+console.log(ans)
