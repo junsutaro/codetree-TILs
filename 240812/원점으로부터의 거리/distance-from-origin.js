bfa82019a5c390c9634727d29d504b6df4ba006d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 22:07:13 +0900

    Title: 원점으로부터의 거리, Time: 214ms, Memory: 16MB, Status: Passed - CodeTree

--- 240812/원점으로부터의 거리/distance-from-origin.js
+++ 240812/원점으로부터의 거리/distance-from-origin.js
@@ -1,1 +1,21 @@
-function cmp(a, b)
+function distance([a, b]) {
+    return Math.abs(a) + Math.abs(b);
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const N = Number(input[0]);
+
+const points = []
+for ( let i = 1; i <= N ; i ++ ) {
+    points.push([distance(input[i].split(' ')), i])
+}
+
+points.sort((a, b) => {
+    return a[0] - b[0];
+})
+
+for (let i = 0 ; i < N ; i ++ ) {
+    console.log(points[i][1])
+}
