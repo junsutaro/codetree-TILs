f8b0e32807ef092e4bec09f9c3643b8f9e832c96
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 22:18:20 +0900

    Title: 원점으로부터의 거리, Time: 207ms, Memory: 16MB, Status: Passed - CodeTree

--- 240812/원점으로부터의 거리/distance-from-origin.js
+++ 240812/원점으로부터의 거리/distance-from-origin.js
@@ -1,21 +1,40 @@
-function distance([a, b]) {
-    return Math.abs(a) + Math.abs(b);
+class Point {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+    }
+
+    getManhattanDistance() {
+        return Math.abs(this.x) + Math.abs(this.y);
+    }
 }
+
+function cmp(a, b) {
+    distA = a.getManhattanDistance();
+    distB = b.getManhattanDistance();
+
+    if (distA !== distB) {
+        return distA - distB;
+    }
+
+    return a.index - b.index;
+}
+
+let points = []
 
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
-
 const N = Number(input[0]);
 
-const points = []
-for ( let i = 1; i <= N ; i ++ ) {
-    points.push([distance(input[i].split(' ')), i])
+for ( let i = 1 ; i <= N ; i ++ ) {
+    const [x, y] = input[i].split(' ').map(Number);
+
+    points.push(new Point(x, y, i))
 }
 
-points.sort((a, b) => {
-    return a[0] - b[0];
-})
+points.sort(cmp);
 
-for (let i = 0 ; i < N ; i ++ ) {
-    console.log(points[i][1])
+for (let point of points) {
+    console.log(point.index);
 }
