b948226e16f82549230ef54e3fd36483ac6a98f9
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 14:57:02 +0900

    Title: 줄 세우기, Time: 247ms, Memory: 15MB, Status: Passed - CodeTree

--- 240812/줄 세우기/line-up-students.js
+++ 240812/줄 세우기/line-up-students.js
@@ -18,13 +18,18 @@ for (let i = 1; i <= n; i++) {
     students.push(new Student(height, weight, i));
 }
 
-// 정렬: 키 내림차순, 몸무게 내림차순, 입력된 순서 오름차순
-students.sort((a, b) => 
-    b.height - a.height || // 키 내림차순
-    b.weight - a.weight || // 몸무게 내림차순
-    a.index - b.index      // 입력된 순서 오름차순
-);
 
+function cmp(a, b) {
+    if ( b.height !== a.height ) {
+        return b.height - a.height
+    }
+    if ( b.weight !== a.weight ) {
+        return b.weight - a.weight 
+    }
+    return a.index - b.index
+}
+// 정렬: 키 내림차순, 몸무게 내림차순, 입력된 순서 오름차순
+students.sort(cmp)
 // 출력
 for (let student of students) {
     console.log(`${student.height} ${student.weight} ${student.index}`);
