426f2dca6ac79c4119ccd93b202c8b76903ef926
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 11:27:13 +0900

    Title: 두 개의 동일한 수열, Time: 244ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240812/두 개의 동일한 수열/two-equal-series.js
+++ 240812/두 개의 동일한 수열/two-equal-series.js
@@ -2,25 +2,26 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-let A = input[1].split(' ').map(Number);
-let B = input[2].split(' ').map(Number);
+let A = input[1].split(' ').map(Number)
+let B = input[2].split(' ').map(Number)
 
-// 먼저 두 배열의 길이가 동일한지 확인합니다.
-if (A.length !== n || B.length !== n) {
-    console.log('No');
-} else {
-    // 두 배열을 정렬합니다.
-    A.sort((prev, cur) => prev - cur);
-    B.sort((prev, cur) => prev - cur);
+A.sort((prev, cur) => {
+    return prev - cur;
+})
+B.sort((prev, cur) => {
+    return prev - cur;
+})
 
-    // 배열이 동일한지 확인합니다.
-    let ans = 'Yes';
-    for (let i = 0; i < n; i++) {
-        if (A[i] !== B[i]) {
-            ans = 'No';
-            break;
-        }
-    }
 
-    console.log(ans);
-}
+// let ans = 'Yes'
+// for ( let i = 0 ; i < n ; i ++ ) {
+//     console.log(`A = ${A[i]}`)
+//     console.log(`B = ${B[i]}`)
+//     if (A[i] !== B[i]) {
+//         ans = 'No';
+//         break;
+//     }
+// }
+
+const ans = A.join(' ') === B.join(' ') ? 'Yes' : 'No';
+console.log(ans);
