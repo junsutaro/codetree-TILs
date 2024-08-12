f4029231aa45770712e326b6f28bf20a66912f1f
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 11:40:56 +0900

    Title: 두 개의 동일한 수열, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240812/두 개의 동일한 수열/two-equal-series.js
+++ 240812/두 개의 동일한 수열/two-equal-series.js
@@ -1,25 +1,21 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-
-console.log("Raw Input A:", input[1]);
-
-
+// 배열을 읽어오는 부분
 const n = Number(input[0]);
-let A = input[1].split(' ').trim()
-let B = input[2].split(' ').trim()
+let A = input[1].split(' ').map(Number);  // 문자열을 숫자로 변환
+let B = input[2].split(' ').map(Number);  // 문자열을 숫자로 변환
 
-A.sort((prev, cur) => {
-    return prev - cur;
-})
-B.sort((prev, cur) => {
-    return prev - cur;
-})
+// 배열 정렬
+A.sort((prev, cur) => prev - cur);
+B.sort((prev, cur) => prev - cur);
 
-
+// 배열 출력
+console.log("Raw Input A:", input[1]);
 console.log("Sorted A:", A.join(' '));
 console.log("Sorted B:", B.join(' '));
 
+// 배열 비교
 let isEqual = true;
 for (let i = 0; i < n; i++) {
     if (A[i] !== B[i]) {
