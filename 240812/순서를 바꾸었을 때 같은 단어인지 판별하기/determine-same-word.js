639c551be072d8775dd1a6a429c3b8f9c9261873
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 12:16:28 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 243ms, Memory: 22MB, Status: Passed - CodeTree

--- 240812/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
+++ 240812/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
@@ -7,14 +7,16 @@ let str2 = input[1];
 let arr1 = [...str1];
 let arr2 = [...str2];
 
-arr1.sort();
-arr2.sort();
+
 
 let ans = 'Yes';
 
 if ( arr1.length !== arr2.length ) {
     ans = 'No'
 } else {
+    arr1.sort();
+    arr2.sort();
+
     for ( let i = 0; i < arr1.length ; i ++ ) {
         if ( arr1[i] !== arr2[i] ) {
             ans = 'No';
