003a2c6ae1a622da68dd7a8744ad293a970f50fe
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-12 12:32:48 +0900

    Title: k번째로 신기한 문자열, Time: 257ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240812/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k, T] = input[0].split(' ');
+
+
+let filteredWords = [];
+
+for ( let i = 1; i <= n ; i ++ ) {
+    if (isStartWithT(input[i], T)) {
+        filteredWords.push(input[i])
+    }
+}  
+
+
+function isStartWithT(str, pattern) {
+    for ( let i = 0; i < pattern.length ; i ++ ) {
+        if (str[i] !== pattern[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(filteredWords.sort()[k-1])
