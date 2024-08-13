64d524701830806e45a6aa6ec24ab906abf1cae6
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-13 10:49:59 +0900

    Title: DateTime to DateTime, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

--- 240813/DateTime to DateTime/datetime-to-datetime.js
+++ 240813/DateTime to DateTime/datetime-to-datetime.js
@@ -5,18 +5,24 @@ const input = fs.readFileSync(0).toString().trim();
 const [ a, b, c ] = input.split(' ').map(Number);
 
 function calculateMinutes(a, b, c) {
+    const MINUTES_IN_A_DAY = 24 * 60; // 하루에 해당하는 총 분
 
-    const startTotalMinutes = 11 * 60 * 24 + 11 * 60 + 11;
-    const endTotalMinutes = a * 60 * 24 + b  * 60 + c;
+    const startDay = 11;
+    const startHour = 11;
+    const startMinute = 11;
+
+    if (a < startDay || 
+    (a === startDay && b < startHour) || 
+    (a === startDay && b === startHour && c < startMinute)) {
+        return -1;
+    }
+    
+    const startTotalMinutes = (startDay * MINUTES_IN_A_DAY) + (startHour * 60) + startMinute;
+    const endTotalMinutes = (a * MINUTES_IN_A_DAY) + (b * 60) + c;
 
     const elapsedTime = endTotalMinutes - startTotalMinutes;
-    if ( elapsedTime >= 0 ) {
-        return elapsedTime;
-    } else {
-        return -1
-    }
+    return elapsedTime;
 }
 
 const result = calculateMinutes(a, b, c)
-
 console.log(result)
