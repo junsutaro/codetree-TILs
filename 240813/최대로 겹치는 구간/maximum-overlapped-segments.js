2762e6d776bb27e488aed69299f8ec4d9821d217
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-13 12:52:54 +0900

    Title: 최대로 겹치는 구간, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240813/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 첫 번째 줄에서 N과 K를 읽어옵니다.
+const n = Number(input[0]);
+const arr = []
+
+for ( let i = 1 ; i <= n ; i ++ ) {
+    const [start, end] = input[i].split(' ').map(Number);
+    for ( j = start ; j < end ; j ++ ) {
+        if ( arr[j] ) {
+            arr[j] += 1
+        } else {
+            arr[j] = 1;
+        }
+    }
+}
+
+let ans = 0;
+for ( let elem of arr ) {
+    if ( elem === 2 ) {
+        ans += 1
+    }
+}
+
+console.log(ans)
