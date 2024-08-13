5a12832609c3abbcd0208d54923bb253e08008f5
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-13 13:46:58 +0900

    Title: 왔다 갔던 구역 2, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240813/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 240813/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -2,35 +2,32 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const OFFSET = 100;
 const arr = Array(201).fill(0);
 
-let index = OFFSET;
+let index = 100;
 
 for ( let i = 1; i <= n ; i ++ ) {
     const [x, dir] = input[i].split(' ');
     const dist = Number(x);
 
-    let [start, end] = [0, 0]
+    let start = index;
+    let end = ( dir === 'R' ) ? index + dist : index - dist
 
-    if ( dir === 'R' ) {
-        start = index;
-        end = index + dist;
-        index = index + dist
-    } else {
-        start = index - dist ;
-        end = index 
-        index = index - dist 
+    if ( start > end ) {
+        [start, end] = [end, start] 
     }
 
     for ( let j = start ; j < end ; j ++ ) {
         arr[j] += 1;
     }
 
+    index = ( dir === 'R' ) ? index + dist : index - dist;
+
 }
+
 let cnt = 0;
 
-for ( let elem of arr ) {
+for ( let elem of arr ) {   
     if ( elem > 1 ) {
         cnt += 1
     }
