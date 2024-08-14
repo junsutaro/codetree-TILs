70d634cc06fbe0d8c046ad60dd8a08f15cfc68af
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-14 14:01:45 +0900

    Title: 연속되는 수 2, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240814/연속되는 수 2/continuous-number2.js
+++ 240814/연속되는 수 2/continuous-number2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-let maxVal = 0;
+let maxVal = 1;
 let count = 1;
 for ( let i = 1 ; i <= n-1 ; i ++) {
     if ( input[i] === input[i+1] ) {
