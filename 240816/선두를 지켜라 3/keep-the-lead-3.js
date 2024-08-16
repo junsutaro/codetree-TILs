6f3d1eaceb6a77757a2576a04217905b9e707555
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-16 11:37:05 +0900

    Title: 선두를 지켜라 3, Time: 276ms, Memory: 49MB, Status: Wrong Answer - CodeTree

---
+++ 240816/선두를 지켜라 3/keep-the-lead-3.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+const MAX_POS = 1000000;
+const posA = Array(MAX_POS + 1).fill(0);
+const posB = Array(MAX_POS + 1).fill(0);
+
+let timeA = 0;
+for ( let i = 1; i <= n; i ++ ) {
+    const [vel, time] = input[i].split(' ').map(Number);
+    for ( let i = 0; i < time ; i ++ ) {
+        timeA += 1;
+        posA[timeA] = posA[timeA - 1] + vel;
+    }
+}
+
+let timeB = 0;
+for ( let i = n + 1; i <= n + m; i ++ ) {
+    const [vel, time] = input[i].split(' ').map(Number);
+    for ( let i = 0; i < time ; i ++ ) {
+        timeB += 1;
+        posB[timeB] = posB[timeB - 1] + vel;
+    }
+}
+
+let first = ''
+let temp = ''
+
+let maxTime = timeA > timeB ? timeA : timeB;
+
+count = 0;
+
+for ( let i = 1 ; i < maxTime ; i ++ ) {
+    if ( posA[i] - posB[i] ) {
+        temp = 'A';
+    } else if ( posA[i] === posB[i] ) {
+        temp = 'AB';
+    } else {
+        temp = 'B'
+    }
+
+    if ( temp !== first ) {
+        count += 1;
+        first = temp;
+    }
+}
+
+console.log(count)
