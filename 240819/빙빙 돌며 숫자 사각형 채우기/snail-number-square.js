b2253606c87ae40ad0ebf57e07341fe1d398ebee
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-19 17:01:25 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 258ms, Memory: 47MB, Status: Passed - CodeTree

--- 240819/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
+++ 240819/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
@@ -7,7 +7,7 @@ const grid = [...Array(n)].map(() => Array(m).fill(0));
 
 function isRange(x, y) {
     return x >= 0 && x < n && y >= 0 && y < m;
-} 
+}
 
 const dx = [0, 1, 0, -1];
 const dy = [1, 0, -1, 0];
@@ -17,27 +17,26 @@ let x = 0, y = 0;
 let temp = 1;
 grid[x][y] = temp;
 
-for ( let i = 0 ; i < n * m - 1; i ++ ) {
-    temp += 1
+for (let i = 0; i < n * m - 1; i++) {
+    temp += 1;
     let nx = x + dx[curDir];
     let ny = y + dy[curDir];
 
     if (!isRange(nx, ny) || grid[nx][ny] !== 0) {
         curDir = (curDir + 1) % 4;
-        nx = x + dx[curDir];
+        nx = x + dx[curDir];  
         ny = y + dy[curDir];
     }
-    
 
     x = nx;
     y = ny;
     grid[x][y] = temp;
 }
 
-for ( let i = 0; i < m ; i ++ ) {
-    let print = ''
-    for ( let j = 0 ; j < n ; j ++ ) {
-        print += `${grid[i][j]} `
+for (let i = 0; i < n; i++) {
+    let print = '';
+    for (let j = 0; j < m; j++) { 
+        print += `${grid[i][j]} `;
     }
-    console.log(print);
+    console.log(print.trim());
 }
