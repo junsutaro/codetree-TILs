81c5ce571717b4118eb5e46195f3bc84eb25a624
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-22 15:09:02 +0900

    Title: 일렬로 서있는 소 2, Time: 240ms, Memory: 47MB, Status: Passed - CodeTree

--- 240822/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
+++ 240822/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
@@ -9,7 +9,7 @@ let count = 0;
 for ( let i = 0 ; i < n - 2 ; i ++ ) {
     for ( let j = i + 1 ; j < n - 1 ; j ++ ) {
         for ( let k = j + 1 ; k < n ; k ++ ) {
-            if (numbers[i] < numbers[j] && numbers[j] < numbers[k]) {
+            if (numbers[i] <= numbers[j] && numbers[j] <= numbers[k]) {
                 count += 1;
             }
         }
