b0ae792dbf9fc2046149dbfcbb139c824eba400e
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-22 16:30:28 +0900

    Title: 이상한 진수 2, Time: 309ms, Memory: 14MB, Status: Passed - CodeTree

--- 240822/이상한 진수 2/awkward-digits-2.js
+++ 240822/이상한 진수 2/awkward-digits-2.js
@@ -4,7 +4,6 @@ const input = fs.readFileSync(0).toString().trim();
 let maxVal = 0;
 const numbers = input.split('').map(Number);
 
-
 function calDigit(numbers) {
     let val = 0;
     for ( let i = 0 ; i < numbers.length ; i ++ ) {
@@ -15,21 +14,12 @@ function calDigit(numbers) {
 }
 
 for ( let i = 0; i < numbers.length ; i ++ ) {
-    if ( numbers[i] === 0 ) {
-        numbers[i] = 1;
+        numbers[i] = 1 - numbers[i];
         const tempVal = calDigit(numbers);
         if ( tempVal > maxVal ) {
             maxVal = tempVal
         }
-        numbers[i] = 0;
-    } else {
-        numbers[i] = 0;
-        const tempVal = calDigit(numbers);
-        if ( tempVal > maxVal ) {
-            maxVal = tempVal
-        }
-        numbers[i] = 1;
-    }
+        numbers[i] = 1 - numbers[i];
 }
 
 console.log(maxVal)
