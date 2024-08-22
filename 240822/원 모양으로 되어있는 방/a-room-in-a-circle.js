7366e2e01afcbb57218e44ca225a5cac8fc446ac
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-22 17:16:07 +0900

    Title: 원 모양으로 되어있는 방, Time: 175ms, Memory: 47MB, Status: Passed - CodeTree

--- 240822/원 모양으로 되어있는 방/a-room-in-a-circle.js
+++ 240822/원 모양으로 되어있는 방/a-room-in-a-circle.js
@@ -4,18 +4,17 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 n = Number(input[0]);
 const people = input.slice(1).map(Number);
 
-console.log(people)
 const MAX_VAL = Number.MAX_SAFE_INTEGER;
 
 
-let minVal = 0;
+let minVal = MAX_VAL;
 for ( let i = 0 ; i < n ; i ++ ) {
     let sumVal = 0;
     for ( let j = 0 ; j < n ; j ++ ) {
         sumVal += ( people[(i + j) % n] * j ); // i가 시작점 ,j는 칸 수
     }
-    // console.log(sumVal);
-    minVal = Math.min((minVal, sumVal))
+    minVal = Math.min(minVal, sumVal)
+    // console.log(sumVal, minVal);
 }
 
 console.log(minVal);
