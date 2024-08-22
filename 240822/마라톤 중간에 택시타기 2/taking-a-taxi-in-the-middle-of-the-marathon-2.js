8be819ea373229dbc354ab0f575d230d6afca4c9
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-22 17:02:49 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

--- 240822/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
+++ 240822/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
@@ -6,24 +6,19 @@ const checkPoints = input.slice(1).map((line) => line.split(' ').map(Number));
 
 const MAX_VAL = Number.MAX_SAFE_INTEGER;
 
-function manDistCal(a, b) {
-    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-}
-
 let minVal = MAX_VAL;
 for ( let i = 1 ; i < n - 1 ; i ++ ) {
-    const newCourse = [];
+    let prevIds = 0;
     let tempSumVal = 0;
-    for ( let j = 0 ; j < n ; j ++ ) {
-        if ( j !== i ) {
-            newCourse.push(checkPoints[j])
+
+    for ( let j = 1; j < n ; j ++ ) {
+        if ( j === i ) {
+            continue;
         }
+        tempSumVal += (Math.abs(checkPoints[j][0] - checkPoints[prevIds][0]) + Math.abs(checkPoints[j][1] - checkPoints[prevIds][1]))
+        prevIds = j;
     }
-    for ( let j = 0 ; j < newCourse.length - 1 ; j ++ ) {
-        const tempDist = manDistCal(newCourse[j], newCourse[j+1])
-        tempSumVal += tempDist;
-    }
-    minVal = Math.min(minVal, tempSumVal)
+    minVal = Math.min(minVal, tempSumVal);
 }
 
 console.log(minVal)
