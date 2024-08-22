d2429fcffa6b77a6091a26d84ec2c51b9de03c74
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-22 15:26:11 +0900

    Title: 체크판위에서 2, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240822/체크판위에서 2/on-the-checkboard-2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [r, c] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map((line) => line.split(' '));
+
+let count = 0;
+for ( let i = 1 ; i < r - 2 ; i ++ ) {
+    for ( let j = 1 ; j < c - 2 ; j ++ ) {
+        if ( arr[i][j] !== arr[0][0] ) ;
+        for ( let k = i + 1 ; k < r - 1 ; k ++ ) {
+            for ( let m = j + 1 ; m < c - 1 ; m ++ ) {
+                if ( arr[k][m] !== arr[i][j] ) {
+                    count += 1;
+                }
+            }
+        }
+    }
+}
+
+console.log(count)
