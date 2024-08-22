37a447a6aeb99469085c8e831e754b5d00dd7299
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-22 15:08:12 +0900

    Title: 일렬로 서있는 소 2, Time: 186ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240822/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const numbers = input[1].split(' ').map(Number);
+
+
+let count = 0;
+for ( let i = 0 ; i < n - 2 ; i ++ ) {
+    for ( let j = i + 1 ; j < n - 1 ; j ++ ) {
+        for ( let k = j + 1 ; k < n ; k ++ ) {
+            if (numbers[i] < numbers[j] && numbers[j] < numbers[k]) {
+                count += 1;
+            }
+        }
+    }
+}
+
+console.log(count)
