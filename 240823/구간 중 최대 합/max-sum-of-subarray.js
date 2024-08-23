04deea3e97226f8492950a061f52d66577815f83
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 13:13:20 +0900

    Title: 구간 중 최대 합, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240823/구간 중 최대 합/max-sum-of-subarray.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number)
+const numbers = input[1].split(' ').map(Number)
+
+
+let ans = 0;
+
+for ( let i = 0 ; i < n - k ; i ++ ) {
+    let tempSum = 0;
+    for ( let j = 0; j < k ; j ++ ) {
+        tempSum += numbers[i + j]
+    }
+    ans = Math.max(tempSum, ans)
+}
+
+console.log(ans)
