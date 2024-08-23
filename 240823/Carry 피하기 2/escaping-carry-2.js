b7e524dc778c78f44d3c03883bc7f17e4a03d422
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 11:15:56 +0900

    Title: Carry 피하기 2, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240823/Carry 피하기 2/escaping-carry-2.js
+++ 240823/Carry 피하기 2/escaping-carry-2.js
@@ -5,10 +5,12 @@ const n = Number(input[0]);
 const numbers = input.slice(1).map((line) => line.split('').map(Number));
 const revNumbers = input.slice(1).map((line) => line.split('').map(Number).reverse());
 
+const MIN_NUM = Number.MIN_SAFE_INTEGER
+
 // console.log(numbers);
 // console.log(revNumbers)
 
-let ans = Number.MIN_SAFE_INTEGER
+let ans = MIN_NUM
 
 for ( let i = 0 ; i < n - 2 ; i ++ ) {
     for ( let j = i + 1 ; j < n - 1 ; j ++ ) {
@@ -31,4 +33,4 @@ for ( let i = 0 ; i < n - 2 ; i ++ ) {
         }
     }
 }
-console.log(ans)
+console.log(ans === MIN_NUM ? -1 : ans)
