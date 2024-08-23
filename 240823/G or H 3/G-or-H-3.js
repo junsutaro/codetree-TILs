8283e3297401dd6f18aa1d4cb0d387348b69a81e
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 13:25:08 +0900

    Title: G or H 3, Time: 5033ms, Memory: 14MB, Status: Time Limit Exceed - CodeTree

--- 240823/G or H 3/G-or-H-3.js
+++ 240823/G or H 3/G-or-H-3.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, k] = input[0].split(' ').map(Number);
-const people = Array(10001).fill(null);
+const people = {};
 
 for (let i = 1; i <= n; i++) {
     const [pos, letter] = input[i].split(' ');
@@ -13,7 +13,7 @@ let maxScore = 0;
 
 for (let start = 1; start <= 10000; start++) {
     let score = 0;
-    for (let end = start; end <= start + k && end <= 10000; end++) {
+    for (let end = start; end <= start + k; end++) {
         if (people[end] === 'G') score += 1;
         else if (people[end] === 'H') score += 2;
     }
