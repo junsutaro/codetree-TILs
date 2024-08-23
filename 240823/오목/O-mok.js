a878c4f70bdf3b03d7af3ec4132e74638bcf8b2f
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 11:51:54 +0900

    Title: 오목, Time: 201ms, Memory: 47MB, Status: Passed - CodeTree

--- 240823/오목/O-mok.js
+++ 240823/오목/O-mok.js
@@ -1,65 +1,51 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const arr = input.map((line) => line.split(' '));
+const arr = input.map((line) => line.split(' ').map(Number));
 
-// console.log(arr);
-
-let ans = 0;
-
-// Is exist that X-axis direction Winner ?
-
-for ( let i = 0 ; i < 15 ; i ++ ) {
-    for ( let j = 0 ; j < 19 ; j ++ ) {
-        if ( arr[i][j] !== '0' ) {
-            if (arr[i][j] === arr[i+1][j] && arr[i+1][j] === arr[i+2][j] && arr[i+2][j] === arr[i+3][j] && arr[i+3][j] === arr[i+4][j]) {
-                console.log(Number(arr[i][j]));
-                console.log(i+3, j+1)
-                return;
-            }
-        }
-    }
+function isRange(x, y) {
+    return 0 <= x && x < 19 && 0 <= y && y < 19;
 }
 
-// Is exist that Y-axis direction Winner ?
+const dx = [0, 1, 1, 1, 0, -1, -1, -1];
+const dy = [1, 1, 0, -1, -1, -1, 0, 1];
 
 for ( let i = 0 ; i < 19 ; i ++ ) {
-    for ( let j = 0 ; j < 15 ; j ++ ) {
-        if ( arr[i][j] !== '0' ) {
-            if (arr[i][j] === arr[i][j+1] && arr[i][j+1] === arr[i][j+2] && arr[i][j+2] === arr[i][j+3] && arr[i][j+3] === arr[i][j+4]) {
-                console.log(Number(arr[i][j]));
-                console.log(i+1, j+3)
-                return;
-            }
-        }
-    }
-}
-
-// Is exist that right-down-axis direction Winner ?
-
-for ( let i = 0 ; i < 15 ; i ++ ) {
-    for ( let j = 0 ; j < 15 ; j ++ ) {
-        if ( arr[i][j] !== '0' ) {
-            if (arr[i][j] === arr[i+1][j+1] && arr[i+1][j+1] === arr[i+2][j+2] && arr[i+2][j+2] === arr[i+3][j+3] && arr[i+3][j+3] === arr[i+4][j+4]) {
-                console.log(Number(arr[i][j]));
-                console.log(i+3, j+3)
-                return;
-            }
-        }
-    }
-}
-
-// Is exist that left-down-axis direction Winner ?
-
-for ( let i = 0 ; i < 15 ; i ++ ) {
     for ( let j = 0 ; j < 19 ; j ++ ) {
-        if ( arr[i][j] !== '0' ) {
-            if (arr[i][j] === arr[i+1][j-1] && arr[i+1][j-1] === arr[i+2][j-2] && arr[i+2][j-2] === arr[i+3][j-3] && arr[i+3][j-3] === arr[i+4][j-4]) {
-                console.log(Number(arr[i][j]));
-                console.log(i+3, j-1)
-                return;
+        if ( arr[i][j] === 0 ) {
+            continue;
+        }
+
+        for ( let dir = 0 ; dir < 8 ; dir ++ ) {
+            let curCount = 1;
+            let curX = i;
+            let curY = j;
+
+            while (true) {
+                const nx = curX + dx[dir];
+                const ny = curY + dy[dir];
+                
+                if ( !isRange(nx, ny) ) {
+                    break;
+                }
+                if ( arr[nx][ny] !== arr[curX][curY] ) {
+                    break;
+                }
+
+                curCount += 1;
+                curX = nx;
+                curY = ny;
+
+                if (curCount === 5) {
+                    console.log(arr[i][j]);
+                    console.log(i + 1 + dx[dir] * 2, j + 1 + dy[dir] * 2)
+                    return;
+                }
             }
+
+
         }
     }
 }
-console.log(ans)
+
+console.log(0);
