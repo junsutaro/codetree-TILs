a93e322419cb34c0f90b8060932e397a4faf2539
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 11:13:40 +0900

    Title: Carry 피하기 2, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240823/Carry 피하기 2/escaping-carry-2.js
+++ 240823/Carry 피하기 2/escaping-carry-2.js
@@ -5,26 +5,29 @@ const n = Number(input[0]);
 const numbers = input.slice(1).map((line) => line.split('').map(Number));
 const revNumbers = input.slice(1).map((line) => line.split('').map(Number).reverse());
 
-let ans = -1;
+// console.log(numbers);
+// console.log(revNumbers)
 
-for (let i = 0; i < n - 2; i++) {
-    for (let j = i + 1; j < n - 1; j++) {
-        for (let k = j + 1; k < n; k++) {
+let ans = Number.MIN_SAFE_INTEGER
+
+for ( let i = 0 ; i < n - 2 ; i ++ ) {
+    for ( let j = i + 1 ; j < n - 1 ; j ++ ) {
+        for ( let k = j + 1 ; k < n ; k ++ ) {
+            let sumVal = 0;
             let isValid = true;
-            for (let m = 0; m < 5; m++) {
-                const sumOfDigits = (revNumbers[i][m] || 0) + (revNumbers[j][m] || 0) + (revNumbers[k][m] || 0);
-                if (sumOfDigits >= 10) {
-                    isValid = false;
+            for ( let m = 0 ; m < 5 ; m ++ ) {
+                if (((revNumbers[i][m] || 0) + (revNumbers[j][m] || 0) + (revNumbers[k][m]) || 0 ) >= 10 ) {
+                    isValid = false
                     break;
                 }
             }
-
+            // console.log('야호!')
             if (isValid) {
-                const sumVal = Number(numbers[i].join('')) + Number(numbers[j].join('')) + Number(numbers[k].join(''));
+                sumVal = Number(numbers[i].join('')) + Number(numbers[j].join('')) + Number(numbers[k].join(''));
                 ans = Math.max(ans, sumVal);
+                // console.log(sumVal)
             }
         }
     }
 }
-
-console.log(ans);
+console.log(ans)
