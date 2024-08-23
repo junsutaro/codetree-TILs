074cd0b51296f4e6d243a4909fbeabdcf65e3e53
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 12:54:43 +0900

    Title: 최고의 13위치 2, Time: 215ms, Memory: 47MB, Status: Passed - CodeTree

--- 240823/최고의 13위치 2/best-place-of-13-2.js
+++ 240823/최고의 13위치 2/best-place-of-13-2.js
@@ -4,60 +4,32 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const N = Number(input[0]);
 const arr = input.slice(1).map((line) => line.split(' ').map(Number));
 
-function isRange(x, y) {
-    return 0 <= x && x < N && 0 <= y && y < N ;
-}
+let maxCnt = 0;
 
-function isNotOverlap(x1, y1, x2, y2) {
-    return x1 === x2 && y1 + 3 < y2 && y2 < N;
-}
+for ( let i = 0 ; i < N ; i ++ ) {
+    for ( let j = 0 ; j < N - 2 ; j ++ ) {
+        for ( let k = 0 ; k < N ; k ++ ) {
+            for ( let l = 0 ; l < N - 2 ; l ++ ) {
 
-// console.log(arr)
-const MIN_NUM = Number.MIN_SAFE_INTEGER
-let ans = MIN_NUM;
+                if ( i === k && (Math.abs(j - l) <= 2) ) {
+                    continue;
+                }
 
-for ( let i = 0; i < N ; i ++ ) {
-    for ( let j = 0 ; j < N ; j ++ ) {
-
-        let count = 0;
-        for ( let k1 = 0 ; k1 < 3 ; k1 ++ ) {
-            if (arr[i][j+k1] === 1) {
-                count += 1;
-            }
-        }
-
-        if ( j < N - 5 ) {
-            for ( let newJ = j + 3 ; newJ < N - 2 ; newJ ++ ) {
-                let totalCount = count;
-                for ( let count = 0; count < 3 ; count ++ ) {
-                    if ( arr[i][newJ + count] === 1 ) {
-                        totalCount += 1;
+                let tempCnt = 0;
+                for ( let count = 0 ; count < 3 ; count += 1 ) {
+                    if ( arr[i][j+ count] === 1 ) {
+                        tempCnt += 1;  
+                    } 
+                    if ( arr[k][l + count] === 1 ) {
+                        tempCnt += 1;
                     }
                 }
-                ans = Math.max(totalCount, ans)
-            }
-        }
-
-        // console.log(count)
-
-        for ( let m = i + 1 ; m < N ; m ++ ) {
-            for ( let n = 0 ; n < N - 2 ; n ++ ) {
-                let totalCount = count;
-                // console.log(totalCount)
-                for ( let k2 = 0; k2 < 3 ; k2 ++ ) {
-                    if ( arr[m][n+k2] === 1 ) {
-                        totalCount += 1;
-                        // console.log('땡그랑', i, j ,m, n+k2)
-                    }
-                }
-                ans = Math.max(totalCount, ans)
+                maxCnt = Math.max(tempCnt, maxCnt);
 
             }
         }
-
-
-
     }
 }
 
-console.log(ans)
+
+console.log(maxCnt)
