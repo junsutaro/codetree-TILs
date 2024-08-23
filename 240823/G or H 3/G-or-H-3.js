9f7d04efe551197bf608038cc80326ffb23576e9
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-08-23 13:25:57 +0900

    Title: G or H 3, Time: 288ms, Memory: 50MB, Status: Passed - CodeTree

--- 240823/G or H 3/G-or-H-3.js
+++ 240823/G or H 3/G-or-H-3.js
@@ -1,23 +1,30 @@
-const fs = require('fs');
+const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+const MAX_NUM = 10000;
+
+// 변수 선언 및 입력
 const [n, k] = input[0].split(' ').map(Number);
-const people = {};
+const arr = new Array(MAX_NUM + 1).fill(0);
 
 for (let i = 1; i <= n; i++) {
-    const [pos, letter] = input[i].split(' ');
-    people[Number(pos)] = letter;
+    const [x, c] = input[i].split(' ');
+    const index = Number(x);
+    
+    arr[index] = (c === 'G') ? 1 : 2;
 }
 
-let maxScore = 0;
-
-for (let start = 1; start <= 10000; start++) {
-    let score = 0;
-    for (let end = start; end <= start + k; end++) {
-        if (people[end] === 'G') score += 1;
-        else if (people[end] === 'H') score += 2;
+// 모든 구간의 시작점을 잡아봅니다.
+let maxSum = 0;
+for (let i = 0; i <= MAX_NUM - k; i++) {
+    // 해당 구간 내 원소의 합을 구합니다.
+    let sumInterval = 0;
+    for (let j = i; j < i + k + 1; j++) {
+        sumInterval += arr[j];
     }
-    maxScore = Math.max(maxScore, score);
+
+    // 최댓값을 구합니다.
+    maxSum = Math.max(maxSum, sumInterval);
 }
 
-console.log(maxScore);
+console.log(maxSum);
