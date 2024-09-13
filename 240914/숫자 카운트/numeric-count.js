41aca37616744fb7f360960e71e63c2a96144e89
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-14 01:48:36 +0900

    Title: 숫자 카운트, Time: 243ms, Memory: 18MB, Status: Passed - CodeTree

--- 240914/숫자 카운트/numeric-count.js
+++ 240914/숫자 카운트/numeric-count.js
@@ -1,46 +1,53 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split('\n')
+const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 
 const tries = input.slice(1);
 
-const possible = [];
+let possible = [];
 
-for ( let i = 0 ; i < 8 ; i ++ ) { 
-    for ( let j = i + 1 ; j < 9 ; j ++ ) {
-        for ( let k = j + 1 ; k < 10 ; k ++ ) {
-            possible.push([i, j, k])
+// 1부터 9까지의 세 숫자로 구성된 모든 경우의 수를 생성
+for (let i = 1; i <= 9; i++) { 
+    for (let j = 1; j <= 9; j++) {
+        for (let k = 1; k <= 9; k++) {
+            possible.push([i, j, k]);
         }
     }
 }
-    
 
-for ( let trying of tries ) {
+for (let trying of tries) {
     const [number, a, b] = trying.split(' ');
-    const numArr = number.split('');
-    console.log(numArr) // 1번 numArr
-    
-    for ( let elem of possible ) {
-        console.log(numArr) // 2번 numArr
+    const numArr = number.split('').map(Number);
+
+    possible = possible.filter((elem) => {
+        if ( elem[0] === elem[1] || elem[1] === elem[2] || elem[0] === elem[2] ) {
+            return;
+        }
+
         let cnt1 = 0;
         let cnt2 = 0;
 
-        for ( let i = 0; i < 3 ; i ++ ) {
-            if ( numArr[i] = elem[i] ) {
-                cnt1 += 1
+        // 같은 자리 비교 (1번 카운트)
+        for (let i = 0; i < 3; i++) {
+            if (numArr[i] === elem[i]) {
+                cnt1 += 1;
             }
         }
 
-        for ( let i = 0; i < 3 ; i ++ ) {
-            for ( let j = 0 ; j < 3 ; j ++ ) {
-                if (numArr[i] = elem[j] || i !== j ) {
-                    cnt2 += 1
+        // 다른 자리 비교 (2번 카운트)
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (numArr[i] === elem[j] && i !== j) {
+                    cnt2 += 1;
+                    break;
                 }
             }
         }
-        
-        if ( cnt1 !== a && cnt2 !== b ) {
-            possible.pop(elem)
-        }
-    }
+
+        // 주어진 조건과 맞지 않으면 제거
+        return cnt1 === Number(a) && cnt2 === Number(b);
+    });
 }
+
+// 가능한 수의 총 개수를 출력
+console.log(possible.length);
