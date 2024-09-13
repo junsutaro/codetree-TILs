bdf6507dac91645680bea04355769967d01502cf
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-13 10:57:49 +0900

    Title: 밭의 높이를 고르게하기, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240913/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// n개 밭 T번 이상, H 높이로
+const [n, h, t] = input[0].split(' ').map(Number);
+const fields = input[1].split(' ').map(Number);
+
+let ans = Number.MAX_SAFE_INTEGER;
+
+for ( let i = 0 ; i < n - t + 1 ; i ++ ) {
+    let diffSum = 0;
+    for ( let j = i ; j < i + 3 ; j ++ ) {
+        diffSum += Math.abs(fields[j] - h)
+    }
+    ans = Math.min(ans, diffSum)
+}
+
+console.log(ans)
