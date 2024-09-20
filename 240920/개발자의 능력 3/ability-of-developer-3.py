55eef8383062a958cff99996c5f42405c4d9912d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-20 19:42:56 +0900

    Title: 개발자의 능력 3, Time: 235ms, Memory: 24MB, Status: Passed - CodeTree

--- 240920/개발자의 능력 3/ability-of-developer-3.py
+++ 240920/개발자의 능력 3/ability-of-developer-3.py
@@ -5,4 +5,17 @@ INT_MAX = sys.maxsize
 n = 6;
 arr = list(map(int, input().split()));
 
-print(arr)
+def get_diff(i, j, k):
+    sum1 = arr[i] + arr[j] + arr[k]
+    sum2 = sum(arr) - sum1
+    
+    return abs(sum1 - sum2)
+
+ans = INT_MAX;
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j+1, n):
+            ans = min(ans, get_diff(i, j, k))
+
+print(ans)
