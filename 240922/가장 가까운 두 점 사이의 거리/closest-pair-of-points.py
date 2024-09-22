deba6cec9f3526b602cb056a19353903058e2fe7
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-22 21:49:39 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240922/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240922/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -9,7 +9,7 @@ min_distance = MAX_SIZE
 
 for i in range(n - 1):
     for j in range(i+ 1, n):
-        temp_distance = abs(arr[i][0] - arr[j][0]) + abs(arr[i][1] - arr[j][1])
+        temp_distance = (arr[i][0] - arr[j][0])**2 + (arr[i][1] - arr[j][1])**2
         if temp_distance < min_distance:
             min_distance = temp_distance
             ans = (arr[i][0] - arr[j][0])**2 + (arr[i][1] - arr[j][1])**2
