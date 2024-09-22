9e753a5b3cdd5df5dfa3f019b7e1fb7dae3feacb
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-22 22:24:59 +0900

    Title: 삼각형 만들기, Time: 171ms, Memory: 26MB, Status: Passed - CodeTree

--- 240922/삼각형 만들기/create-triangle.py
+++ 240922/삼각형 만들기/create-triangle.py
@@ -8,18 +8,13 @@ ans = 0
 for i in range(n - 2):
     for j in range(i+1, n-1):
         for k in range(j+1, n):
-            # 한 변은 x축에평행, 다른 한 변은 y축에 평행
-
-            # A, B, C = arr[i], arr[j], arr[k]
-            # if (A[0] - B[0] == 0 and (B[1] - C[1] == 0 or A[1] - C[1] == 0)) or \
-            #    (A[0] - C[0] == 0 and (B[1] - C[1] == 0 or A[1] - B[1] == 0)) or \
-            #    (B[0] - C[0] == 0 and (A[1] - C[1] == 0 or A[1] - B[1] == 0)):
-
             A = arr[i]; B = arr[j]; C = arr[k]
+
+            # 한 변은 x축에평행, 다른 한 변은 y축에 평행
             if (A[0] - B[0] == 0 and ( B[1] - C[1] == 0 or A[1] - C[1] == 0 )) or \
                 (A[0] - C[0] == 0 and ( B[1] - C[1] == 0 or A[1] - B[1] == 0 )) or \
                 (B[0] - C[0] == 0 and ( A[1] - C[1] == 0 or A[1] - B[1] == 0 )):
-                temp = abs(A[0]*B[1] + B[0]*C[1] + C[0]*A[1]) - abs(B[0]*A[1] + C[0]*B[1] + A[0]*C[1])
+                temp = abs((A[0]*B[1] + B[0]*C[1] + C[0]*A[1]) - (B[0]*A[1] + C[0]*B[1] + A[0]*C[1]))
                 ans = max(temp, ans)
 
 print(ans)
