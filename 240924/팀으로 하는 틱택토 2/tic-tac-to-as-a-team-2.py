a063617fe167e9ea2cf8decaca877cc252aa97b6
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 21:34:22 +0900

    Title: 팀으로 하는 틱택토 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240924/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,39 +1,43 @@
 arr = [list(map(int, input().strip())) for _ in range(3)]
 
+# 등장하는 숫자들 안에서만 보면 됨
 unique_numbers = set()
+for elem in arr:
+    for number in elem:
+        unique_numbers.add(number)
+
+# 그 숫자들로 2개씩 묶는 모든 팀 경우의 수
+import itertools
+all_teams = list(itertools.combinations(unique_numbers, 2))
+
+# 가로세로대각선 모든 경우의 집합 (을 바로 set으로 넣어)
+all_case = []
 for row in arr:
-    for num in row:
-        unique_numbers.add(num)
+    all_case.append(set(row))
 
-from itertools import combinations
+for i in range(3):
+    temp = set()
+    for j in range(3):
+        temp.add(arr[j][i])
+    all_case.append(temp)
 
-teams = combinations(unique_numbers, 2)
+diag1 = set()
+diag2 = set()
+for i in range(3):
+    diag1.add(arr[i][i])
+all_case.append(diag1)
 
-# 이기면? -> 가로 세로 대각선 돌면서 모든 조합 만들어서 team이 그안에 있으면 됩니다
+for i in range(3):
+    diag2.add(arr[i][2-i])
+all_case.append(diag2)
 
-lines = []
-for row in arr:
-    lines.append(set(row))
-
-for col_idx in range(3):
-    vertical = set()
-    for row_idx in range(3):
-        vertical.add(arr[row_idx][col_idx])
-    lines.append(vertical)
-
-diag1 = set(arr[i][i] for i in range(3))
-lines.append(diag1)
-
-diag2 = set(arr[i][2-i] for i in range(3))
-lines.append(diag2)
-
-
+# all_teams 돌면서 all_case에 있는지 체크
 ans = 0
 
-for team in teams:
-    for line in lines:
-        # print(team, line)
-        if set(team) == line:
-            ans +=1
-            break;
+for team in all_teams:
+    for case in all_case:
+        if set(team) == case:
+            ans += 1
+            break
+
 print(ans)
