1468d098829cc30f984beb58daaafd83caef207a
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 11:51:34 +0900

    Title: 야바위, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240924/야바위/ya-rock.py
@@ -0,0 +1,25 @@
+n = int(input())
+inputs = [tuple(map(int, input().split())) for _ in range(n)]
+
+max_val = 0
+ans = 0
+
+for i in range(1, 4):
+    cups = [False] * 4
+    cups[i] = True
+    
+    cnt = 0
+
+    # a번 b번 교환 후 c 열어서 조약돌 있으면 점수
+    for elem in inputs:
+        a, b, c = elem
+        cups[a], cups[b] = cups[b], cups[a]
+
+        if cups[c] == True:
+            cnt += 1
+
+    if cnt > max_val:
+        cnt = max_val
+        ans = i
+        
+print(ans)
