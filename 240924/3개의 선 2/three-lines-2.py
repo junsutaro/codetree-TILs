89c907731f98a67e96af9a45ea7bfc37243ead89
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 13:37:20 +0900

    Title: 3개의 선 2, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240924/3개의 선 2/three-lines-2.py
+++ 240924/3개의 선 2/three-lines-2.py
@@ -12,21 +12,26 @@ flag = True
 import itertools
 
 for combo in itertools.combinations(all_lines, 3):
+    all_covered = False
+
     for x, y in points:
+        # 다 돌았는데 inner_flag가 True이면? flag가 True가 된다~
         inner_flag = False
-        
+        cnt = 0
         for line_type, coord in combo:
+
             if line_type == "V":
                 if y == coord:
-                    inner_flag = True
+                    cnt += 1
             elif line_type == "H":
                 if x == coord:
-                    inner_flag = True
+                    cnt += 1
+        if cnt >= 1:
+            inner_flag = True
 
-        if inner_flag:
-            continue
-        else:
+        if inner_flag == False:
             flag = False
+    
 
 if flag:
     print(1)
