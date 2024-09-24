7d7ad4a39b193af6efb0c93f858f96cc2322847d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 11:17:25 +0900

    Title: 정보에 따른 숫자 2, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240924/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,35 +1,52 @@
-import bisect
-
+# T개 알파벳, a 이상 b 이하 구간
 T, a, b = map(int, input().split())
+# T개 줄에 걸쳐 각 알파벳 값 c, 놓인 위치 x
+alphas = [tuple(input().split()) for _ in range(T)]
 
-S_positions = []
-N_positions = []
 
-for _ in range(T):
-    c, x = input().split()
-    x = int(x)
-    if c == 'S':
-        S_positions.append(x)
-    else:
-        N_positions.append(x)
+# arr에서 현재 i(temp)와 가장 가까운 수를 찾아서 거리 return 
+# def find_nearest(arr, temp):
+#     dist = 10000
+#     for elem in arr:
+#         dist = min(dist, abs(elem - temp))
+    
+#     return dist
 
-S_positions.sort()
-N_positions.sort()
+import bisect
+def find_nearest(arr, temp):
+    index = bisect.bisect_left(arr ,temp)
 
-def closest_distance(positions, x):
-    index = bisect.bisect_left(positions, x)
     if index == 0:
-        return abs(positions[0] - x)
-    elif index == len(positions):
-        return abs(positions[-1] - x)
-    else:
-        return min(abs(positions[index - 1] - x), abs(positions[index] - x))
+        return abs(arr[0] - temp)
+    elif index == len(arr):
+        return abs(arr[-1] - temp)
+    
+    before = arr[index - 1]
+    after = arr[index]
 
-count = 0
-for x in range(a, b + 1):
-    d1 = closest_distance(S_positions, x)
-    d2 = closest_distance(N_positions, x)
-    if d1 <= d2:
-        count += 1
+    return min( abs(before - temp), abs(after - temp) )
 
-print(count)
+# 특정위치 k에서 S까지 거리가 N보다 가까우면 특별한 위치
+
+N_pos = []
+S_pos = []
+
+for alpha in alphas:
+    if alpha[0] == 'S':
+        S_pos.append(int(alpha[1]))
+    if alpha[0] == 'N':
+        N_pos.append(int(alpha[1]))
+
+S_pos.sort()
+N_pos.sort()
+
+ans = 0;
+
+for i in range(a, b+1):
+    S_nearest = find_nearest(S_pos, i)
+    N_nearest = find_nearest(N_pos, i)
+    if S_nearest <= N_nearest:
+        ans += 1
+
+
+print(ans)
