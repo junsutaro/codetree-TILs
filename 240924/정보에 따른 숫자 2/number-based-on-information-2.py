afffda7b906c518efb734573b65b241b5cfc1fa5
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 11:01:37 +0900

    Title: 정보에 따른 숫자 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240924/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,38 +1,35 @@
-# T개 알파벳, a 이상 b 이하 구간
+import bisect
+
 T, a, b = map(int, input().split())
-# T개 줄에 걸쳐 각 알파벳 값 c, 놓인 위치 x
-alphas = [tuple(input().split()) for _ in range(T)]
 
+S_positions = []
+N_positions = []
 
-# arr에서 현재 i(temp)와 가장 가까운 수를 찾아서 거리 return 
-def find_nearest(arr, temp):
-    dist = 10000
-    for elem in arr:
-        dist = min(dist, abs(elem - temp))
-    
-    return dist
+for _ in range(T):
+    c, x = input().split()
+    x = int(x)
+    if c == 'S':
+        S_positions.append(x)
+    else:
+        N_positions.append(x)
 
-# 특정위치 k에서 S까지 거리가 N보다 가까우면 특별한 위치
+S_positions.sort()
+N_positions.sort()
 
-N_pos = []
-S_pos = []
+def closest_distance(positions, x):
+    index = bisect.bisect_left(positions, x)
+    if index == 0:
+        return abs(positions[0] - x)
+    elif index == len(positions):
+        return abs(positions[-1] - x)
+    else:
+        return min(abs(positions[index - 1] - x), abs(positions[index] - x))
 
-for alpha in alphas:
-    if alpha[0] == 'S':
-        S_pos.append(int(alpha[1]))
-    if alpha[0] == 'N':
-        N_pos.append(int(alpha[1]))
+count = 0
+for x in range(a, b + 1):
+    d1 = closest_distance(S_positions, x)
+    d2 = closest_distance(N_positions, x)
+    if d1 <= d2:
+        count += 1
 
-S_pos.sort()
-N_pos.sort()
-
-ans = 0;
-
-for i in range(a, b+1):
-    S_nearest = find_nearest(S_pos, i)
-    N_nearest = find_nearest(N_pos, i)
-    if S_nearest <= N_nearest:
-        ans += 1
-
-
-print(ans)
+print(count)
