97ee7a418dd33bf6bab77a6e4b1bdb185afb59ee
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 14:04:19 +0900

    Title: 좌표평면 위의 균형 2, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240924/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,26 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+
+ans = float('inf')
+
+for x in range(1, 101):
+    for y in range(1, 101):
+        LU = 0
+        RU = 0
+        LD = 0
+        RD = 0
+        
+        for px, py in points:
+            if px < x and py < y:
+                LD += 1
+            elif px < x and py > y:
+                LU += 1
+            elif px > x and py < y:
+                RD += 1
+            elif px > x and py > y:
+                RU += 1
+        
+        ans = min(ans, max(LD, LU, RU, RD))
+
+print(ans)
