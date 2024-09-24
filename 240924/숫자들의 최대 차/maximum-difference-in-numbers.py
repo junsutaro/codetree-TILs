d283550a4a29d85935c62bf9e8253a0a23fd5620
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 23:28:35 +0900

    Title: 숫자들의 최대 차, Time: 156ms, Memory: 38MB, Status: Passed - CodeTree

--- 240924/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240924/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,15 +1,28 @@
 n, k = map(int, input().split())
 nums = [int(input()) for _ in range(n)]
 
+nums.sort()
+
 ans = 0
-
-import itertools
-for i in range(n, 0, -1):
-    num_combs = itertools.combinations(nums, i)
-
-    for num_comb in num_combs:
-        if (max(num_comb) - min(num_comb)) <= k:
-            ans = max(len(num_comb), ans)
+for i in range(n):
+    cnt = 1
+    for j in range(i + 1, n):
+        if nums[j] - nums[i] <= k:
+            cnt += 1
+        else:
             break
-            
+    ans = max(cnt, ans)
+
 print(ans)
+
+
+
+# ans = 0
+# import itertools
+# for i in range(n, 0, -1):
+#     num_combs = itertools.combinations(nums, i)
+#     for num_comb in num_combs:
+#         if (max(num_comb) - min(num_comb)) <= k:
+#             ans = max(len(num_comb), ans)
+#             break
+# print(ans)
