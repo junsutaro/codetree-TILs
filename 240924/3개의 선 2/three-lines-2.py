dea7c4b404ed98f1767e76430281919ee856d8eb
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 13:25:20 +0900

    Title: 3개의 선 2, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240924/3개의 선 2/three-lines-2.py
+++ 240924/3개의 선 2/three-lines-2.py
@@ -1,35 +1,34 @@
-import itertools
-
-# 입력 받기
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-# 가능한 모든 수직선과 수평선 생성
-vertical_lines = [("V", x) for x in range(11)]    # 수직선: x=0 ~ x=10
-horizontal_lines = [("H", y) for y in range(11)]  # 수평선: y=0 ~ y=10
-all_lines = vertical_lines + horizontal_lines       # 총 22개
+# inputs를 돌면서 하나씩 조건을 체크해서 flag가 False일 때 break?
 
-# 모든 3개 직선 조합 탐색
-found = False  # 조건을 만족하는 조합이 있는지 여부
+vertical_lines = [("V", x) for x in range(11)]
+horizontal_lines = [("H", y) for y in range(11)]
+all_lines = vertical_lines + horizontal_lines
+
+flag = False
+
+import itertools
 
 for combo in itertools.combinations(all_lines, 3):
-    # 각 조합에 대해 모든 점이 적어도 하나의 직선 위에 있는지 확인
     all_covered = True
-    for (px, py) in points:
-        covered = False
-        for (line_type, coord) in combo:
-            if line_type == "V" and px == coord:
-                covered = True
-                break
-            elif line_type == "H" and py == coord:
-                covered = True
-                break
-        if not covered:
-            all_covered = False
-            break  # 현재 조합에서는 모든 점을 커버하지 못하므로 다음 조합으로 이동
-    if all_covered:
-        found = True
-        break  # 조건을 만족하는 조합을 찾았으므로 탐색 종료
 
-# 결과 출력
-print(1 if found else 0)
+    for x, y in points:
+        for line_type, coord in combo:
+            if line_type == "V":
+                if y == coord:
+                    continue
+            elif line_type == "H":
+                if x == coord:
+                    continue
+            else:
+                all_covered = False
+
+    if all_covered:
+        flag = True
+
+if flag:
+    print(1)
+else:
+    print(0)
