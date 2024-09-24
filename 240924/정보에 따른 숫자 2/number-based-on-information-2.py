ab969694414f08c342912fc751d7f3e1881d57a3
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-24 10:57:57 +0900

    Title: 정보에 따른 숫자 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240924/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -0,0 +1,38 @@
+# T개 알파벳, a 이상 b 이하 구간
+T, a, b = map(int, input().split())
+# T개 줄에 걸쳐 각 알파벳 값 c, 놓인 위치 x
+alphas = [tuple(input().split()) for _ in range(T)]
+
+
+# arr에서 현재 i(temp)와 가장 가까운 수를 찾아서 거리 return 
+def find_nearest(arr, temp):
+    dist = 10000
+    for elem in arr:
+        dist = min(dist, abs(elem - temp))
+    
+    return dist
+
+# 특정위치 k에서 S까지 거리가 N보다 가까우면 특별한 위치
+
+N_pos = []
+S_pos = []
+
+for alpha in alphas:
+    if alpha[0] == 'S':
+        S_pos.append(int(alpha[1]))
+    if alpha[0] == 'N':
+        N_pos.append(int(alpha[1]))
+
+S_pos.sort()
+N_pos.sort()
+
+ans = 0;
+
+for i in range(a, b+1):
+    S_nearest = find_nearest(S_pos, i)
+    N_nearest = find_nearest(N_pos, i)
+    if S_nearest <= N_nearest:
+        ans += 1
+
+
+print(ans)
