db06695370b00b0cd6e2d6b4d75bb7de05a1e15b
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 20:42:33 +0900

    Title: 두 직사각형, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/두 직사각형/two-rectangles.py
@@ -0,0 +1,15 @@
+sq1 = map(int, input().split())
+sq2 = map(int, input().split())
+
+def find_ans(sq1, sq2):
+    x1, y1, x2, y2 = sq1
+    a1, b1, a2, b2 = sq2
+    print(x1, y1, x2, y2)
+    print(a1, b1, a2, b2)
+    if x1 > a2 or a1 > x2 or y1 > b2 or b1 > y2:
+        return True
+
+if find_ans(sq1, sq2):
+    print('nonoverlapping')
+else:
+    print('overlapping')
