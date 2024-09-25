c49c9e1d3f45106a65d91ee84b3d0cb0ee68e55f
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 22:15:26 +0900

    Title: 코딩톡, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/코딩톡/coding-talk.py
@@ -0,0 +1,13 @@
+n, m, p = map(int,(input().split()))
+msgs = [tuple(input().split()) for _ in range(m)]
+
+ans_lst = []
+for i in range(n):
+    ans_lst.append(chr(65+i))
+
+for i in range(m):
+    if i >= p-1:
+        if msgs[i][0] in ans_lst:
+            ans_lst.remove(msgs[i][0])
+
+print(*ans_lst)
