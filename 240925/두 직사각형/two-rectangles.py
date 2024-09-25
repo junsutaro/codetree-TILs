58fb4e90c0f36f7a19922243fb713042e824c446
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 20:42:58 +0900

    Title: 두 직사각형, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/두 직사각형/two-rectangles.py
+++ 240925/두 직사각형/two-rectangles.py
@@ -4,8 +4,6 @@ sq2 = map(int, input().split())
 def find_ans(sq1, sq2):
     x1, y1, x2, y2 = sq1
     a1, b1, a2, b2 = sq2
-    print(x1, y1, x2, y2)
-    print(a1, b1, a2, b2)
     if x1 > a2 or a1 > x2 or y1 > b2 or b1 > y2:
         return True
 
