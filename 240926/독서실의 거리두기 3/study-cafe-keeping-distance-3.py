0fa49dddb5395635634bce1124bdb688ce0da8c3
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 00:37:44 +0900

    Title: 독서실의 거리두기 3, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240926/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,29 @@
+n = int(input())
+seats = list(map(int, input()))
+
+def find_min_dist(arr):
+
+    before_idx = 0
+    now_idx = 0
+    temp_dist = 100
+
+    while now_idx < len(arr) - 1:
+        now_idx += 1
+        if arr[now_idx] == 1:
+            temp_dist = min(temp_dist, now_idx - before_idx)
+            before_idx = now_idx
+
+    return temp_dist
+    
+
+ans = 0
+
+idx = 0
+while idx < n-1:
+    if seats[idx] == 0:
+        seats[idx] = 1
+        ans = max(ans, find_min_dist(seats))
+        seats[idx] = 0
+    idx += 1
+
+print(ans)
