1015add8bfc46d6c9528fbbe1f9623036e9e7b99
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 01:01:21 +0900

    Title: 연속된 숫자 만들기 3, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240926/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240926/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,21 +1,8 @@
 lst = list(map(int, input().split()))
 
-lst.sort()
-
 cnt = 0
 
-while(True):
-
-    if lst[1]-lst[0] > lst[2]-lst[1]:
-        lst[2] = lst[1] - 1
-    else:
-        lst[0] = lst[1] + 1
-
-    cnt += 1
-    
-    lst.sort()
-    # print(lst)
-
-    if lst[2] - lst[1] == 1 and lst[1] - lst[0] == 1:
-        print(cnt)
-        break
+if lst[1]-lst[0] > lst[2]-lst[1]:
+    print(lst[1] - lst[0] - 1)
+else:
+    print(lst[2] - lst[1] - 1)
