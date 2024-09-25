a7699f2cc2ae4ce20a6cc42200df27ad736a6ee4
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 22:27:33 +0900

    Title: 코딩톡, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/코딩톡/coding-talk.py
+++ 240925/코딩톡/coding-talk.py
@@ -5,10 +5,19 @@ ans_lst = []
 for i in range(n):
     ans_lst.append(chr(65+i))
 
+new_p = p
+
+for i in range(p-1, -1, -1):
+    if msgs[i][1] == msgs[p][1]:
+        new_p = i
+        
+
+
 for i in range(m):
-    if i >= p-1:
+    if i >= new_p-1:
         if msgs[i][0] in ans_lst:
             ans_lst.remove(msgs[i][0])
+
 ans_lst.sort()
 
 if p == 1:
