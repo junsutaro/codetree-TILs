05dfe871748bba1387bde69d651d35880296d23a
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 22:51:56 +0900

    Title: 코딩톡, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240925/코딩톡/coding-talk.py
+++ 240925/코딩톡/coding-talk.py
@@ -9,11 +9,13 @@ new_p = p
 
 for i in range(p-2, -1, -1):
     if msgs[i][1] == msgs[p-1][1]:
-        new_p = i + 1
+        new_p = i + 1 # p는 번째, i 는idx라서 + 1
     else:
         break
 
-for i in range(new_p - 1, m):
+print(new_p)
+
+for i in range(new_p - 1, m): # p는 번째, i는 idx라서 -1
     if msgs[i][0] in ans_lst:
         ans_lst.remove(msgs[i][0])
 
