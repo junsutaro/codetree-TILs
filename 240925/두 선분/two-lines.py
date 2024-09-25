b85b724ee4fdb552638adf829e698bebf391f89c
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 20:34:31 +0900

    Title: 두 선분, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/두 선분/two-lines.py
+++ 240925/두 선분/two-lines.py
@@ -1,13 +1,15 @@
 x1, x2, x3, x4 = list(map(int, input().split()))
 
 mx = max(x1, x2, x3, x4)
+
 cnt_lst = [0] * ( mx + 1 )
+
 for i in range(x1, x2):
     cnt_lst[i] += 1
 for i in range(x3, x4):
     cnt_lst[i] += 1
 
 if max(cnt_lst) >= 2:
-    print(intersecting)
+    print('intersecting')
 else:
-    print(nonintersecting)
+    print('nonintersecting')
