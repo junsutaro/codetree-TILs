7f146ef349e34460ab25eeba90f342a17c544621
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 20:34:55 +0900

    Title: 두 선분, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/두 선분/two-lines.py
+++ 240925/두 선분/two-lines.py
@@ -4,9 +4,9 @@ mx = max(x1, x2, x3, x4)
 
 cnt_lst = [0] * ( mx + 1 )
 
-for i in range(x1, x2):
+for i in range(x1, x2+1):
     cnt_lst[i] += 1
-for i in range(x3, x4):
+for i in range(x3, x4+1):
     cnt_lst[i] += 1
 
 if max(cnt_lst) >= 2:
