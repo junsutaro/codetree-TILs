650bd2594729c5095d3c5b7cead2fed3fa4525b0
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-25 23:42:21 +0900

    Title: X 달리기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/X 달리기/x-run.py
+++ 240925/X 달리기/x-run.py
@@ -2,27 +2,20 @@ n = int(input())
 
 def find_ans(n):
     ans = 0
-    if n <= 110:
-        for i in range(1, 21):
-            max_dist = 0
-            if i % 2 == 1:
-                max_dist += (1 + (i + 1) // 2) * ((i + 1) // 2)
-                max_dist -= (i + 1) // 2
-            else:
-                max_dist += (1 + i // 2) * (i // 2)
 
-            if max_dist >= n:
-                ans = i
-                break
+    for i in range(1, 10000):
+        max_dist = 0
+        if i % 2 == 1:
+            max_dist += (1 + (i + 1) // 2) * ((i + 1) // 2)
+            max_dist -= (i + 1) // 2
+        else:
+            max_dist += (1 + i // 2) * (i // 2)
 
-    else:
-        ans = 21 + ((n - 110) // 10)
+        if max_dist >= n:
+            ans = i
+            break
 
     print(ans)
 
 
-for i in range(20):
-    find_ans(i)
-
-    
-# find_ans(n)
+find_ans(n)
