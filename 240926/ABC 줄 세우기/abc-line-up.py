b29c9c99a929856828973a2f1a8e96549cc9a83a
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 16:19:05 +0900

    Title: ABC 줄 세우기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240926/ABC 줄 세우기/abc-line-up.py
+++ 240926/ABC 줄 세우기/abc-line-up.py
@@ -4,23 +4,16 @@ ppl = list(input().split())
 for i in range(n):
     ppl[i] = ord(ppl[i]) - 65
 
-
 ans = 0
-for i in range(n-1):
-    # 그 자리면 바꿀 필요 X
-    if i != ppl[i]: # i가 1인데 ppl[i]가 1이 아니다?
-        for j in range(i+1, n): # i보다 큰거 쭉 돌면서 ppl[j] = i, ppl[i] = j 인거 찾아
-            if ppl[j] == i and ppl[i] == j:
-                ppl[i], ppl[j] = ppl[j], ppl[i]
-                ans += 1
-
-        for j in range(i+1, n):
-            # 그런거 없으면 i 갖고있는 곳 가서 뺏어와
-            if ppl[j] == i:
-                # 지금 ppl[i]가 가진놈 가져가라 그리고 내놔라
-                ppl[j], ppl[i] = ppl[i], ppl[j]
-
-                ans += 1
-
-
+while True:
+    isChange = False
+    for i in range(n-1):
+        if ppl[i] > ppl[i+1]:
+            ppl[i], ppl[i+1] = ppl[i+1], ppl[i]
+            ans += 1
+            isChange = True
+    
+    if not isChange:
+        break
+    
 print(ans)
