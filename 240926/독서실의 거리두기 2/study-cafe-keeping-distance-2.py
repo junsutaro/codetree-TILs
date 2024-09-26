a40eb9665647743404a7a878e0a82d27af21340d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 15:21:26 +0900

    Title: 독서실의 거리두기 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240926/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240926/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -7,13 +7,12 @@ empties = [i for i, seat in enumerate(seats) if seat == 0]
 ans = 0
 
 for empty in empties:
-    occs.append(empty)
-    occs.sort()
+    new_occs = occs[:]
+    new_occs.append(empty)
+    new_occs.sort()
     dist = float('inf')
     for i in range(1, len(occs)):
         dist = min(dist, occs[i] - occs[i-1])
     ans = max(ans, dist)
-    occs.remove(empty)
-
 
 print(ans)
