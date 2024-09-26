aeffb2828fb7439d77a6a0be1397c32bc3f44665
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 21:05:35 +0900

    Title: 최소 와이파이 수, Time: 5090ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240926/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+idx = 0
+ans = 0
+while True:
+    if arr[idx] == 0:
+        idx += 1
+        continue
+    else:
+        if idx + m < n:
+            ans += 1
+            idx = idx + 2*m + 1
+    
+    if idx >= n:
+        break
+
+print(ans)
