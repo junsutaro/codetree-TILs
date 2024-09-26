a31fd2ca1e792c64460e9367445d5bcdaad0828e
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 17:20:15 +0900

    Title: 두 번째로 작은 수의 위치, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240926/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+nums = list(map(int, input().split()))
+
+min1 = float('inf')
+min2 = float('inf')
+
+
+ans = -1
+
+for num in nums:
+    if num < min1:
+        min2 = min1
+        min1 = num
+    elif min1 < num < min2:
+        min2 = num
+
+if min2 == float('inf'):
+    ans = -1
+else:
+    cnt_2 = 0
+    for i in range(n):
+        if nums[i] == min2:
+            ans = i + 1
+            cnt_2 += 1
+            if cnt_2 > 1:
+                ans = -1
+                
+
+print(ans)
