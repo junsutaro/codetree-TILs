a9fe2b92f01256795418522d47013344354b903d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 16:52:05 +0900

    Title: 순위 경쟁2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240926/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+scores = [tuple(input().split()) for _ in range(n)]
+
+print(scores)
+
+sc_board = {}
+sc_board['A'] = 0
+sc_board['B'] = 0
+
+
+honor = [0] * (n+1)
+
+for idx in range(len(scores)):
+    name, score = scores[idx]
+    sc_board[name] += int(score)
+
+    if sc_board['A'] - sc_board['B'] > 0:
+        honor[idx + 1] = 1
+    elif sc_board['A'] - sc_board['B'] < 0:
+        honor[idx + 1] = -1
+
+
+ans = 0
+for i in range(0, n):
+    if honor[i] != honor[i+1]:
+        ans += 1
+
+print(ans)
