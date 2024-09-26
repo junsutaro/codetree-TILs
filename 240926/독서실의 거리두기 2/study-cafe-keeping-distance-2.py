475f7345c64264d3a8a89c8b1912e6a403791e23
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 15:29:07 +0900

    Title: 독서실의 거리두기 2, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240926/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240926/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -1,25 +1,20 @@
 n = int(input())
 seats = list(map(int, input()))
 
+# 1이 있는 좌석의 위치를 저장
 occs = [i for i, seat in enumerate(seats) if seat == 1]
-empties = [i for i, seat in enumerate(seats) if seat == 0]
-
 ans = 0
 
-for empty in empties:
-    new_occs = occs[:]
-    new_occs.append(empty)
-    new_occs.sort()
+for i in range(n):
+    if seats[i] == 0:
+        new_occs = occs[:]
+        new_occs.append(i)
+        new_occs.sort() 
 
-    dist = float('inf')
-    for i in range(1, len(new_occs)):
-        dist = min(dist, new_occs[i] - new_occs[i-1])
-    ans = max(ans, dist)
-
-    if 0 in empties:
-        ans = max(ans, new_occs[0])
-
-    if n-1 in empties:
-        ans = max(ans, n-1 - new_occs[-1])
+        dist = float('inf')
+        for j in range(1, len(new_occs)):
+            dist = min(dist, new_occs[j] - new_occs[j - 1])
+        
+        ans = max(ans, dist)
 
 print(ans)
