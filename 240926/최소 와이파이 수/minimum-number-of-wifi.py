17148d31b8412c4ac755940ac2ee27acd483612a
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 21:15:31 +0900

    Title: 최소 와이파이 수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240926/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240926/최소 와이파이 수/minimum-number-of-wifi.py
@@ -5,11 +5,10 @@ idx = 0
 ans = 0
 
 while idx < n:
-    # 사람이 있는 위치를 찾음
     if arr[idx] == 1:
         ans += 1
-        # 와이파이를 설치한 후, 그 범위를 넘어설 때까지 idx를 증가
-        idx += 2 * m + 1
+        idx += 2*m + 1
+    
     else:
         idx += 1
 
