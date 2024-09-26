7443a31fc8ab0d3996ffec40701e0355ed6980e3
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-26 21:16:12 +0900

    Title: 최소 와이파이 수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240926/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240926/최소 와이파이 수/minimum-number-of-wifi.py
@@ -5,11 +5,12 @@ idx = 0
 ans = 0
 
 while idx < n:
-    if arr[idx] == 1:
-        ans += 1
-        idx += 2*m + 1
-    
+    if arr[idx] == 0:
+        if idx + m < n:
+            ans += 1
+            idx = idx + 2*m + 1
     else:
         idx += 1
-
+        continue
+    
 print(ans)
