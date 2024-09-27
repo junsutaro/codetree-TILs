8ce7cdcb764cd256be74c6c7cbced39a478208be
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-27 12:56:59 +0900

    Title: 수열의 순서 바꾸기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240927/수열의 순서 바꾸기/reorder-sequence.py
+++ 240927/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,34 +1,52 @@
-def group_blocks(arr):
-    blocks = []
-    current_block = [arr[0]]  # 첫 번째 숫자로 시작하는 블록
-
-    for i in range(1, len(arr)):
-        if arr[i] == arr[i - 1] + 1:  # 연속된 숫자는 같은 블록에 추가
-            current_block.append(arr[i])
-        else:
-            blocks.append(current_block)  # 새로운 블록 시작
-            current_block = [arr[i]]
-
-    blocks.append(current_block)  # 마지막 블록 추가
-    return blocks
-
-def move_blocks(arr, blocks):
-    move_count = 0
-    for block in blocks:
-        move_count += len(block)
-        # 여기서 블록을 실제로 이동시키는 작업이 필요합니다.
-        # 정렬 여부를 확인합니다. 이 부분은 문제 요구 사항에 따라 구현할 수 있습니다.
-        # 예시로 각 블록을 끝으로 보낸다고 가정합니다.
-        for num in block:
-            arr.remove(num)
-        arr.extend(block)  # 블록을 뒤에 추가
-
-    return move_count
-
 n = int(input())
 arr = list(map(int, input().split()))
+new_arr = []
 
-blocks = group_blocks(arr)  # 블록으로 그룹화
-move_count = move_blocks(arr, blocks)  # 블록을 이동시키며 카운트 계산
+temp_arr = []
 
-print(move_count - 1)
+# 연속된 숫자를 블록으로 그룹화
+for i in range(n-1):
+    temp_arr.append(arr[i])
+    if arr[i] != arr[i+1] - 1:
+        new_arr.append(temp_arr)
+        temp_arr = []
+temp_arr.append(arr[n-1])  # 마지막 요소 처리
+new_arr.append(temp_arr)
+
+print("초기 블록:", new_arr)
+
+ans = 0
+
+while True:
+    moved = False
+    # 첫 번째 블록을 꺼냄
+    first_block = new_arr.pop(0)
+
+    # 어디에 삽입할지 적절한 위치를 찾음
+    inserted = False
+    for i in range(len(new_arr)):
+        # 만약 현재 블록의 첫 번째 값이 첫 번째 블록의 마지막 값보다 크다면
+        if new_arr[i][0] > first_block[-1]:
+            new_arr.insert(i+1, first_block)  # 적절한 위치에 삽입
+            ans += len(first_block)
+            inserted = True
+            moved = True
+            # print("블록 이동:", new_arr)
+            break
+
+    # 만약 적절한 위치를 찾지 못했다면 맨 뒤에 추가
+    if not inserted:
+        new_arr.append(first_block)
+        ans += len(first_block)
+        moved = True
+        # print("블록 이동 (맨 뒤로):", new_arr)
+        break
+
+    # 더 이상 이동할 필요가 없으면 종료
+    if not moved:
+        break
+
+# print("최종 정렬:", new_arr)
+# print("이동 횟수:", ans)
+
+print(ans)
