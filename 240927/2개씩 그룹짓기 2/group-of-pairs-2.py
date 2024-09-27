d20647613feecbed5e62be6a43bcc97da52b12eb
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-27 13:33:08 +0900

    Title: 2개씩 그룹짓기 2, Time: 199ms, Memory: 40MB, Status: Passed - CodeTree

--- 240927/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240927/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -3,14 +3,10 @@ arr = list(map(int, input().split()))
 
 arr.sort()
 
-# arr = [num - arr[0] for num in arr]
-
-# print(arr)
 
 ans = float('inf')
 for i in range(n):
     temp = arr[n + i] - arr[i]
-    # print(arr[n+i], arr[i], temp)
     ans = min(temp, ans)
 
 print(ans)
