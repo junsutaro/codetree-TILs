51558543458eb30a87b1b8d685ce0c32f89613d2
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-27 11:17:41 +0900

    Title: 홀수 짝수의 묶음, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240927/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240927/홀수 짝수의 묶음/odd-even-bundle.py
@@ -5,37 +5,24 @@ arr = list(map(int, input().split()))
 odd_count = sum(1 for num in arr if num % 2 == 1)
 even_count = n - odd_count
 
-ans = 0  # 그룹의 수
-parity = 0  # 현재 필요한 합의 패리티 (0: 짝수, 1: 홀수)
-
+group_num = 0
 while True:
-    if parity == 0:
-        # 짝수 합을 만들어야 함
-        if even_count > 0:
+    if group_num % 2 == 0:
+        if even_count:
             even_count -= 1
-            ans += 1
-            parity = 1
-        elif odd_count >= 2:
-            odd_count -= 2
-            ans += 1
-            parity = 1
+            group_num += 1
+        elif odd >= 2:
+            odd -= 2
+            group_num += 1
         else:
+            if even_count > 0 or odd_count > 0:
+                group_num -= 1
             break
     else:
-        # 홀수 합을 만들어야 함
-        if odd_count > 0:
-            odd_count -= 1
-            ans += 1
-            parity = 0
+        if odd:
+            odd -= 1
+            group_num += 1
         else:
             break
-
-# 남은 숫자가 있다면 그룹 수를 조정하여 모든 숫자를 사용
-remaining_numbers = odd_count + even_count
-if remaining_numbers > 0:
-    if ans > 0:
-        ans -= 1  # 마지막 그룹에 남은 숫자를 포함
-    else:
-        ans = 1  # 모든 숫자를 한 그룹으로 묶음
-
-print(ans)
+    
+print(group_num)
