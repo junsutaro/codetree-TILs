5ba6e709862152ab8d43b42b0bb2b5e5ae8e2f05
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-27 13:11:31 +0900

    Title: 수열의 순서 바꾸기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240927/수열의 순서 바꾸기/reorder-sequence.py
+++ 240927/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,13 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-
-
 ans = 0
 
 for i in range(n-2, -1, -1):
     if arr[i] > arr[i+1]:
         ans = i + 1
-
+        break
 
 print(ans)
