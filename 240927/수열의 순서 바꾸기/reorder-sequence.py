86cf41f64e05e64cba1590d2d6151d94af3d33e4
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-27 11:54:57 +0900

    Title: 수열의 순서 바꾸기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240927/수열의 순서 바꾸기/reorder-sequence.py
+++ 240927/수열의 순서 바꾸기/reorder-sequence.py
@@ -9,12 +9,14 @@ ans = 0
 # 1부터 순서대로 i+1이 어디있냐... 찾다가? 찾았다! 근데 i가 i+1보다 작으면 
 # (5인덱스가 4보다 작으면) 그인덱스+1만큼 보내 
 
-for i in range(n-1):
+for i in range(n):
     # 4찾을차례 -> j가 돌면서 i위치가 4인곳을 찾음. 근데 걔(i)가 4보다 작잖아
     for j in range(1, n+1):
         if arr[i] == j:
             if i < arr[i]:
                 ans = i + 1
 
-
+if ans == n:
+    ans = 0
+    
 print(ans)
