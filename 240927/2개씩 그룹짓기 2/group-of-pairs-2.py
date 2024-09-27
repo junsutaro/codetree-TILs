6b6f42c31dc2a915a36fa0fb8dcebf7b7f2b126b
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-27 13:29:57 +0900

    Title: 2개씩 그룹짓기 2, Time: 198ms, Memory: 40MB, Status: Passed - CodeTree

--- 240927/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240927/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -1,14 +1,16 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-# 배열을 정렬
 arr.sort()
 
-# 각 그룹 내 두 수의 차이 중 최솟값이 최대가 되도록 설정
+arr = [num - arr[0] for num in arr]
+
+# print(arr)
+
 ans = float('inf')
-for i in range(0, 2 * n, 2):
-    # 인접한 두 수끼리 묶어서 차이를 계산
-    temp = arr[i + 1] - arr[i]
+for i in range(n):
+    temp = arr[n + i] - arr[i]
+    # print(arr[n+i], arr[i], temp)
     ans = min(temp, ans)
 
 print(ans)
