1f3b567035febd81a5b16dc188bbd9352eb5c37f
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-29 10:54:12 +0900

    Title: 금 채굴하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240929/금 채굴하기/gold-mining.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+def return_mine(i, j):
+    for K in range(n):
+        cnt = 0
+        for ni in range(i+K):
+            for nj in range(j+K):
+                if ni < n and nj < n:
+                    if abs(ni - i) + abs(nj - j) <= K and arr[ni][nj] == 1:
+                        cnt +=1
+        
+        if m*cnt - (K*K + (K+1)*(K+1)) >= 0:
+            return cnt
+    return 0
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        ans = max(ans, return_mine(i, j))
+
+print(ans)
