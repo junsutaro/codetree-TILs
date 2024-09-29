621e4be78a952c2d1e726ff103970a0a0ec2e8eb
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-29 10:56:32 +0900

    Title: 금 채굴하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240929/금 채굴하기/gold-mining.py
+++ 240929/금 채굴하기/gold-mining.py
@@ -5,8 +5,8 @@ def return_mine(i, j):
     for K in range(n):
         cnt = 0
         for ni in range(i-K, i+K + 1):
-            for nj in range(j-Km j+K+1):
-                if ni < n and nj < n:
+            for nj in range(j-K, j+K+1):
+                if 0 <= ni < n and 0 <= nj < n:
                     if abs(ni - i) + abs(nj - j) <= K and arr[ni][nj] == 1:
                         cnt +=1
         
