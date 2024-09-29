ff9a0e0cf0d8bf88f5187353e69c611fb7f419ff
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-29 09:47:13 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240929/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240929/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,33 +1,32 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
+def is_happy_sequence(arr, m):
+    count = 1
+    for i in range(len(arr) - 1):
+        if arr[i] == arr[i + 1]:
+            count += 1
+        else:
+            count = 1
+        if count == m:
+            return True
+    return False
+
+
 ans = 0
 
-# i 고정하고 세로로 봅시다
-for i in range(n):
-    cnt = 1
-    for j in range(n-1):
-        if arr[i][j] == arr[i][j+1]:
-            cnt += 1
-        else:
-            cnt = 1
-        if cnt == m:
-            ans += 1
-            break
-
-# j 고정하고 가로로 봅시다
-for j in range(n):
-    cnt = 1
-    for i in range(n-1):
-        if arr[i][j] == arr[i+1][j]:
-            cnt += 1
-        else:
-            cnt = 1
-        if cnt == m:
-            ans += 1
-            break
-
 if n == 1:
-    ans = 2
+    print(2)
+    break
+
+# 가로
+for i in range(n):
+    if is_happy_sequence(arr[i], m):
+        ans += 1
+
+for j in range(n):
+    col_arr = [arr[i][j] for i in range(n)]
+    if is_happy_sequence(col_arr, m):
+        ans += 1
 
 print(ans)
