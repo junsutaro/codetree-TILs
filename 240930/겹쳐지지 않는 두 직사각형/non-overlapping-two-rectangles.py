f3ca1a438a28d7632e77d4481f165fb42c321f3e
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-30 11:59:46 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 535ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240930/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240930/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -6,45 +6,36 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 # a1 < a2 가정하고 not (c1 > a2 and d1 > b2)
 # c2 >= a2 , d2 >= b2
 
-for i in range(n-1):
-    for j in range(i+1, n):
-        continue
-
 # a1, a2 조합 찾기
 import itertools
-abcombs = list(itertools.product(range(n), repeat=2))
+combinations = itertools.product(range(n), range(m), range(n), range(m), range(n), range(m), range(n), range(m))
+
 
 ans = -float('inf')
+cnt = 0
+for a1, b1, c1, d1, a2, b2, c2, d2 in combinations:
+        if c1 > a2 and d1 > b2:
+            continue
+        if a1 >= a2 and b1 >= b2:
+            continue
+        if c1 >= c2 and d1 >= d2:
+            continue
+        
+        if a1 > c1 or b1 > d1 or a2 > c2 or b2 > d2:
+            continue
 
-# a2, a2 찾았으면 b1, b2도 찾아야 함.. 일단찾자
-for comb in abcombs:
-    a1, a2 = comb
-    for b1 in range(m):
-        for b2 in range(m):
-            # a1, b1 a2, b2 찾았습니다~
-            # 그러면 c1, d1 이랑 c2, d2 찾아야겠죠
-            for c1 in range(a1, n):
-                for d1 in range(b1, m):
-                    # c1, d1 찾았으니까 이제 c2, d2
-                    for c2 in range(a2, n):
-                        for d2 in range(b2, m):
-                            if a1 == a2 and b1 == b2:
-                                continue
-                            if (c1 > a2 and d1 > b2):
-                                continue
-                            # print(a1, b1, ',' ,c1, d1)
-                            # print(a2, b2, ',' ,c2, d2)
-                            temp = 0
-                            for i in range(a1, c1+1):
-                                for j in range(b1, d1+1):
-                                    temp += arr[i][j]
-                            
-                            for i in range(a2, c2+1):
-                                for j in range(b2, d2+1):
-                                    temp += arr[i][j]
-                            
-                            ans = max(ans, temp)
-                            # print(temp)
-                            # print()
+        temp = 0
+
+        for i in range(a1, c1 + 1):
+            for j in range(b1, d1 + 1):
+                temp += arr[i][j]
+        for i in range(a2, c2 + 1):
+            for j in range(b2, d2 + 1):
+                temp += arr[i][j]
+        
+        cnt += 1
+        # print(a1, b1, c1, d1)
+        # print(a2, b2, c2, d2)
+        ans = max(ans, temp)
 
 print(ans)
