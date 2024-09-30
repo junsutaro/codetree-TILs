fd1dd9085a3199633acddd7b5c4545154d808e6c
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-30 12:06:41 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 446ms, Memory: 29MB, Status: Passed - CodeTree

--- 240930/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240930/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -14,7 +14,7 @@ combinations = itertools.product(range(n), range(m), range(n), range(m), range(n
 ans = -float('inf')
 cnt = 0
 for a1, b1, c1, d1, a2, b2, c2, d2 in combinations:
-        if c1 > a2 and d1 > b2:
+        if c1 >= a2 and d1 >= b2:
             continue
         if a1 >= a2 and b1 >= b2:
             continue
@@ -34,8 +34,8 @@ for a1, b1, c1, d1, a2, b2, c2, d2 in combinations:
                 temp += arr[i][j]
         
         cnt += 1
-        # print(a1, b1, c1, d1)
-        # print(a2, b2, c2, d2)
+        # print(a1, b1, ',', c1, d1)
+        # print(a2, b2, ',', c2, d2)
+        # print()
         ans = max(ans, temp)
-
 print(ans)
