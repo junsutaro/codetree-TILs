6c5e8595ab2584b27b29708afc35bb5152fb1fec
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-09-30 12:54:57 +0900

    Title: 양수 직사각형의 최대 크기, Time: 224ms, Memory: 27MB, Status: Passed - CodeTree

--- 240930/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240930/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,11 +1,11 @@
 n, m = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(n)]
-
+# arr = [[6, -2, 4, -3, 1,], [3, 6, 7, -4, 1],[6, 1, 8, 15, -5],[3, -5, 1, 16, 3]]
 
 def all_plus(arr, a1, b1, a2, b2):
-    for i in range(a1, b1+1):
-        for j in range(a2, b2+1):
+    for i in range(a1, a2+1):
+        for j in range(b1, b2+1):
             if arr[i][j] <= 0:
                 return False
     return True
@@ -19,8 +19,8 @@ for a1, b1, a2, b2 in combs:
     if a1 > a2 or b1 > b2:
         continue
 
-
     if all_plus(arr, a1, b1, a2, b2):
+        # print(a1, b1, ',', a2, b2)
         temp = (b2 + 1 - b1) * (a2 + 1 - a1)
 
         ans = max(ans, temp)
