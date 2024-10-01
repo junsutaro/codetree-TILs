938171e7a0989c8a14d3f62961f65ed5545c8692
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-01 15:22:04 +0900

    Title: 기울어진 직사각형의 회전, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 241001/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 241001/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,31 +1,21 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-r, c, m1, m2, m3, m4, dir = map(int, input().split())
+r, c, m1, m2, m3, m4, start_dir = map(int, input().split())
 
 arr_copy = [elem[:] for elem in arr]
 
-# for elem in arr_copy:
-#     print(*elem)
-# print()
-
 r -= 1
 c -= 1
-
 # dir = 0 반시계, dir = 1 시계
 
-cur_dir = 0
-
 di = [-1, -1, 1, 1]
-if dir == 0:
-    dj = [1, -1, -1, 1]
-else:
-    dj = [-1, 1, 1, -1]
+dj = [1, -1, -1, 1]
 
 i = r; j = c
-
+cur_dir = 0
 while cur_dir < 4:
 
-    if cur_dir == 0:
+    if cur_dir == 0: # 첫번째 방향으로
         dist = m1
     elif cur_dir == 1:
         dist = m2
@@ -38,12 +28,16 @@ while cur_dir < 4:
         ni = i + di[cur_dir]
         nj = j + dj[cur_dir]
 
-        arr[ni][nj] = arr_copy[i][j]
+        if start_dir == 0:
+            arr[ni][nj] = arr_copy[i][j]
+        else:
+            arr[i][j] = arr_copy[ni][nj]
 
         i = ni
         j = nj
-    
+
     cur_dir += 1
 
 for lst in arr:
     print(*lst)
+print()
