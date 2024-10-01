cdb4cbdd972e831b306370768376090e197d6fc4
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-01 21:27:23 +0900

    Title: 단 한 번의 2048 시도, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 241001/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -1,60 +1,56 @@
+def rotate_90_clockwise(arr):
+    return [[arr[3-j][i] for j in range(4)] for i in range(4)]
+
+def rotate_90_counterclockwise(arr):
+    return [[arr[j][3-i] for j in range(4)] for i in range(4)]
+
+def rotate_180(arr):
+    return [[arr[3-i][3-j] for j in range(4)] for i in range(4)]
+
+def move_left(arr):
+    new_arr = [[0] * 4 for _ in range(4)]
+    for i in range(4):
+        temp = []
+        for j in range(4):
+            if arr[i][j] != 0:
+                temp.append(arr[i][j])
+        
+        # 숫자 합치기
+        k = 0
+        while k < len(temp) - 1:
+            if temp[k] == temp[k + 1]:
+                temp[k] *= 2
+                del temp[k + 1]
+            k += 1
+        
+        # 합친 숫자 왼쪽으로 이동
+        for j in range(len(temp)):
+            new_arr[i][j] = temp[j]
+    
+    return new_arr
+
 arr = [list(map(int, input().split())) for _ in range(4)]
 dir = input()
 
-# 배열을 회전하기 위한 작업
-new_arr = [[0] * 4 for _ in range(4)]
+# 방향에 따라 회전
 if dir == 'U':
-    for i in range(4):
-        for j in range(4):
-            new_arr[i][j] = arr[j][3-i]  # 시계 반대 방향으로 90도
+    arr = rotate_90_clockwise(arr)
 elif dir == 'R':
-    for i in range(4):
-        for j in range(4):
-            new_arr[i][j] = arr[3-i][3-j]  # 180도 회전
+    arr = rotate_180(arr)
 elif dir == 'D':
-    for i in range(4):
-        for j in range(4):
-            new_arr[i][j] = arr[3-j][i]  # 시계 방향으로 90도
-else:
-    new_arr = arr  # 방향이 L인 경우는 회전 없음
+    arr = rotate_90_counterclockwise(arr)
 
-# 싹 다 왼쪽으로 붙이는 작업
-ans = [[0] * 4 for _ in range(4)]
-for i in range(4):
-    temp = []
-    for j in range(4):
-        if new_arr[i][j] != 0:  # 0이 아닌 숫자만 임시 배열에 모음
-            temp.append(new_arr[i][j])
+# 왼쪽으로 이동하는 함수 호출
+arr = move_left(arr)
 
-    # 숫자가 같은 경우 합치기
-    k = 0
-    while k < len(temp) - 1:
-        if temp[k] == temp[k + 1]:
-            temp[k] *= 2
-            del temp[k + 1]  # 합친 후 그 다음 숫자 제거
-        k += 1
-
-    # 왼쪽으로 붙이고 오른쪽에 0을 채워서 4칸 맞춤
-    for j in range(len(temp)):
-        ans[i][j] = temp[j]
-
-# 처리 후 다시 원래 방향으로 회전
-ANS = [[0] * 4 for _ in range(4)]
+# 다시 원래 방향으로 되돌림
 if dir == 'U':
-    for i in range(4):
-        for j in range(4):
-            ANS[i][j] = ans[j][3-i]  # 다시 시계 방향 90도 돌림
+    arr = rotate_90_counterclockwise(arr)
 elif dir == 'R':
-    for i in range(4):
-        for j in range(4):
-            ANS[i][j] = ans[3-i][3-j]  # 180도 회전
+    arr = rotate_180(arr)
 elif dir == 'D':
-    for i in range(4):
-        for j in range(4):
-            ANS[i][j] = ans[3-j][i]  # 시계 반대 방향 90도 돌림
-else:
-    ANS = ans  # L인 경우는 그대로
+    arr = rotate_90_clockwise(arr)
 
 # 결과 출력
-for elem in ANS:
+for elem in arr:
     print(*elem)
