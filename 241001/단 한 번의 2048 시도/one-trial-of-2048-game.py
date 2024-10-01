081e40d250b4503897e3c02c8fee359262238337
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-01 21:26:20 +0900

    Title: 단 한 번의 2048 시도, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 241001/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -1,54 +1,44 @@
 arr = [list(map(int, input().split())) for _ in range(4)]
 dir = input()
 
-# 싹 다 왼쪽으로 갖다붙인다고 가정하고, 
-# U면 90도, R이면 180도, D면 270도 왼쪽으로 돌림
-# 돌리고 풀고나서 다시 오른쪽으로 돌려
-
+# 배열을 회전하기 위한 작업
 new_arr = [[0] * 4 for _ in range(4)]
 if dir == 'U':
     for i in range(4):
         for j in range(4):
-            new_arr[i][j] = arr[3-j][3-i]
+            new_arr[i][j] = arr[j][3-i]  # 시계 반대 방향으로 90도
 elif dir == 'R':
     for i in range(4):
         for j in range(4):
-            new_arr[i][j] = arr[3-i][3-j]
+            new_arr[i][j] = arr[3-i][3-j]  # 180도 회전
 elif dir == 'D':
     for i in range(4):
         for j in range(4):
-            new_arr[i][j] = arr[3-j][i]
+            new_arr[i][j] = arr[3-j][i]  # 시계 방향으로 90도
+else:
+    new_arr = arr  # 방향이 L인 경우는 회전 없음
 
+# 싹 다 왼쪽으로 붙이는 작업
 ans = [[0] * 4 for _ in range(4)]
 for i in range(4):
-    cnt = 0
     temp = []
     for j in range(4):
-
-        if new_arr[i][j] == 0:
-            continue
-
-        cnt += 1
-
-        if j == 3:
-            if cnt == 1:
-                temp.append(new_arr[i][j])
-            else:
-                temp.append(new_arr[i][j] * 2)
-
-        elif cnt == 2:
-            cnt = 0
-            temp.append(new_arr[i][j] * 2)
-
-        elif new_arr[i][j] != new_arr[i][j+1]:
-            cnt = 0
+        if new_arr[i][j] != 0:  # 0이 아닌 숫자만 임시 배열에 모음
             temp.append(new_arr[i][j])
 
+    # 숫자가 같은 경우 합치기
+    k = 0
+    while k < len(temp) - 1:
+        if temp[k] == temp[k + 1]:
+            temp[k] *= 2
+            del temp[k + 1]  # 합친 후 그 다음 숫자 제거
+        k += 1
 
+    # 왼쪽으로 붙이고 오른쪽에 0을 채워서 4칸 맞춤
     for j in range(len(temp)):
         ans[i][j] = temp[j]
 
-
+# 처리 후 다시 원래 방향으로 회전
 ANS = [[0] * 4 for _ in range(4)]
 if dir == 'U':
     for i in range(4):
@@ -65,6 +55,6 @@ elif dir == 'D':
 else:
     ANS = ans  # L인 경우는 그대로
 
-
+# 결과 출력
 for elem in ANS:
     print(*elem)
