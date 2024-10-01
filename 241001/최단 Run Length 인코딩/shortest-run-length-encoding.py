2fb475e4dd9fca8a76a506d14d91cfb1e71baecd
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-01 15:43:16 +0900

    Title: 최단 Run Length 인코딩, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 241001/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -5,28 +5,29 @@ ans = float('inf')
 
 def find_len(A):
     global ans
-    
-    temp = []
+
+    temp = ''
     
     cnt = 1
     for i in range(1, len(A)):
         now = A[i-1]
         if i == len(A) - 1:
             cnt += 1
-            temp.append(now)
-            temp.append(cnt)
+            temp += now
+            temp += str(cnt)
         if A[i] != A[i-1]:
-            temp.append(now)
-            temp.append(cnt)
+            temp += now
+            temp += str(cnt)
             cnt = 1
         else:
             cnt += 1
-    
+    # print(temp)
     ans = min(ans, len(temp))
 
 for _ in range(length):
     last = A.pop(0)
     A.append(last)
+
     find_len(A)
 
 print(ans)
