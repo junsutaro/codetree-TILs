1de79223adb89c26f647c3e39f276492e1534fa5
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-01 20:23:16 +0900

    Title: 1차원 폭발 게임, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/1차원 폭발 게임/The-1D-bomb-game.py
+++ 241001/1차원 폭발 게임/The-1D-bomb-game.py
@@ -7,9 +7,10 @@ while explode_happen:
     new_bombs = [] # m개 안넘는놈들 담아놓을거
     explode_happen = False
     temp = [] # 폭탄 실시간으로 m개 넘는지 체크하고 비울거
-    i = 0
-
-    while i < len(bombs):
+    
+    for i in range(len(bombs)):
+    # i = 0
+    # while i < len(bombs):
         temp.append(bombs[i])
 
         if i == len(bombs) - 1 or bombs[i] != bombs[i+1]:
@@ -20,7 +21,7 @@ while explode_happen:
             else:
                 new_bombs.append(temp)
                 temp = []
-        i += 1
+        # i += 1
 
     bombs = []
     for elem in new_bombs:
