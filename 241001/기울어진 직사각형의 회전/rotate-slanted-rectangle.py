969511ddbe992c6381515409bd01822ed1347b14
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-01 14:55:45 +0900

    Title: 기울어진 직사각형의 회전, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241001/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -0,0 +1,52 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+r, c, m1, m2, m3, m4, dir = map(int, input().split())
+
+arr_copy = [elem[:] for elem in arr]
+
+# for elem in arr_copy:
+#     print(*elem)
+# print()
+
+r -= 1
+c -= 1
+
+# dir = 0 반시계, dir = 1 시계
+
+cur_dir = 0
+di = [-1, -1, 1, 1]
+dj = [1, -1, -1, 1]
+
+i = r; j = c
+cnt = 0
+while cnt < 4:
+
+    dist = 0
+
+    if cur_dir == 0:
+        dist = m1
+    elif cur_dir == 1:
+        dist = m2
+    elif cur_dir == 2:
+        dist = m3
+    else:
+        dist = m4
+
+    for _ in range(dist):
+        ni = i + di[cur_dir]
+        nj = j + dj[cur_dir]
+
+        arr[ni][nj] = arr_copy[i][j]
+
+        i = ni
+        j = nj
+    
+    if dir == 0:
+        cur_dir += 1
+    else:
+        cur_dir = (cur_dir + 3) % 4
+    
+    cnt += 1
+
+for lst in arr:
+    print(*lst)
