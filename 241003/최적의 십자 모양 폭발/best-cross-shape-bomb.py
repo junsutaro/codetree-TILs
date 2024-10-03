f8356b9bf3c8474d6b53bfc8c03a70e6a71ba9e3
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-03 16:03:57 +0900

    Title: 최적의 십자 모양 폭발, Time: 311ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 241003/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -0,0 +1,105 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+
+def explode(arr, i, j): # 인덱스로 사용
+    di = [-1, 0, 1, 0]
+    dj = [0, 1, 0, -1]
+    size = arr[i][j] - 1
+    
+    arr[i][j] = 0
+
+    for dir in range(4):
+        for damage in range(size):
+            ni = i + di[dir];
+            nj = j + dj[dir]
+
+            if ni >= n or ni < 0 or nj >= n or nj < 0:
+                continue
+
+            arr[ni][nj] = 0
+
+
+
+def gravity(arr):
+    for j in range(n):
+        index = n-1
+        for i in range(n-1, -1, -1):
+            if arr[i][j] != 0:
+                arr[index][j] = arr[i][j]
+                index -= 1
+            
+        for i in range(index+1):
+            arr[i][j] = 0
+
+
+def find_ans(arr):
+    cnt = 0
+
+    for i in range(n):
+        j = 0
+        temp_cnt = 1
+        while True:
+            if j == n-1:
+                if temp_cnt == 2:
+                    cnt += 1
+
+                break
+
+            if arr[i][j] == 0:
+                j += 1
+                continue
+
+
+            if arr[i][j] == arr[i][j+1]:
+                temp_cnt += 1
+            
+            else:
+                if temp_cnt == 2:
+                    cnt += 1
+
+                temp_cnt = 1
+            
+            j += 1
+
+    for j in range(n):
+        i = 0
+        temp_cnt = 1
+        while True:
+            if i == n-1:
+                if temp_cnt == 2:
+                    cnt += 1
+
+                break
+
+            if arr[i][j] == 0:
+                i += 1
+                continue
+
+
+            if arr[i][j] == arr[i+1][j]:
+                temp_cnt += 1
+            
+            else:
+                if temp_cnt == 2:
+                    cnt += 1
+
+                temp_cnt = 1
+            
+            i += 1
+
+
+
+    return cnt
+
+ans = 0
+
+
+for i in range(n):
+    for j in range(n):
+        new_arr = [elem[:] for elem in arr]
+        explode(new_arr, i, j)
+        gravity(new_arr)
+        ans = max(ans, find_ans(new_arr))
+        
+print(ans)
