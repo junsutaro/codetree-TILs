186d063a94879894269b772abfac3625bd17d9e4
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-03 17:27:27 +0900

    Title: 떨어지는 1자 블록, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241003/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241003/떨어지는 1자 블록/falling-horizontal-block.py
@@ -2,24 +2,22 @@ n, m, k = map(int, input().split())
 k -= 1 # 인덱스화
 arr = [list(map(int, input().split())) for _ in range(n)]
 
+
 # block : for j in range(k, k+m) i가 n-1 -1 -1 로 돌면서 True? 빢! break
 
-for i in range(n-1, -1, -1):
-    can = True
+for i in range(1, n):
+    can = False
     for j in range(k, k+m):
-        # 여기서 걸리면 i 바꾸러 감
+
+        # 내려가면서 처음 걸리면?
         if arr[i][j] == 1:
-            can = False
-            break
+            can = True
+            for l in range(k, k+m):
+                arr[i-1][l] = 1
+            
+    if can:
+        break
 
-    if can == False:
-        continue
-    
-    # 안걸리고 도착하면 걍 끝냄
-    for j in range(k, k+m):
-        arr[i][j] = 1
-
-    break
 
 for elem in arr:
     print(*elem)
