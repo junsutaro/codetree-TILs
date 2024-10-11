0be1e49b91479ab95f6499c11f74e5b353b4ba3d
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-11 16:56:37 +0900

    Title: 대폭발, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 241011/대폭발/big-explosion.py
+++ 241011/대폭발/big-explosion.py
@@ -7,10 +7,12 @@ c -= 1
 grid = [[0] * n for _ in range(n)]
 grid[r][c] = 1
 
+bombs = [[r, c]]
 # 북 서 남 동
 dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 def make_bomb(time, i, j):
+    new_bombs = []
     for k in range(4):
         di, dj = dirs[k]
         ni = i + di * 2**(time-1)
@@ -18,20 +20,18 @@ def make_bomb(time, i, j):
 
         if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] == 0:
             grid[ni][nj] = 1  # 새로운 폭탄 생성
+            new_bombs.append((ni, nj))
+
+    return new_bombs
 
 # m번의 시간 동안 폭탄 확산
 for time in range(1, m + 1):
-    bombs = []
-
-    # 현재 폭탄이 있는 위치를 탐색
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == 1:
-                bombs.append([i, j])
-
+    new_bombs = []
     # 폭탄 확산
     for i, j in bombs:
-        make_bomb(time, i, j)
+        new_bombs.extend(make_bomb(time, i, j))
+    
+    bombs.extend(new_bombs)
 
 # 최종 폭탄의 개수를 계산
 ans = sum(sum(row) for row in grid)
