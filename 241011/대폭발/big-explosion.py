48173a1d86b68e747376c52d4d9b7688a93e08b9
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-11 16:22:52 +0900

    Title: 대폭발, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241011/대폭발/big-explosion.py
@@ -0,0 +1,47 @@
+n, m, r, c = map(int, input().split())
+
+# 인덱스화
+r -= 1
+c -= 1
+
+grid = [[0] * n for _ in range(n)]
+grid[r][c] = 1
+
+# 북 서 남 동
+dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+
+def make_bomb(time, i, j):
+    # grid에 대한 로직
+    
+    for k in range(4):
+        di, dj = dirs[k]
+        ni = i + di*time
+        nj = j + dj*time
+
+        if not ( 0 <= ni < n and 0 <= nj < n ):
+            continue
+        
+        grid[ni][nj] = 1
+
+
+
+for time in range(1, m+1):
+    
+    bombs = []
+
+    for i in range(n):
+        for j in range(n):
+            # 폭탄 찾아서 make bomb
+            if grid[i][j] == 1:
+                bombs.append([i, j])
+    
+    for i, j in bombs:
+        make_bomb(time, i, j)
+
+    # print()
+    # for elem in grid:
+    #     print(*elem) 
+
+ans = sum(sum(elem) for elem in grid)
+print(ans)
