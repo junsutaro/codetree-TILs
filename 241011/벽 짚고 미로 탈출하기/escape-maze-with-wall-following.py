0f5cb3f34301fc0223bb08c283c41b3df8d6cb4a
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-11 14:37:33 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241011/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 241011/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,7 +1,14 @@
-n = int(input())
-start_i, start_j = map(int, input().split())
-temp = [list(input()) for _ in range(n)]
-
+# n = int(input())
+# start_i, start_j = map(int, input().split())
+# temp = [list(input()) for _ in range(n)]
+n = 5
+start_i = 1
+start_j = 1
+temp = [['.', '.', '.', '.', '.'], 
+        ['#', '#', '#', '#', '#'], 
+        ['.', '.', '.', '.', '.'], 
+        ['.', '.', '.', '.', '.'], 
+        ['.', '.', '.', '.', '.']]
 
 arr = [[0] * (n+2) for _ in range(n+2)]
 visited = [[0] * (n+2) for _ in range(n+2)]
@@ -31,6 +38,7 @@ while True:
 
     if dir_cnt == 4:
         ans = -1
+        print('4번바꿈!')
         break
 
     next = arr[i+di[dir]][j+dj[dir]]
@@ -51,36 +59,46 @@ while True:
         ans = time
         break
 
-    # 그 둘다 아니면? 간 후에 
+    # 그 둘다 아니면? 간 후에 방문체크
     ni = i + di[dir]
     nj = j + dj[dir]
-    time += 1
-    
-    # 오른쪽에 벽이 있나 체크
-    # 그 오른쪽 : 가서 오른쪽으로 한칸 더 간 위치.
-    # dir을 직접 바꾸지 말자 가기전에 체크만 하는거임
-    ni_right = i + di[(dir + 3) % 4]
-    nj_right = j + dj[(dir + 3) % 4]
-    
-    # 벽이 없으면?
-    if arr[ni_right][nj_right] == '.':
-        # 가고 dir도 바꿔
-        ni = ni_right
-        nj = nj_right
-        dir = (dir + 3) % 4
-        dir_cnt += 1
-    # 벽이 있으면? 그대로니까 넘어감
-    # else: pass
-
-    # 방문할곳이 처음이면 가고 누가 방문했었으면 -1하고 종료. 뻉뺑이임
-    # 아니면 visited를 전부 배열로 만들고 도착당시의 방향을 기록하고, not in 일때 추가 이미 있으면 종료 ( 하기싫음 ! )
     if visited[ni][nj] == 0:
         visited[ni][nj] = 1
-        dir_cnt = 0
+        time += 1
     else:
         ans = -1
         break
+    
+    # 간 후에 오른쪽에 벽이 있나 체크
+    # 그 오른쪽 : 가서 오른쪽으로 한칸 더 간 위치.
+    # dir을 직접 바꾸지 말자 가기전에 체크만 하는거임
+    ni_right = ni + di[(dir + 3) % 4]
+    nj_right = nj + dj[(dir + 3) % 4]
+    
+    # 벽이 없으면?
+    if arr[ni_right][nj_right] == '.':
+        # visited 체크하고 안간곳이면
+        if visited[ni_right][nj_right] == 0:
+            # 가고 방문체크 후 dir도 바꿔
+            ni = ni_right
+            nj = nj_right
 
+            visited[ni][nj] = 1
+            dir = (dir + 3) % 4
+            dir_cnt += 1
+
+
+        # 갔던곳이면? 탈출못하는거지?
+        else:
+            ans = -1
+            break
+
+    # 벽이 있으면? 그대로니까 넘어감
+    # else: pass
+
+    # while 돌기전에 i, j 최신화
+    i = ni
+    j = nj
 # for elem in arr:
 #     print(*elem)
 # print()
