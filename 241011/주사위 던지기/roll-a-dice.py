f23a0dd1f83925f6164a1954d378dd1aecbd799f
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-11 15:55:17 +0900

    Title: 주사위 던지기, Time: 200ms, Memory: 26MB, Status: Passed - CodeTree

--- 241011/주사위 던지기/roll-a-dice.py
+++ 241011/주사위 던지기/roll-a-dice.py
@@ -1,9 +1,13 @@
 n, m, r, c = map(int, input().split())
+dirs = input().split()
+
+# [n, m, r, c] = [4, 4, 2, 3]
+# dirs = ['L', 'L', 'L', 'L']
+
 r -= 1
 c -= 1
 
 # m번 굴림
-dirs = input().split()
 
 # 주사위 펼치기
 #      위 북 서 남 동 아래
@@ -22,31 +26,35 @@ def rolling(direction):
     dice = [dice[i] for i in transforms[direction]]
 
 grid = [[0] * n for _ in range(n)]
+grid[r][c] = 6
 
 i = r
 j = c 
 
 for direction in dirs:
     if direction == 'L':
-        if j - 1 >= 0:
-            rolling(direction)
-
-            j -= 1
+        if j - 1 < 0:
+            continue
+        j -= 1
     elif direction == 'R':
-        if j + 1 < n:
-            rolling(direction)
-            j += 1
+        if j + 1 >= n:
+            continue
+        j += 1
     elif direction == 'D':
-        if i + 1 < n:
-            rolling(direction)
-            i += 1
+        if i + 1 >= n:
+            continue
+        i += 1
     else:
-        if i - 1 >= 0:
-            rolling(direction)
-            i -= 1
+        if i - 1 < 0:
+            continue
+        i -= 1
 
+    rolling(direction)
     grid[i][j] = dice[5]
 
+
+# for elem in grid:
+#     print(*elem)
 
 ans = sum(sum(elem) for elem in grid)
 print(ans)
