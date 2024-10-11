cb1aae756feb4b3d50d01e0f43d79398b272c25e
JUNSUTARO <139520011+LEEJUNSU11@users.noreply.github.com>
2024-10-11 16:49:26 +0900

    Title: 대폭발, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241011/대폭발/big-explosion.py
+++ 241011/대폭발/big-explosion.py
@@ -10,28 +10,28 @@ grid[r][c] = 1
 # 북 서 남 동
 dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
+# 처음 폭탄 위치 저장
+bombs = [[r, c]]  # 폭탄의 초기 위치
+
 def make_bomb(time, i, j):
+    new_bombs = []
     for k in range(4):
         di, dj = dirs[k]
         ni = i + di * time
         nj = j + dj * time
 
         if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] == 0:
-            grid[ni][nj] = 1  # 새로운 폭탄 생성
+            grid[ni][nj] = 1
+            new_bombs.append([ni, nj])  # 새로운 폭탄 좌표 추가
+    return new_bombs
 
 # m번의 시간 동안 폭탄 확산
 for time in range(1, m + 1):
-    bombs = []
-
-    # 현재 폭탄이 있는 위치를 탐색
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == 1:
-                bombs.append([i, j])
-
-    # 폭탄 확산
+    new_bombs = []
     for i, j in bombs:
-        make_bomb(time, i, j)
+        # 기존 폭탄도 다시 터지도록 처리
+        new_bombs.extend(make_bomb(time, i, j))
+    bombs.extend(new_bombs)  # 새로 생긴 폭탄 좌표도 추가하여 반복 폭발 가능하게 함
 
 # 최종 폭탄의 개수를 계산
 ans = sum(sum(row) for row in grid)
