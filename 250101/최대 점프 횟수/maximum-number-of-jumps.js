52980aacf449ef232e9cff07c39d837ffd285faa
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-01 23:17:58 +0900

    Title: 최대 점프 횟수, Time: 160ms, Memory: 15MB, Status: Passed - CodeTree

--- 250101/최대 점프 횟수/maximum-number-of-jumps.js
+++ 250101/최대 점프 횟수/maximum-number-of-jumps.js
@@ -5,15 +5,18 @@ const n = Number(input[0]);
 const arr = input[1].split(' ').map(Number);
 
 const dp = Array(n).fill(0);
+dp[0] = 1
 
 for ( let i = 0 ; i < n ; i ++ ) {
+    if ( dp[i] === 0 ) { continue }
     const dist = arr[i];
     for ( let j = 1 ; j <= dist ; j ++ ) {
         if ( i + j >= n ) { continue }
-        dp[i+j] = dp[i] + 1
+        dp[i+j] = Math.max(dp[i+j], dp[i] + 1);
     }
     // console.log(i);
     // console.log(dp);
 }
 
-console.log(Math.max(...dp));
+// console.log(dp);
+console.log(Math.max(...dp) - 1);
