4091ca99acacdec015d75eb8de24fbc64732392c
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-01 22:52:47 +0900

    Title: 최대 감소 부분 수열, Time: 178ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250101/최대 감소 부분 수열/longest-decreasing-subsequence.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+
+// console.log(arr);
+
+// initialize dp array
+const dp = Array(n).fill(0);
+dp[0] = 1;
+
+// console.log(dp);
+
+for ( let i = 1 ; i < n ; i ++ ) {
+    for ( let j = 0 ; j < i ; j ++ ) {
+        if ( arr[i] < arr[j] ) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+}
+
+console.log(Math.max(...dp));
