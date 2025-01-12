c0e3bbbf919e1b1f04fcf3f6b17554308607c0ba
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-12 20:14:01 +0900

    Title: 서로 다른 BST 개수 세기, Time: 174ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250112/서로 다른 BST 개수 세기/number-of-unique-bst.js
+++ 250112/서로 다른 BST 개수 세기/number-of-unique-bst.js
@@ -8,9 +8,10 @@ const dp = Array(n+1).fill(0);
 
 dp[1] = 1;
 dp[2] = 2;
+dp[3] = 5;
 
-for ( let i = 3 ; i <= n ; i ++ ) {
-    dp[i] = dp[i-1] * 2 + dp[i-1] - 1;
+for ( let i = 4 ; i <= n ; i ++ ) {
+    dp[i] = dp[i-1] * 2 + 10 * (i-3) + 4;
 }
 
 console.log(dp[n])
