668d9519da5128731d7ccaee53b099e65f83f192
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-13 18:48:30 +0900

    Title: 사각형 채우기 2, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 250113/사각형 채우기 2/rectangle-fill-2.js
+++ 250113/사각형 채우기 2/rectangle-fill-2.js
@@ -9,7 +9,7 @@ dp[1] = 1;
 dp[2] = 3;
 
 for ( let i = 3 ; i <= n ; i += 1 ) {
-    dp[i] = dp[i-1] + 2 * dp[i-2];
+    dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
 }    
 
-console.log(dp[n] % 10007);
+console.log(dp[n]);
