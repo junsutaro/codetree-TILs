d2c9ded1baede713d8720d55de5f84228c3c64d7
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-13 18:45:37 +0900

    Title: 사각형 채우기 2, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250113/사각형 채우기 2/rectangle-fill-2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+const n = Number(input);
+// const n = 8;
+const dp = Array(n + 1).fill(0);
+
+dp[1] = 1;
+dp[2] = 3;
+
+for ( let i = 3 ; i <= n ; i += 1 ) {
+    dp[i] = dp[i-1] + 2 * dp[i-2];
+}    
+
+console.log(dp[n] % 10007);
