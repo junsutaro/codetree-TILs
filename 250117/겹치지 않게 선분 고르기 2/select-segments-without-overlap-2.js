4e22ffa1cbf23bfcb91e3eac76373b54a083deb8
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-17 21:29:04 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 205ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250117/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.js
+++ 250117/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.js
@@ -4,18 +4,16 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const inputs = input.slice(1).map(elem => elem.split(' ').map(Number));
 
-inputs.sort((a, b) => {
-    return a[1] - b[1];
-})
+inputs.sort((a, b) => a[1] - b[1]);
 
-const dp = Array(n).fill(1);
+let count = 0;
+let lastEnd = -1;
 
-for ( let i = 0 ; i < n ; i ++ ) {
-    for ( let j = 0 ; j < i ; j ++ ) {
-        if ( inputs[j][1] < inputs[i][1] ) {
-            dp[i] = Math.max(dp[i], dp[j] + 1);
-        }
+for (let i = 0; i < n; i++) {
+    const [start, end] = inputs[i];
+    if (start > lastEnd) { 
+        lastEnd = end;
     }
 }
 
-console.log(Math.max(...dp));
+console.log(count);
