9f8dc335ddb9b906c7be9f785b87bc6e5064da16
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-18 22:04:56 +0900

    Title: 동전 거슬러주기, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 250118/동전 거슬러주기/coin-change.js
+++ 250118/동전 거슬러주기/coin-change.js
@@ -4,19 +4,25 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 const coins = input[1].split(' ').map(Number);
 
-const dp = Array(m+1).fill(Infinity);
-dp[0] = 0;
 
 
-for ( let i = 0 ; i <= m ; i ++ ) {
-    for ( let j = 0 ; j < n ; j ++ ) {
-        if ( i < coins[j] ) continue
-        dp[i] = Math.min(dp[i - coins[j]] + 1 , dp[i])
+
+function solve(n, m, coins) {
+    const dp = Array(m+1).fill(Infinity);
+    dp[0] = 0;
+
+    for ( let i = 0 ; i <= m ; i ++ ) {
+        for ( let j = 0 ; j < n ; j ++ ) {
+            if ( i < coins[j] ) continue
+            dp[i] = Math.min(dp[i - coins[j]] + 1 , dp[i])
+        }
     }
+
+    return ( dp[m] !== Infinity ? dp[m] : -1 );
 }
 
-if ( dp[m] === Infinity ) {
-    console.log(-1);
-} else {
-    console.log(dp[m]);
-}
+
+const ans =  solve(n, m, coins)
+
+console.log(ans);
+
