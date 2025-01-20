5bba4466692ffd5fd536a6e0395b5ced82f837fd
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-20 17:07:33 +0900

    Title: 최소 차 분할, Time: 168ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250120/최소 차 분할/minimum-diff-partition.js
+++ 250120/최소 차 분할/minimum-diff-partition.js
@@ -7,18 +7,20 @@ const nums = input[1].trim().split(' ').map(Number);
 let total = nums.reduce((acc, num) => acc + num , 0);
 
 let minVal = Infinity;
-const dp = Array(n+1).fill(0);
+const dp = Array(n).fill(0);
 dp[0] = 0;
 
 let ans = Infinity;
 for ( const num of nums ) {
-    for ( let i = 1 ; i <= n ; i ++ ) {
+    for ( let i = 1 ; i < n ; i ++ ) {
         let sumVal = dp[i-1] + num; // first, adapt now num
         let temp = Math.abs(sumVal * 2 - total)
 
-        if ( temp < minVal ) {
+        if ( temp <= minVal ) {
+            // console.log('num, sumVal, temp, minVal')
             minVal = temp;
             dp[i] = sumVal;
+            // console.log(num, sumVal, temp, minVal)
         }
     }
 }
