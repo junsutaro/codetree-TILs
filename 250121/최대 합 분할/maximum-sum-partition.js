d9beac44e4c463354824aae4d5c78b7484415855
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-21 11:45:23 +0900

    Title: 최대 합 분할, Time: 5090ms, Memory: 78MB, Status: Time Limit Exceed - CodeTree

--- 250121/최대 합 분할/maximum-sum-partition.js
+++ 250121/최대 합 분할/maximum-sum-partition.js
@@ -4,6 +4,11 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const nums = input[1].trim().split(' ').map(Number);
 
+// const n = 6
+// const nums = [4, 1, 1, 2, 4, 5];
+
+nums.sort((a, b) => b - a);
+
 const total = nums.reduce((acc, num) => acc + num, 0);
 
 const dp = Array.from({length : total + 1}, () => false);
@@ -26,11 +31,18 @@ for ( const lst of dp ) {
     const dp2 = Array(lstTotal+1).fill(false);
     dp2[0] = true;
     
+
+
     for ( const num of lst ) {
         for ( let i = lstTotal ; i >= num ; i -- ) {
             dp2[i] = dp2[i-num] || dp2[i];
         }
     }
+
+    // console.log(lst);
+    // console.log(dp2);
+    // console.log(aTotal)
+
     if ( dp2[aTotal] ) {
         ans = Math.max(aTotal, ans);
     }
