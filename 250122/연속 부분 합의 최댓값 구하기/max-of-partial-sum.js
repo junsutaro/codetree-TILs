6a4350a23d0fa1a9d66d70e8595afef5183dde82
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-22 19:17:30 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 207ms, Memory: 23MB, Status: Passed - CodeTree

--- 250122/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
+++ 250122/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
@@ -5,11 +5,11 @@ const n = Number(input[0]);
 const nums = input[1].trim().split(' ').map(Number);
 
 
-const dp = Array(n+1).fill(Infinity);
+const dp = Array(n).fill(Infinity);
 dp[0] = nums[0];
 
-for ( let i = 1 ; i <= n ; i ++ ) {
-    dp[i] = Math.max(nums[i-1], dp[i-1] + nums[i-1]);    
+for ( let i = 1 ; i < n ; i ++ ) {
+    dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);    
 }
 
 console.log(Math.max(...dp));
