ac7a6e474cbc2eb60d788df82d9aaaee0093379f
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-01-23 19:42:31 +0900

    Title: 숫자 전쟁, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 250123/숫자 전쟁/number-war.js
+++ 250123/숫자 전쟁/number-war.js
@@ -2,65 +2,34 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const you = input[1].trim().split(' ').map(Number);
-const me = input[2].trim().split(' ').map(Number);
+const A = [0, ...input[1].split(' ').map(Number)];
+const B = [0, ...input[2].split(' ').map(Number)];
 
-const p1Max = Math.max(...you);
-const dp = Array.from({length: 2 * n}, () => Array(2 * n).fill([0, 0, 0]));
+const dp = Array.from({ length: n+1 }, () => Array(n+1).fill(-1));
 
-function isRange(i, j) {
-    return 0 <= i && i <= n && 0 <= j && j <= n;
+function solve(a, b) {
+  if (a === n+1 || b === n+1) {
+    return 0;
+  }
+  
+  if (dp[a][b] !== -1) {
+    return dp[a][b];
+  }
+  
+  let ret = 0;
+  
+  if (A[a] < B[b]) {
+    ret = Math.max(ret, solve(a+1, b));
+  }
+  
+  if (A[a] > B[b]) {
+    ret = Math.max(ret, solve(a, b+1) + B[b]);
+  }
+  
+  ret = Math.max(ret, solve(a+1, b+1));
+  
+  dp[a][b] = ret;
+  return ret;
 }
 
-function game(i, j) {
-    if ( me[i] < you[j] ) { 
-        return [i+1, j, me[i]];
-    }
-    else if ( me[i] === you[j] ) {
-        return [i+1, j+1, 0];
-    } else {
-        return [i, j+1, 0];
-    }
-}
-
-dp[0][0] = [0, 0, 0]
-// dp[0][0] = game(0, 0)
-
-
-for ( let j = 1 ; j < n ; j ++ ) {
-    const [bi, bj, score] = dp[0][j-1];
-    dp[0][j] = [bi+1, bj+1, 0]
-}
-
-for ( let i = 1 ; i < 2 * n - 1 ; i ++ ) {
-    const [bi, bj, nowScore] = dp[i-1][0];
-    const [ni, nj, addScore] = game(bi, bj);
-    dp[i][0] = [ni, nj, nowScore + addScore];
-}
-
-
-let ans = 0;
-
-for ( let i = 1 ; i < 2 * n - 1 ; i ++ ) {
-    for ( let j = 1 ; j < n ; j ++ ) { // 둘 다 버리는 횟수
-
-
-        const [ui, uj, upScore] = dp[i-1][j]; // 위에서 온거
-        const [ni1, nj1, addScore1] = game(ui, uj);
-
-        const [li, lj, leftScore] = dp[i][j-1]; // 왼쪽에서 온거
-        const [ni2, nj2, addScore2] = game(li, lj);
-
-        if ( leftScore > upScore + addScore1 ) {
-            dp[i][j] = [ni2, nj2, leftScore];
-        } else {
-            dp[i][j] = [ni1, nj1, upScore + addScore1];
-        }
-        ans = Math.max(ans, dp[i][j][2] )
-    }
-}
-
-console.log(ans);
-
-
-
+console.log(solve(1, 1));
