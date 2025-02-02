8597033c96cd0708ec3872e1aaf0d949e2419908
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-02 23:16:38 +0900

    Title: 세 수의 합과 평균 구하기, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 250202/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
+++ 250202/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim();
 const [a, b, c] = input.split(' ').map(Number);
 
 const sum = a + b + c;
-const avg = sum // 3;
+const avg = sum / 3;
 
 console.log(sum);
-console.log(avg);
+console.log(Math.floor(avg));
