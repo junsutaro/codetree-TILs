f62df5b004bbcee881f9adebe262cfffcda52db2
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-06 14:34:20 +0900

    Title: 도전! 369게임, Time: 183ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250206/도전! 369게임/369-game-challenge.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+const n = Number(input);
+
+function find123(num) {
+    while (num > 0) {
+        if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+            return true
+        }
+        num = Math.floor(num / 10);
+    }
+}
+
+
+function judge(num) {
+    
+    if ( find123(num) ) {
+        return true;
+    }
+    else if ( num % 3 === 0 ) {
+        return true;
+    }
+
+}
+
+let ans = 0;
+for ( let i = 1 ; i <= n ; i ++ ) {
+    if (judge(i)) {
+        ans += 1;
+    }
+}
+
+console.log(ans & ( 10 ** 9 + 7))
