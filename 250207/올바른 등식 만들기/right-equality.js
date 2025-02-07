0e1f2af8a2d74778f894bb3e389466b17ed5006f
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-07 17:48:54 +0900

    Title: 올바른 등식 만들기, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 250207/올바른 등식 만들기/right-equality.js
+++ 250207/올바른 등식 만들기/right-equality.js
@@ -7,14 +7,14 @@ const nums = input[1].trim().split(' ').map(Number);
 const OFFSET = 20;
 const MAX_RANGE = 40;
 
-const dp = Array.from({length : n} , () => Array(MAX_RANGE + 1).fill(0));
-dp[0][OFFSET - nums[0]] += 1;
-dp[0][OFFSET + nums[0]] += 1;
+const dp = Array.from({length : n} , () => Array(MAX_RANGE + 1).fill(0n));
+dp[0][OFFSET - nums[0]] += 1n;
+dp[0][OFFSET + nums[0]] += 1n;
 
 // i 는 선택 횟수, j는 dp 카운팅 배열임
 
 for ( let i = 1 ; i < n ; i ++ ) {
-    const newDp = Array(MAX_RANGE + 1).fill(0);
+    const newDp = Array(MAX_RANGE + 1).fill(0n);
 
     for ( let j = 0 ; j <= MAX_RANGE ; j ++ ) {
         if ( dp[i-1][j] === 0 ) continue;
@@ -30,4 +30,4 @@ for ( let i = 1 ; i < n ; i ++ ) {
     dp[i] = newDp;
 }
 
-console.log(dp[n-1][m+OFFSET]);
+console.log(dp[n-1][m+OFFSET].toString());  // BigInt는 .toString()으로 변환 필요
