ea4e0b5931673d4863c4079e00ec5718c5823782
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-09 14:50:49 +0900

    Title: M개의 구간을 선택하기, Time: 168ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250209/M개의 구간을 선택하기/select-m-sections.js
+++ 250209/M개의 구간을 선택하기/select-m-sections.js
@@ -8,7 +8,10 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].trim().split(' ').map(Number);
 const nums = [0].concat(input[1].trim().split(' ').map(Number));
 
-const dp = Array.from({length: n+1}, () => Array(m+1).fill(-1));
+// const [n, m] = [5, 2];
+// const nums = [0, -5, -2, -3, -1, -6];
+
+const dp = Array.from({length: n+1}, () => Array(m+1).fill(-Infinity));
 
 dp[0][0] = 0;
 dp[1][1] = nums[1];
@@ -19,11 +22,19 @@ for ( let i = 2 ; i <= n ; i ++ ) {
 
 for ( let i = 2 ; i <= n ; i ++ ) {
     for ( let j = 1; j <= m ; j ++ ) {
-        if ( i < j * 2 - 1 ) continue;
-
-        dp[i][j] = Math.max(dp[i-2][j-1] + nums[i], dp[i][j]);
-        dp[i][j] = Math.max(dp[i-1][j] + nums[i], dp[i][j]);
+        if ( i >= j * 2 - 1 ) {
+            dp[i][j] = Math.max(dp[i-2][j-1] + nums[i], dp[i][j]);
+            dp[i][j] = Math.max(dp[i-1][j] + nums[i], dp[i][j]);
+        }
     }
 }
 
-console.log(dp[n][m]);
+// console.log(dp[n][m]);
+// console.log(dp);
+
+let ans = -Infinity;
+for ( let i = 0 ; i <= n ; i ++ ) {
+    ans = Math.max(ans, dp[i][m]);
+}
+
+console.log(ans);
