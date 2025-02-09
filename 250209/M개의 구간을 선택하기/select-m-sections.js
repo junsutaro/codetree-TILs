222eba9a1163e0f61bef57dc8267c2f3cdd77273
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-09 21:13:17 +0900

    Title: M개의 구간을 선택하기, Time: 171ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250209/M개의 구간을 선택하기/select-m-sections.js
+++ 250209/M개의 구간을 선택하기/select-m-sections.js
@@ -8,51 +8,22 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].trim().split(' ').map(Number);
 const nums = [0].concat(input[1].trim().split(' ').map(Number));
 
-const prefix = Array(n + 1).fill(0);
-for (let i = 1; i <= n; i++) {
-  prefix[i] = prefix[i - 1] + nums[i];
-}
+const dp = Array.from({length: n+1}, () => Array(m+1).fill(-Infinity));
 
-// const [n, m] = [5, 2];
-// const nums = [0, -5, -2, -3, -1, -6];
+dp[0][0] = 0;
+dp[1][1] = nums[1];
 
-const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(-Infinity));
-const sub = Array.from({ length: n + 1 }, () => Array(m + 1).fill(-Infinity));
-
-for (let i = 0; i <= n; i++) {
-  dp[i][0] = 0;
+for ( let i = 2 ; i <= n ; i ++ ) {
+    dp[i][1] = Math.max(nums[i], dp[i-1][1] + nums[i]); 
 }
 
 for ( let i = 2 ; i <= n ; i ++ ) {
-    dp[i][1] = dp[i-1][1] + nums[i]; 
-}
-
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= m; j++) {
-    // j개의 구간을 만들기 위한 최소 원소 개수는 2*j - 1입니다.
-    if (i < 2 * j - 1) continue;
-
-    if (i === 1) {
-      // i가 1이면 오직 j가 1인 경우만 가능합니다.
-      if (j === 1) {
-        sub[i][j] = nums[i];
-        dp[i][j] = nums[i];
-      }
-    } else {
-        // j가 1이어도 i가 1부터 시작 안해도 됨 ;
-      if (j === 1) {
-        const candidate1 = sub[i - 1][j] + nums[i];
-        const candidate2 = nums[i];
-        sub[i][j] = Math.max(candidate1, candidate2);
-        dp[i][j] = Math.max(dp[i - 1][j], sub[i][j]);
-      } else {
-        const candidate1 = sub[i - 1][j] + nums[i];
-        const candidate2 = (i - 2 >= 0 ? dp[i - 2][j - 1] : -Infinity) + nums[i];
-        sub[i][j] = Math.max(candidate1, candidate2);
-        dp[i][j] = Math.max(dp[i - 1][j], sub[i][j]);
-      }
+    for ( let j = 1; j <= m ; j ++ ) {
+        if ( i >= j * 2 - 1 ) {
+            dp[i][j] = Math.max(dp[i-2][j-1] + nums[i], dp[i][j]);
+        }
+        dp[i][j] = Math.max(dp[i-1][j] + nums[i], dp[i][j]);
     }
-  }
 }
 
 // console.log(dp[n][m]);
