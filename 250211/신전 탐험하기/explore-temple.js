ff4b923bd0c9ab3979721f699df47752fca1a531
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-11 13:41:46 +0900

    Title: 신전 탐험하기, Time: 214ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250211/신전 탐험하기/explore-temple.js
+++ 250211/신전 탐험하기/explore-temple.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
-const golds = input.slice(1).map(elem => elem.split(' ').map(Number));
+const golds = input.slice(1).map(elem => elem.trim().split(' ').map(Number));
 // console.log(golds);
 
 const dp = Array.from({length: n}, () => Array(3).fill(0));
-dp[0] = golds[0];
+dp[0] = [...golds[0]];
 
 for ( let i = 1 ; i < n ; i ++ ) {
     for ( let j = 0 ; j < 3 ; j ++ ) {
         for ( let k = 0 ; k < 3 ; k ++ ) {
             if ( j === k ) continue;
-            if ( !golds[i][j] ) continue;
+
             dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + golds[i][j]);
         }
     }
