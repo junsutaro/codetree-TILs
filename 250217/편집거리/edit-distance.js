f5146f65d39e8f1ced61bd8ba23549dfc9bf9488
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-17 13:14:10 +0900

    Title: 편집거리, Time: 267ms, Memory: 31MB, Status: Passed - CodeTree

--- 250217/편집거리/edit-distance.js
+++ 250217/편집거리/edit-distance.js
@@ -1,29 +1,29 @@
-
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const A = [0, ...input[0].split('')];;
-const B = [0, ...input[1].split('')];;
+const A = [0, ...input[0].split('')];
+const B = [0, ...input[1].split('')];
 
 const n = A.length - 1;
 const m = B.length - 1;
 
-const dp = Array.from({length: n+1}, () => Array(m+1).fill(0));
+const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
 
-for ( let i = 1 ; i <= n ; i ++ ) dp[i][1] = i;
-for ( let j = 1 ; j <= m ; j ++ ) dp[1][j] = j;;
+for (let i = 1; i <= n; i++) dp[i][0] = i;
+for (let j = 1; j <= m; j++) dp[0][j] = j;
 
-for ( let i = 1 ; i <= n ; i ++ ) {
-    for ( let j = 1; j <= m ; j ++ ) {
-        if ( A[i-1] === B[j-1]) {
-            dp[i][j] = dp[i-1][j-1];
-        }else {
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+        if (A[i] === B[j]) {
+            dp[i][j] = dp[i - 1][j - 1]; 
+        } else {
             dp[i][j] = Math.min(
-                dp[i-1][j] + 1,
-                dp[i][j-1] + 1,
-                dp[i-1][j-1] + 1
-            )
+                dp[i - 1][j] + 1,   // 삭제
+                dp[i][j - 1] + 1,   // 삽입
+                dp[i - 1][j - 1] + 1 // 변경
+            );
         }
     }
 }
 
+// 최종 편집 거리 출력
 console.log(dp[n][m]);
