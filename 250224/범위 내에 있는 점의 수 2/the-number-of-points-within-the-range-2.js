8b00f3004834dcf58d4cb02aa6c83f7b90084db3
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-24 20:56:16 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 182ms, Memory: 17MB, Status: Runtime Error - CodeTree

---
+++ 250224/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, q] = input[0].split(' ').map(Number);
+
+const MAX_NUM = 100000;
+const dp = Array(MAX_NUM + 1).fill(0);
+input[1].split(' ').map(elem => {
+    dp[Number(elem)] = 1;
+})
+const prefixSum = Array(MAX_NUM + 1).fill(0);
+
+for ( let i = 1 ; i <= MAX_NUM ; i ++ ) {
+    prefixSum[i] = prefixSum[i-1] + dp[i];
+}
+
+input.slice(2).map(elem => {
+const prefixSum = Array(MAX_NUM + 1).fill(0);
+
+for ( let i = 1 ; i <= MAX_NUM ; i ++ ) {
+    prefixSum[i] = prefixSum[i-1] + dp[i];
+}
+
+input.slice(2).map(elem => {
+    [s, e] = elem.split(' ').map(Number);
+    console.log(prefixSum[e] - prefixSum[s-1]);
+})
+
+닫기
+    [s, e] = elem.split(' ').map(Number);
+    console.log(prefixSum[e] - prefixSum[s-1]);
+})
