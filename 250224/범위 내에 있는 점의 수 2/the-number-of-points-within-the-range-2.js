bb642f2008968ec6728d15067a64010ddea05623
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-24 21:04:04 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 1440ms, Memory: 60MB, Status: Wrong Answer - CodeTree

--- 250224/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.js
+++ 250224/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.js
@@ -3,9 +3,9 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, q] = input[0].split(' ').map(Number);
 
-const MAX_NUM = 100000;
+const MAX_NUM = 1000000;
 const dp = Array(MAX_NUM + 1).fill(0);
-input[1].split(' ').map(elem => {
+input[1].trim().split(' ').map(elem => {
     dp[Number(elem)] = 1;
 })
 const prefixSum = Array(MAX_NUM + 1).fill(0);
@@ -15,6 +15,6 @@ for ( let i = 1 ; i <= MAX_NUM ; i ++ ) {
 }
 
 input.slice(2).map(elem => {
-    [s, e] = elem.split(' ').map(Number);
+    [s, e] = elem.trim().split(' ').map(Number);
     console.log(prefixSum[e] - prefixSum[s-1]);
 })
