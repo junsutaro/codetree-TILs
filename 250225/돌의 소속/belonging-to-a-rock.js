1ef5f9af8e6f801e51f05e9aeb4b06ed640bde7f
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-25 10:03:12 +0900

    Title: 돌의 소속, Time: 1463ms, Memory: 55MB, Status: Passed - CodeTree

--- 250225/돌의 소속/belonging-to-a-rock.js
+++ 250225/돌의 소속/belonging-to-a-rock.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n, q] = input[0].split(' ').map(Number);
-const stones = Array.from({length : 4}, () => Array(n+1).fill(0));
+const [n, q] = input[0].trim().split(' ').map(Number);
+const arr = input.slice(1, 1 + n).map(Number);
+const queries = input.slice(1 + n, 1 + n + q).map(line => line.trim().split(' ').map(Number));
 
-input.slice(1, n+1).map((elem, idx) => {
-    const [group, num] = [Number(elem), idx + 1]
-    stones[group][num] = 1;
-})
+const MAX_C = 3;
+const prefixSum = Array.from(Array(MAX_C + 1), () => Array(n + 1).fill(0));
 
-const prefixSums = Array.from({length : 4}, () => Array(n+1).fill(0));
+function getSum(c, s, e) {
+    return prefixSum[c][e] - prefixSum[c][s - 1];
+}
 
-for ( let k = 1 ; k <= 3 ; k ++ ) {
-    for ( let i = 1 ; i <= n ; i ++ ) {
-        prefixSums[k][i] = prefixSums[k][i-1] + stones[k][i];
+for (let i = 1; i <= MAX_C; i++) {
+    prefixSum[i][0] = 0;
+    for (let j = 1; j <= n; j++) {
+        // arr[j - 1]가 i라면, 개수가 하나 더 증가합니다.
+        if (arr[j - 1] === i) {
+            prefixSum[i][j] = prefixSum[i][j - 1] + 1;
+        } else {
+            prefixSum[i][j] = prefixSum[i][j - 1];
+        }
     }
 }
 
-
-input.slice(n+1).map(elem => {
-    const [s, e] = elem.split(' ').map(Number);
-    const ans = [];
-    for ( let i = 1 ; i <= 3 ; i ++ ) {
-        const score = prefixSums[i][e] - prefixSums[i][s-1];
-        ans.push(score)
-    }
-    console.log(ans.join(' '));
-})
+queries.forEach(([a, b]) => {
+    console.log(getSum(1, a, b), getSum(2, a, b), getSum(3, a, b));
+});
