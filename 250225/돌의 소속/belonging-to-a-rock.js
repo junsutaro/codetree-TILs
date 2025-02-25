6512d056850f5c79aa743736452db714c647430d
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-25 10:26:41 +0900

    Title: 돌의 소속, Time: 1568ms, Memory: 56MB, Status: Passed - CodeTree

--- 250225/돌의 소속/belonging-to-a-rock.js
+++ 250225/돌의 소속/belonging-to-a-rock.js
@@ -1,29 +1,25 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n, q] = input[0].trim().split(' ').map(Number);
-const arr = input.slice(1, 1 + n).map(Number);
-const queries = input.slice(1 + n, 1 + n + q).map(line => line.trim().split(' ').map(Number));
+const [n, q] = input[0].split(' ').map(Number);
+const arr = [0, ...input.slice(1, n+1).map(Number)];
+const queries = input.slice(n+1, n+q+1).map(elem => elem.split(' ').map(Number))
+const prefixSum = Array.from({length : 4} , () => Array(n+1).fill(0));
 
-const MAX_C = 3;
-const prefixSum = Array.from(Array(MAX_C + 1), () => Array(n + 1).fill(0));
-
-function getSum(c, s, e) {
-    return prefixSum[c][e] - prefixSum[c][s - 1];
-}
-
-for (let i = 1; i <= MAX_C; i++) {
-    prefixSum[i][0] = 0;
-    for (let j = 1; j <= n; j++) {
-        // arr[j - 1]가 i라면, 개수가 하나 더 증가합니다.
-        if (arr[j - 1] === i) {
-            prefixSum[i][j] = prefixSum[i][j - 1] + 1;
+for ( let i = 1 ; i <= 3 ; i ++ ) {
+    for ( let j = 1 ; j <= n ; j ++ ) {
+        if ( arr[j] === i ) {
+            prefixSum[i][j] = prefixSum[i][j-1] + 1;
         } else {
-            prefixSum[i][j] = prefixSum[i][j - 1];
+            prefixSum[i][j] = prefixSum[i][j-1];
         }
     }
 }
 
-queries.forEach(([a, b]) => {
-    console.log(getSum(1, a, b), getSum(2, a, b), getSum(3, a, b));
-});
+function getAns(group, s, e) {
+    return prefixSum[group][e] - prefixSum[group][s-1];
+}
+
+for ( const [s, e] of queries ) {
+    console.log(getAns(1, s, e), getAns(2, s, e), getAns(3, s ,e))
+}
