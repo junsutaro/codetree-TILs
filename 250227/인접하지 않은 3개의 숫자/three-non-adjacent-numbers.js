1c0c7b75f30175f19d5100e8430e45ccd4d4aef2
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-02-27 18:46:44 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250227/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const nums = [0, ...input[1].split(' ').map(Number)];
+const dp = Array.from({length : n+1}, () => Array(4).fill(0));
+
+for ( let i = 1 ; i <= n ; i ++ ) {
+    dp[i][1] = nums[i];
+}
+
+// console.log(dp);
+
+let ans = 0;
+
+for ( let i = 2 ; i <= n ; i ++ ) {
+    for ( let j = 2 ; j < 4 ; j ++ ) {
+        dp[i][j] = Math.max(dp[i-2][j-1] + nums[i], dp[i-1][j])
+        ans = Math.max(ans, dp[i][j])
+    }
+}
+// console.log(dp);
+console.log(ans);
