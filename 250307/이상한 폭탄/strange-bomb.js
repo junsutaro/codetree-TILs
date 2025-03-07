e4858a3e576901e3c84ec00ebe1074839efb5e75
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-07 11:54:49 +0900

    Title: 이상한 폭탄, Time: 229ms, Memory: 25MB, Status: Passed - CodeTree

--- 250307/이상한 폭탄/strange-bomb.js
+++ 250307/이상한 폭탄/strange-bomb.js
@@ -2,28 +2,28 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 let [n, k] = input[0].split(' ').map(Number);
-let bombs = input.slice(1, Number(n) + 1).map(Number);
+let bombs = input.slice(1, n + 1).map(Number);
 
-// Please Write your code here.
 let ans = -1;
-
 const frontK = new Map();
 
-for ( let i = 0 ; i < n ; i ++ ) {
+for (let i = 0; i < k; i++) {
     const thisBomb = bombs[i];
-    if ( frontK.has(thisBomb) ) {
+    if (frontK.has(thisBomb)) {
         ans = Math.max(ans, thisBomb);
     }
-    frontK.set(thisBomb, (frontK.get(thisBomb) || 0) + 1 )
+    frontK.set(thisBomb, (frontK.get(thisBomb) || 0) + 1);
 }
 
-for ( let i = k ; i < n ; i ++ ) {
+for (let i = k; i < n; i++) {
     const thisBomb = bombs[i];
-    const deleteBomb = bombs[i-k];
-    if ( frontK.has(thisBomb) ) {
+    const deleteBomb = bombs[i - k];
+
+    if (frontK.has(thisBomb)) {
         ans = Math.max(ans, thisBomb);
     }
-    frontK.set(thisBomb, (frontK.get(thisBomb) || 0 ) + 1)
+    frontK.set(thisBomb, (frontK.get(thisBomb) || 0) + 1);
+
     if (frontK.get(deleteBomb) === 1) {
         frontK.delete(deleteBomb);
     } else {
