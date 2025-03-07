82a49f58033ecf0009655f6219a706cf32e9918b
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-07 21:15:38 +0900

    Title: 최소 에너지 비용, Time: 214ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250307/최소 에너지 비용/minimum-energy-cost.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const dist = input[1].split(' ').map(Number);
+const cost = input[2].split(' ').map(Number);
+
+// Please Write your code here.
+
+const suffix = Array(n).fill(0);
+for ( let i = n-2 ; i >= 0 ; i -- ) {
+    suffix[i] = suffix[i+1] + dist[i]
+}
+const ansList = Array(n).fill(0);
+
+for ( let i = n - 2 ; i >= 0 ; i -- ) {
+    let chargeAll = suffix[i] * cost[i];
+    let fitted = (suffix[i] - suffix[i+1]) * cost[i] + ansList[i+1]
+    ansList[i] = Math.min(chargeAll, fitted);
+}
+
+console.log(ansList[0])
