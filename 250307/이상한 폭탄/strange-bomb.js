7a793499b07eba8f027735c69b0f27d7cd690874
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-07 11:38:47 +0900

    Title: 이상한 폭탄, Time: 199ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250307/이상한 폭탄/strange-bomb.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [n, k] = input[0].split(' ').map(Number);
+let bombs = input.slice(1, Number(n) + 1).map(Number);
+
+// Please Write your code here.
+
+const frontK = Array(10).fill(0);
+for ( let i = 0 ; i < k ; i ++ ) {
+    const thisBomb = bombs[i]
+    frontK[thisBomb] += 1;
+}
+
+let ans = -1;
+for ( let i = k ; i < n ; i ++ ) {
+    const thisBomb = bombs[i]
+    const deleteBomb = bombs[i-k]
+    if ( frontK[thisBomb] ) {
+        ans = Math.max(thisBomb)
+    }
+    frontK[thisBomb] += 1;
+    frontK[deleteBomb] -= 1;
+}
+
+console.log(ans);
