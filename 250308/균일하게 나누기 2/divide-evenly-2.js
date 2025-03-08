086c05d05d943d58c8d3a3763ee98f5567008ce7
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-08 19:41:42 +0900

    Title: 균일하게 나누기 2, Time: 223ms, Memory: 17MB, Status: Passed - CodeTree

--- 250308/균일하게 나누기 2/divide-evenly-2.js
+++ 250308/균일하게 나누기 2/divide-evenly-2.js
@@ -1,58 +1,40 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
+const MAX_R = 1000;
+const MAX_Q = 4;
 const n = parseInt(input[0]);
 const points = [];
-const X = [];
-const Y = [];
-
 for (let i = 1; i <= n; i++) {
   const [x, y] = input[i].split(" ").map(Number);
   points.push([x, y]);
-  X.push(x);
-  Y.push(y);
 }
 
-const uniqueX = [...new Set(X)].sort((a, b) => a - b);
-const uniqueY = [...new Set(Y)].sort((a, b) => a - b);
+points.sort((a, b) => a[0] - b[0]);
 
-const xMap = new Map();
-const yMap = new Map();
-uniqueX.forEach((val, idx) => xMap.set(val, idx + 1));
-uniqueY.forEach((val, idx) => yMap.set(val, idx + 1));
+let ans = Infinity;
 
-const xLen = uniqueX.length;
-const yLen = uniqueY.length;
-
-const freq = Array.from({ length: xLen + 1 }, () => Array(yLen + 1).fill(0));
-for (const [x, y] of points) {
-  const cx = xMap.get(x);
-  const cy = yMap.get(y);
-  freq[cx][cy]++;
-}
-
-const ps = Array.from({ length: xLen + 1 }, () => Array(yLen + 1).fill(0));
-for (let i = 1; i <= xLen; i++) {
-  for (let j = 1; j <= yLen; j++) {
-    ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + freq[i][j];
+for (let b = 0; b <= MAX_R; b += 2) {
+  const cnt = new Array(MAX_Q + 1).fill(0);
+  // x = 0일 때 모든 점은 오른쪽에 있으므로 y값에 따라 1번 또는 4번 사분면에 분류
+  for (const [x, y] of points) {
+    if (y > b) cnt[1]++;
+    else cnt[4]++;
+  }
+  // x좌표 오름차순 스위핑
+  for (let i = 0; i < n; i++) {
+    if (i === 0 || points[i][0] !== points[i - 1][0]) {
+      ans = Math.min(ans, Math.max(cnt[1], cnt[2], cnt[3], cnt[4]));
+    }
+    const y = points[i][1];
+    if (y > b) {
+      cnt[1]--;
+      cnt[2]++;
+    } else {
+      cnt[4]--;
+      cnt[3]++;
+    }
   }
 }
 
-function safeGetSum(x1, y1, x2, y2) {
-  if (x1 > x2 || y1 > y2) return 0;
-  return ps[x2][y2] - ps[x1 - 1][y2] - ps[x2][y1 - 1] + ps[x1 - 1][y1 - 1];
-}
-
-let answer = Infinity;
-for (let v = 0; v <= xLen; v++) {
-  for (let h = 0; h <= yLen; h++) {
-    const lb = safeGetSum(1, 1, v, h);
-    const lt = safeGetSum(1, h + 1, v, yLen);
-    const rb = safeGetSum(v + 1, 1, xLen, h);
-    const rt = safeGetSum(v + 1, h + 1, xLen, yLen);
-    const curMax = Math.max(lb, lt, rb, rt);
-    answer = Math.min(answer, curMax);
-  }
-}
-
-console.log(answer);
+console.log(ans);
