a9b63d08b0ea8e6302c751fb6e5cdcb4d5b54d3b
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-08 19:38:03 +0900

    Title: 균일하게 나누기 2, Time: 206ms, Memory: 15MB, Status: Passed - CodeTree

--- 250308/균일하게 나누기 2/divide-evenly-2.js
+++ 250308/균일하게 나누기 2/divide-evenly-2.js
@@ -7,10 +7,10 @@ const X = [];
 const Y = [];
 
 for (let i = 1; i <= n; i++) {
-    const [x, y] = input[i].split(" ").map(Number);
-    points.push([x, y]);
-    X.push(x);
-    Y.push(y);
+  const [x, y] = input[i].split(" ").map(Number);
+  points.push([x, y]);
+  X.push(x);
+  Y.push(y);
 }
 
 const uniqueX = [...new Set(X)].sort((a, b) => a - b);
@@ -18,47 +18,41 @@ const uniqueY = [...new Set(Y)].sort((a, b) => a - b);
 
 const xMap = new Map();
 const yMap = new Map();
-
-uniqueX.forEach((val, idx) => {
-    xMap.set(val, idx + 1);
-});
-uniqueY.forEach((val, idx) => {
-    yMap.set(val, idx + 1);
-});
+uniqueX.forEach((val, idx) => xMap.set(val, idx + 1));
+uniqueY.forEach((val, idx) => yMap.set(val, idx + 1));
 
 const xLen = uniqueX.length;
 const yLen = uniqueY.length;
 
 const freq = Array.from({ length: xLen + 1 }, () => Array(yLen + 1).fill(0));
 for (const [x, y] of points) {
-    const cx = xMap.get(x);
-    const cy = yMap.get(y);
-    freq[cx][cy]++;
+  const cx = xMap.get(x);
+  const cy = yMap.get(y);
+  freq[cx][cy]++;
 }
 
 const ps = Array.from({ length: xLen + 1 }, () => Array(yLen + 1).fill(0));
 for (let i = 1; i <= xLen; i++) {
-    for (let j = 1; j <= yLen; j++) {
-        ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + freq[i][j];
-    }
+  for (let j = 1; j <= yLen; j++) {
+    ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + freq[i][j];
+  }
 }
 
-function getSum(x1, y1, x2, y2) {
-    return ps[x2][y2] - ps[x1 - 1][y2] - ps[x2][y1 - 1] + ps[x1 - 1][y1 - 1];
+function safeGetSum(x1, y1, x2, y2) {
+  if (x1 > x2 || y1 > y2) return 0;
+  return ps[x2][y2] - ps[x1 - 1][y2] - ps[x2][y1 - 1] + ps[x1 - 1][y1 - 1];
 }
 
 let answer = Infinity;
-
-for (let v = 1; v < xLen; v++) {
-    for (let h = 1; h < yLen; h++) {
-        const leftBottom = getSum(1, 1, v, h);
-        const leftTop = getSum(1, h + 1, v, yLen);
-        const rightBottom = getSum(v + 1, 1, xLen, h);
-        const rightTop = getSum(v + 1, h + 1, xLen, yLen);
-
-        const M_cur = Math.max(leftBottom, leftTop, rightBottom, rightTop);
-        answer = Math.min(answer, M_cur);
-    }
+for (let v = 0; v <= xLen; v++) {
+  for (let h = 0; h <= yLen; h++) {
+    const lb = safeGetSum(1, 1, v, h);
+    const lt = safeGetSum(1, h + 1, v, yLen);
+    const rb = safeGetSum(v + 1, 1, xLen, h);
+    const rt = safeGetSum(v + 1, h + 1, xLen, yLen);
+    const curMax = Math.max(lb, lt, rb, rt);
+    answer = Math.min(answer, curMax);
+  }
 }
 
 console.log(answer);
