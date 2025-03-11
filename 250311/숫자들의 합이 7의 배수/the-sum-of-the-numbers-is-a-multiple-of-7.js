4bed0672bfabc32e45e944cc43748c887c423dad
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-11 14:41:02 +0900

    Title: 숫자들의 합이 7의 배수, Time: 194ms, Memory: 19MB, Status: Passed - CodeTree

--- 250311/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.js
+++ 250311/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.js
@@ -5,17 +5,15 @@ const n = parseInt(input[0]);
 const nums = input.slice(1, n + 1).map(Number);
 
 // Please Write your code here.
-const prefix = Array(n).fill(0);
-prefix[0] = nums[0] % 7;
+let tempRemain = nums[0] % 7;
 
 const mins = Array(7).fill(Infinity);
 const maxes = Array(7).fill(Infinity);
-mins[prefix[0]] = 0;
-maxes[prefix[0]] = 0;
+mins[tempRemain] = 0;
+maxes[tempRemain] = 0;
 
 for ( let i = 1; i < n ; i ++ ) {
-    let tempRemain = (prefix[i-1] + nums[i]) % 7;
-    prefix[i] = tempRemain
+    tempRemain = (tempRemain + nums[i]) % 7;
 
     mins[tempRemain] = Math.min(i, mins[tempRemain])
     maxes[tempRemain] = i
