20d01e49050165aa1164960b0e2285e12942a7b3
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-13 16:35:26 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 165ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250313/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = parseInt(input[0], 10);
+const arr = input[1].split(" ").map(Number);
+
+// Please Write your code here.
+
+let j = 0;
+let cntArr = Array(5).fill(0);
+let ans = 0;
+
+for ( let i = 0 ; i < n ; i ++ ) {
+    while ( j < n && cntArr[arr[j]] !== 1 ) {
+        cntArr[arr[j]] += 1;
+        j += 1;
+    }
+
+    ans = Math.max(ans, j - i + 1)
+    cntArr[i] -= 1
+}
+
+console.log(ans);
