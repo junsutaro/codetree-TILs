c7a8ea4581abb6d083b9c2ae43a093c55b5b281e
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-14 16:47:47 +0900

    Title: 바구니 안의 사탕, Time: 538ms, Memory: 68MB, Status: Wrong Answer - CodeTree

--- 250314/바구니 안의 사탕/candy-in-the-basket.js
+++ 250314/바구니 안의 사탕/candy-in-the-basket.js
@@ -4,29 +4,35 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const candies = [];
 
+let maxCandyPos = 0;
+
 for (let i = 1; i <= n; i++) {
     const [cnt, x] = input[i].split(' ').map(Number);
     candies.push({ x, cnt });
+    maxCandyPos = Math.max(maxCandyPos, x);
 }
 
-const MAX = 1000001
-// Please Write your code here.
+const MAX = Math.min(1000001, maxCandyPos + 1); 
 const candyArr = Array(MAX + 1).fill(0);
 const prefixSum = Array(MAX + 1).fill(0);
 
 candies.forEach(elem => {
-    candyArr[elem.x] = elem.cnt;
-})
-for ( let i = 1 ; i <= MAX ; i ++ ) {
-    prefixSum[i] = prefixSum[i-1] + candyArr[i]
-};
+    if (elem.x <= MAX) {
+        candyArr[elem.x] = elem.cnt;
+    }
+});
+
+for (let i = 1; i <= MAX; i++) {
+    prefixSum[i] = prefixSum[i - 1] + candyArr[i];
+}
+
 let ans = 0;
 
 if (2 * k + 1 >= MAX) {
-    ans = prefixSum[MAX]; // 모든 사탕을 포함하는 경우
+    ans = prefixSum[MAX - 1]; 
 } else {
-    for (let i = 2 * k + 1; i <= MAX; i++) {
-        let leftBound = Math.max(0, i - 2 * k - 1); // 0 이하 방지
+    for (let i = 0; i <= MAX; i++) {
+        let leftBound = Math.max(0, i - 2 * k - 1); 
         ans = Math.max(ans, prefixSum[i] - prefixSum[leftBound]);
     }
 }
