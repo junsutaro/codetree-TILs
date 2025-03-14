9cbe270dce5c74bd9ea9a9e2e8e5415058be2dd8
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-14 18:12:00 +0900

    Title: 바구니 안의 사탕, Time: 543ms, Memory: 48MB, Status: Passed - CodeTree

--- 250314/바구니 안의 사탕/candy-in-the-basket.js
+++ 250314/바구니 안의 사탕/candy-in-the-basket.js
@@ -3,30 +3,26 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const [n, k] = input[0].split(" ").map(Number);
 const candies = [];
+
 for (let i = 1; i <= n; i++) {
-    const [cnt, x] = input[i].split(" ").map(Number);
-    candies.push({ x, cnt });
+  const [cnt, x] = input[i].split(" ").map(Number);
+  candies.push({ x, cnt });
 }
 
-const MAX = 1000001; // 위치: 0 ~ 1000000
-const candyArr = Array(MAX).fill(0);  // 길이 1000001, 인덱스 0 ~ 1000000
-for (const { x, cnt } of candies) {
-    candyArr[x] += cnt; // 같은 위치에 여러 바구니가 있으면 누적
-}
-
-// prefixSum 배열: prefixSum[i]는 candyArr[0]부터 candyArr[i-1]까지의 합
-// 길이는 MAX+1
-const prefixSum = Array(MAX + 1).fill(0);
-for (let i = 0; i < MAX; i++) {
-    prefixSum[i+1] = prefixSum[i] + candyArr[i];
-}
+candies.sort((a, b) => a.x - b.x);
 
 let ans = 0;
-const windowLen = 2 * k + 1;
-for (let i = 0; i <= MAX; i++) {
-    const left = Math.max(0, i - windowLen);
-    const windowSum = prefixSum[i] - prefixSum[left];
-    ans = Math.max(ans, windowSum);
+let total_nums = 0;
+let j = 0;
+
+for (let i = 0; i < n; i++) {
+  while (j < n && candies[j].x - candies[i].x <= 2 * k) {
+    total_nums += candies[j].cnt;
+    j++;
+  }
+  ans = Math.max(ans, total_nums);
+  
+  total_nums -= candies[i].cnt;
 }
 
 console.log(ans);
