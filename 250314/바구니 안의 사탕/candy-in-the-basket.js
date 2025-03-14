65495f332efe276804b8d95cee6fd4671ef453ee
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-14 16:22:44 +0900

    Title: 바구니 안의 사탕, Time: 509ms, Memory: 68MB, Status: Wrong Answer - CodeTree

---
+++ 250314/바구니 안의 사탕/candy-in-the-basket.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const candies = [];
+
+for (let i = 1; i <= n; i++) {
+    const [cnt, x] = input[i].split(' ').map(Number);
+    candies.push({ x, cnt });
+}
+
+const MAX = 1000001
+// Please Write your code here.
+const candyArr = Array(MAX).fill(0);
+const prefixSum = Array(MAX).fill(0);
+candies.forEach(elem => candyArr[elem.x] = elem.cnt)
+prefixSum[0] = candyArr[0];
+for ( let i = 1 ; i < MAX ; i ++ ) {
+    prefixSum[i] = prefixSum[i-1] + candyArr[i];
+}
+
+let ans = 0;
+for ( let i = 2*k + 1 ; i < MAX ; i ++ ) {
+    ans = Math.max(ans, prefixSum[i] - prefixSum[i-2*k - 1])
+}
+console.log(ans);
