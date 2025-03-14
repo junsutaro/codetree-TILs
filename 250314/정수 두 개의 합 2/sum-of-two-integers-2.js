56a3c9a5183700a54c649992447ca68eb783cc50
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-14 16:10:26 +0900

    Title: 정수 두 개의 합 2, Time: 161ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250314/정수 두 개의 합 2/sum-of-two-integers-2.js
+++ 250314/정수 두 개의 합 2/sum-of-two-integers-2.js
@@ -3,5 +3,20 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, k] = input[0].split(" ").map(Number);
 const arr = input.slice(1).map(Number);
+arr.sort((a, b) => a - b)
+// Please Write your code here
+console.log(arr)
 
-// Please Write your code here.
+let ans = 0;
+let j = n-1;
+
+for ( let i = 0 ; i < n ; i ++ ) {
+    while ( j > i && arr[i] + arr[j] > k ) {
+        j -= 1;
+    }
+    
+    if ( i < j ) {
+        ans += (j - i);
+    }
+}
+console.log(ans);
