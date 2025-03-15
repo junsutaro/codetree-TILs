37ccac42dc5760fd02d3e1a981fdbd3de989d4f5
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-15 16:07:04 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 195ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.js
+++ 250315/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.js
@@ -13,13 +13,13 @@ let ans = 0;
 
 for ( let i = 0 ; i < n ; i ++ ) {
     // Is it work when get's result is undefined?
-    while ( j < n && wordMap.get(word[j]) !== 1 ) { 
+    while ( j < n && !wordMap.has(word[j]) ) { 
         wordMap.set(word[j], (wordMap.get(word[j]) || 0) + 1 )
         j += 1
     }
     // console.log(i, j);
     // console.log(wordMap)
     ans = Math.max(ans, j - i);
-    wordMap.set(word[i], 0)
+    wordMap.delete(word[i])
 }
 console.log(ans)
