44478875169792f7dbc9e12882053e44490d77c0
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-16 13:55:05 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 163ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250316/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.js
+++ 250316/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.js
@@ -1,132 +1,128 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync(0).toString().trim().split("\n");
 const [n, d] = input[0].split(" ").map(Number);
 const points = [];
 for (let i = 1; i <= n; i++) {
-  const [x, y] = input[i].split(" ").map(Number);
-  points.push({ x, y });
+    const [x, y] = input[i].split(" ").map(Number);
+    points.push({ x, y });
 }
-
-points.sort((a, b) => a.x - b.x);
 
 class Heap {
-  constructor(compare = (a, b) => a - b) {
-    this.arr = [];
-    this.compare = compare;
-  }
-
-  top() {
-    return this.arr[0];
-  }
-
-  pop() {
-    if (this.arr.length === 0) return undefined;
-    if (this.arr.length === 1) return this.arr.pop();
-    const top = this.arr[0];
-    this.arr[0] = this.arr.pop();
-    this._heapifyDown();
-    return top;
-  }
-
-  push(val) {
-    this.arr.push(val);
-    this._heapifyUp();
-  }
-
-  _heapifyUp() {
-    let idx = this.arr.length - 1;
-    while (idx > 0) {
-      let parent = Math.floor((idx - 1) / 2);
-      if (this.compare(this.arr[parent], this.arr[idx]) > 0) {
-        [this.arr[parent], this.arr[idx]] = [this.arr[idx], this.arr[parent]];
-        idx = parent;
-      } else {
-        break;
-      }
+    constructor(compare = (a, b) => a - b) {
+        this.arr = [];
+        this.compare = compare;
     }
-  }
 
-  _heapifyDown() {
-    let idx = 0;
-    while (true) {
-      let left = idx * 2 + 1;
-      let right = idx * 2 + 2;
-      let smallest = idx;
-      if (left < this.arr.length && this.compare(this.arr[smallest], this.arr[left]) > 0) {
-        smallest = left;
-      }
-      if (right < this.arr.length && this.compare(this.arr[smallest], this.arr[right]) > 0) {
-        smallest = right;
-      }
-      if (smallest === idx) break;
-      [this.arr[idx], this.arr[smallest]] = [this.arr[smallest], this.arr[idx]];
-      idx = smallest;
+    top() {
+        return this.arr[0];
     }
-  }
+
+    size() {
+        return this.arr.length;
+    }
+    
+    pop() {
+        if ( this.arr.length === 0 ) { return undefined };
+        if ( this.arr.length === 1 ) { return this.arr.pop() };
+        const top = this.arr[0];
+        this.arr[0] = this.arr.pop();
+        this.shiftDown();
+        return top
+    }
+
+    push(val) {
+        this.arr.push(val);
+        this.shiftUp();
+    }
+
+    shiftUp() {
+        let idx = this.arr.length - 1;
+        while ( idx > 0 ) {
+            let parent = Math.floor((idx - 1) / 2);
+            if ( this.compare(this.arr[parent], this.arr[idx]) > 0 ) {
+                [this.arr[parent], this.arr[idx]] = [this.arr[idx], this.arr[parent]];
+                idx = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    shiftDown() {
+        let idx = 0;
+        while ( true ) {
+            let left = idx * 2 + 1
+            let right = idx * 2 + 2
+            let smallest = idx;
+
+            if ( left < this.arr.length && this.compare(this.arr[smallest], this.arr[left]) > 0 ) {
+                smallest = left;
+            }
+            if ( right < this.arr.length && this.compare(this.arr[smallest], this.arr[right]) > 0 ) {
+                smallest = right;
+            }
+            if ( smallest === idx)  {
+                break;
+            }
+            [this.arr[idx], this.arr[smallest]] = [this.arr[smallest], this.arr[idx]];
+            idx = smallest;
+        }
+    }
 }
 
-const freq = new Map();
 
-function inc(val) {
-  freq.set(val, (freq.get(val) || 0) + 1);
+const counter = new Map();
+
+function add(idx) {
+    let key = points[idx].y;
+    counter.set(key, (counter.get(key) || 0) + 1)
 }
 
-function dec(val) {
-  freq.set(val, freq.get(val) - 1);
-  if (freq.get(val) === 0) {
-    freq.delete(val);
-  }
+function del(idx) {
+    let key = points[idx].y;
+    if ( counter.get(key) === 1 ) {
+        counter.delete(key)
+    } else {
+        counter.set(key, counter.get(key) - 1);
+    }
 }
 
 function getValidTop(heap) {
-  while (heap.arr.length > 0) {
-    const val = heap.top();
-    if (freq.has(val) && freq.get(val) > 0) {
-      return val;
+    while (heap.size() > 0) {
+        const val = heap.top();
+        if ( counter.has(val) ) {
+            return val;
+        }
+        heap.pop()
     }
-    heap.pop();
-  }
-  return undefined;
 }
 
-const minHeap = new Heap((a, b) => a - b);     
-const maxHeap = new Heap((a, b) => b - a);    
 
-let j = 0;
-let ans = Infinity;
+// Please Write your code here.
+points.sort((a, b) => a.x - b.x)
+// console.log(points)
 
-for (let i = 0; i < n; i++) {
-  while (j < n) {
-    const minVal = getValidTop(minHeap);
-    const maxVal = getValidTop(maxHeap);
+let j = 1;
+const minHeap = new Heap();
+const maxHeap = new Heap((a, b) => b - a);
 
-    if (
-      minVal !== undefined &&
-      maxVal !== undefined &&
-      maxVal - minVal >= d
-    ) {
-      break;
+maxHeap.push(points[0].y)
+minHeap.push(points[0].y)
+add(0);
+
+let ans = Infinity; 
+for ( let i = 0 ; i < n ; i ++ ) {
+    while ( j < n && (getValidTop(maxHeap) - getValidTop(minHeap)) < d ) {
+        add(j);
+        minHeap.push(points[j].y)
+        maxHeap.push(points[j].y)
+        j += 1
     }
-
-    const y = points[j].y;
-    inc(y);
-    minHeap.push(y);
-    maxHeap.push(y);
-    j++;
-  }
-
-  const currMin = getValidTop(minHeap);
-  const currMax = getValidTop(maxHeap);
-  if (
-    currMin !== undefined &&
-    currMax !== undefined &&
-    currMax - currMin >= d
-  ) {
-    ans = Math.min(ans, points[j - 1].x - points[i].x);
-  }
-
-  const removeY = points[i].y;
-  dec(removeY);
+    // console.log(i, j);
+    if ( j > i + 1 ) {
+        ans = Math.min(ans, points[j-1].x - points[i].x)
+    }
+    del(i)
 }
 
-console.log(ans === Infinity ? -1 : ans);
+console.log(ans);
