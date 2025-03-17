8fe01fbe27d77cb8c1b89e8af5fb16e6eb845381
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-17 20:57:09 +0900

    Title: 특정 조건으로 묶여있는 수 그룹, Time: 252ms, Memory: 20MB, Status: Passed - CodeTree

--- 250317/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.js
+++ 250317/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.js
@@ -12,8 +12,29 @@ arr.sort((a, b) => a - b);
 // Please Write your code here.
 
 const prefix = Array(n).fill(0);
+let left = 0;
 for ( let i = 0 ; i < n ; i ++ ) {
-    
+    while ( left < n && arr[i] - arr[left] > k ) {
+        left += 1;
+    }
+    const current = i - left + 1;
+    prefix[i] = i === 0 ? current : Math.max(prefix[i-1], current);
+}
+
+
+const suffix = Array(n).fill(0);
+let right = n-1;
+for ( let i = n-1 ; i >= 0 ; i -- ) {
+    while ( arr[right] - arr[i] > k ) {
+        right -= 1;
+    }
+    const current = right - i + 1;
+    suffix[i] = i === n-1 ? current : Math.max(suffix[i+1], current);
+}
+
+let ans = 0;
+for ( let i = 0 ; i < n-1 ; i ++ ) {
+    ans = Math.max(ans , prefix[i] + suffix[i+1])
 }
 
 console.log(ans);
