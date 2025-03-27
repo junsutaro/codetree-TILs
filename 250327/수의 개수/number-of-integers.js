88b2f7b5a0d819215e3ff51f38e4e8dc6e28167e
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-27 09:29:27 +0900

    Title: 수의 개수, Time: 757ms, Memory: 40MB, Status: Passed - Codetree

--- 250327/수의 개수/number-of-integers.js
+++ 250327/수의 개수/number-of-integers.js
@@ -2,30 +2,38 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].trim().split(' ').map(Number);
-const numbers = input[1].trim().split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
 const queries = input.slice(2).map(Number);
 
+
 function lowerBound(arr, target) {
-    let left = 0, right = arr.length;
-    while (left < right) {
+    let left = 0;
+    let right = arr.length;
+    while ( left < right ) {
         const mid = Math.floor((left + right) / 2);
-        if (arr[mid] < target) left = mid + 1;
-        else right = mid;
+        if ( arr[mid] >= target ) { 
+            right = mid;
+        } else {
+            left = mid + 1
+        }
     }
     return left;
 }
 
 function upperBound(arr, target) {
-    let left = 0, right = arr.length;
-    while (left < right) {
+    let left = 0; let right = arr.length;
+    while ( left < right ) {
         const mid = Math.floor((left + right) / 2);
-        if (arr[mid] <= target) left = mid + 1;
-        else right = mid;
+        if ( arr[mid] <= target ) {
+            left = mid + 1;
+        } else {    
+            right = mid;
+        }
     }
-    return left;
+    return right
 }
 
-queries.forEach(q => {
-    const count = upperBound(numbers, q) - lowerBound(numbers, q);
-    console.log(count);
-});
+for ( const num of queries ) {
+    const ans = upperBound(arr, num) - lowerBound(arr, num);
+    console.log(ans);
+}
