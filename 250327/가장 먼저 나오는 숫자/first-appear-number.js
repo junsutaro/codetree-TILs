2b327abf583c489960916615f7a880d4002d1928
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-03-27 09:41:15 +0900

    Title: 가장 먼저 나오는 숫자, Time: 788ms, Memory: 51MB, Status: Passed - Codetree

--- 250327/가장 먼저 나오는 숫자/first-appear-number.js
+++ 250327/가장 먼저 나오는 숫자/first-appear-number.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [N, M] = input[0].split(' ').map(Number);
-const arr = input[1].split(' ').map(Number);
-const queries = input[2].split(' ').map(Number);
+const [N, M] = input[0].trim().split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
+const queries = input[2].trim().split(' ').map(Number);
 
 // Please Write your code here.
 
