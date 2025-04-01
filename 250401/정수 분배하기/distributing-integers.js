f98123959fa19155038ff79ee532122ab4da7e2f
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-04-01 23:10:34 +0900

    Title: 정수 분배하기, Time: 192ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250401/정수 분배하기/distributing-integers.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(" ").map(Number);
+const arr = input.slice(1, 1 + n).map(Number);
+
+// Please Write your code here.
+
+function devide(arr, maxVal) {
+    let cnt = 0;
+    for ( let i = 0 ; i < arr.length ; i ++ ) {
+        while ( num <)
+        if ( num >= maxVal ) {
+            cnt += 1;
+            num -=
+        }
+    }
+}
