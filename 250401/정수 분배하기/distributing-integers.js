0631b75fb66c11eca8c7ff076e5d6579f7bf50e8
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-04-01 23:22:24 +0900

    Title: 정수 분배하기, Time: 5085ms, Memory: 15MB, Status: Time Limit Exceed - Codetree

--- 250401/정수 분배하기/distributing-integers.js
+++ 250401/정수 분배하기/distributing-integers.js
@@ -6,13 +6,30 @@ const arr = input.slice(1, 1 + n).map(Number);
 
 // Please Write your code here.
 
-function devide(arr, maxVal) {
+function canDevide(arr, maxVal) {
     let cnt = 0;
     for ( let i = 0 ; i < arr.length ; i ++ ) {
-        while ( num <)
-        if ( num >= maxVal ) {
+        let num = arr[i];
+        while ( num >= maxVal ) {
+            num -= maxVal;
             cnt += 1;
-            num -=
         }
+        if ( cnt >= m ) return true;
+    }
+    return false;
+}
+
+
+let left = 0;
+let right = 100000;
+
+while ( left < right ) {
+    const mid = Math.floor((left + right) / 2);
+    if ( canDevide(arr, mid) ) {
+        left = mid + 1;
+    } else {
+        right = mid;
     }
 }
+
+console.log(left - 1)
