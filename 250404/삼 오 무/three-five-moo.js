7a04b9fc66ae1c766c170624adf2e929f2b4d545
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-04-04 21:53:23 +0900

    Title: 삼 오 무, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

--- 250404/삼 오 무/three-five-moo.js
+++ 250404/삼 오 무/three-five-moo.js
@@ -4,12 +4,31 @@ const n = parseInt(input[0]);
 // Please Write your code here.
 
 // 이 숫자까지 moo가 몇개?
-function numOfMoo(num) {
-    let cnt = 0;
-    cnt += (num % 15) * 7;
-    num = num % 15
-    if ( num > 12 ) { cnt += 6 }
-    else if ( num > 10 ) { cnt += 5 }
-    else if ( num > 6 ) { cnt += 3 }
-    else if ( num > 3 ) { cnt += 1}
+function countMoo(num) {
+    const count3 = Math.floor(num / 3);
+    const count5 = Math.floor(num / 5);
+    const count15 = Math.floor(num / 15);
+    return count3 + count5 - count15;
 }
+
+function findAns(n) {
+    let left = 1;
+    let right = 2 * n; 
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        const mooCount = countMoo(mid);
+        const realCount = mid - mooCount; // Moo를 제외한 숫자의 개수
+
+        if (realCount < n) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;
+}
+
+const ans = findAns(n);
+console.log(ans);
