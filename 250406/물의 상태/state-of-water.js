c72878849e5bc349b59879405b26b3293650d673
JUNSUTARO <139520011+junsutaro@users.noreply.github.com>
2025-04-06 23:37:32 +0900

    Title: 물의 상태, Time: 188ms, Memory: 22MB, Status: Passed - Codetree

--- 250406/물의 상태/state-of-water.js
+++ 250406/물의 상태/state-of-water.js
@@ -3,6 +3,6 @@ const input = fs.readFileSync(0).toString().trim();
 
 const a = Number(input);
 
-if ( a < 1.0 ) { console.log('ice')}
+if ( a < 0 ) { console.log('ice')}
 else if ( a >= 100 ) {console.log('vapor')}
 else { console.log('water')}
